b2366cdad05c7b2f44473311d6c85dd032c0d9ff
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-04-22 09:49:45 +0000

    NEW: Two more simple tests.
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8007 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ tests/test_list/000250_update_field.php
@@ -0,0 +1,11 @@
+<?php
+include_once dirname(__FILE__) . "/../../include/general.php";
+include_once dirname(__FILE__) . "/../../include/resource_functions.php";
+
+
+update_field(1,8,"Test title");
+
+
+# Was it set?
+return (get_data_by_field(1,8)=="Test title");
+
--- tests/test_list/000300_copy_resource.php
+++ tests/test_list/000300_copy_resource.php
@@ -5,8 +5,10 @@ include_once dirname(__FILE__) . "/../../include/resource_functions.php";
 
 $new=copy_resource(1);
 
-# To do - add some metadata to the first one in tests prior to this one.
-# Then check to see if the data is preserved in the copy.
+
 
 # Did it work?
-return (get_resource_data($new)!==false);
+if (get_resource_data($new)===false) {return false;}
+
+# Was the title field we set on the original resource copied?
+return (get_data_by_field($new,8)=="Test title");
