dc92be0cfd336435434fab0ba02e24f9798edac5
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-04-22 09:53:24 +0000

    CHG: Added CLI check to tests, just in case they somehow work independantly and introduce a vulnerability.
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8009 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- tests/test_list/000200_create_resource.php
+++ tests/test_list/000200_create_resource.php
@@ -1,4 +1,5 @@
 <?php
+if (php_sapi_name()!=="cli") {exit("This utility is command line only.");}
 include_once dirname(__FILE__) . "/../../include/general.php";
 include_once dirname(__FILE__) . "/../../include/resource_functions.php";
 
