92ff80b1307447b7e6175062f4f28f007b6d8451
mhinton <mhinton@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-04-26 09:44:32 +0000

    NEW: Added -nosetup, -noteardown and ability to pass a series of test numbers as parameters.  E.g. "php test.php -nosetup -noteardown 800 400 623" will run tests 000400 000800 in isolation (and in order) and not 623 as not found
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8028 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- tests/test_list/000800_escape_check.php
+++ tests/test_list/000800_escape_check.php
@@ -6,7 +6,9 @@ $test_data=array
     (
     "this a line with a newline at the end\n" => 'this a line with a newline at the end\n',
     "this a line with a backslash-r at the end\r" => 'this a line with a backslash-r at the end\r',
-    "this a line with a backslashes \\in the middle before the first in" => 'this a line with a backslashes in the middle before the first in'
+    "this a line with a backslashes \\in the middle before the first in" => 'this a line with a backslashes in the middle before the first in',
+    "this a line has too many backslashes before the backslash-n \\\\\\n I hope it resolves it" => 'this a line has too many backslashes before the backslash-n \n I hope it resolves it',
+    "this a line has several single quotes '''  that are unescaped" => 'this a line has several single quotes \\\'\\\'\\\'  that are unescaped'
     );
 
 foreach($test_data as $input_value=>$expected_value)
