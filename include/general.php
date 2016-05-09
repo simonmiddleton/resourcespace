4610e60320b986a31b10db7d8a210fd977d66c65
rdamrau <rdamrau@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-05-09 12:26:39 +0000

    FIX: function auto_create_user_account() produced fatal error with PHP7. See issue #465 and #497
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8084 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/general.php
+++ include/general.php
@@ -1378,7 +1378,10 @@ function auto_create_user_account()
 			{
 			global $rs_session;
 			$rs_session=get_rs_session_id();
-			if($rs_session==false){continue;}
+			if($rs_session==false)
+				{
+				exit("No rs_session id found");
+				}
 			# Copy any anonymous session collections to the new user account 
 			if (!function_exists("get_session_collections"))
 				{
