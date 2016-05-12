c34e6d76768c1b9893f5638f97695b6eb49ff962
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-05-12 17:08:55 +0000

    FIX: clear function for simple search was not clearing 'add to simple search' fields
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8104 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/render_functions.php
+++ include/render_functions.php
@@ -22,7 +22,8 @@ function render_search_field($field,$value="",$autoupdate,$class="stdwidth",$for
     {
     node_field_options_override($field);
     
-    global $auto_order_checkbox, $auto_order_checkbox_case_insensitive, $lang, $category_tree_open, $minyear, $daterange_search, $is_search, $values, $n, $simple_search_show_dynamic_as_dropdown, $clear_function, $simple_search_display_condition, $autocomplete_search, $baseurl, $fields, $baseurl_short, $extrafooterhtml;
+    global $auto_order_checkbox, $auto_order_checkbox_case_insensitive, $lang, $category_tree_open, $minyear, $daterange_search, $searchbyday, $is_search, $values, $n, $simple_search_show_dynamic_as_dropdown, $clear_function, $simple_search_display_condition, $autocomplete_search, $baseurl, $fields, $baseurl_short, $extrafooterhtml;
+    
     $name="field_" . ($forsearchbar ? htmlspecialchars($field["name"]) : $field["ref"]);
     $id="field_" . $field["ref"];
     
@@ -293,7 +294,7 @@ function render_search_field($field,$value="",$autoupdate,$class="stdwidth",$for
 				<div class="SearchItem">
 			<?php } 
 			# Add to the clear function so clicking 'clear' clears this box.
-			$clear_function.="document.getElementById('field_" . $field["name"] . "').value='';";
+			$clear_function.="document.getElementById('field_" . ($forsearchbar? $field["ref"] : $field["name"]) . "').value='';";
         break;
     
         case 2: 
@@ -350,7 +351,7 @@ function render_search_field($field,$value="",$autoupdate,$class="stdwidth",$for
                 if($forsearchbar)
                 	{
                 	# Add to the clear function so clicking 'clear' clears this box.
-					$clear_function.="document.getElementById('field_" . $field["name"] . "').selectedIndex=0;";
+					$clear_function.="document.getElementById('field_" . ($forsearchbar ? $field["ref"] : $field["name"]) . "').selectedIndex=0;";
                 	}
                 }
             else
@@ -581,9 +582,9 @@ function render_search_field($field,$value="",$autoupdate,$class="stdwidth",$for
             	{
             	# Add to the clear function so clicking 'clear' clears this box.
 				$clear_function.="
-					document.getElementById('field_" . $field["name"] . "_year').selectedIndex=0;
-					document.getElementById('field_" . $field["name"] . "_month').selectedIndex=0;
-					document.getElementById('field_" . $field["name"] . "_day').selectedIndex=0;
+					document.getElementById('field_" . $field["ref"] . "_year').selectedIndex=0;
+					document.getElementById('field_" . $field["ref"] . "_month').selectedIndex=0;
+					document.getElementById('field_" . $field["ref"] . "_day').selectedIndex=0;
 					";
 				}
             }
