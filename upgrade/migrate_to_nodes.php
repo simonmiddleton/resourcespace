19d5d9a1cf3e661dc1cdaf240665c79a71686a57
mhinton <mhinton@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-05-17 15:48:19 +0000

    FIX - Now supports hit_count
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160427_montala_nodes_phase_3@8126 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- upgrade/migrate_to_nodes.php
+++ upgrade/migrate_to_nodes.php
@@ -25,7 +25,7 @@ foreach($resource_type_fields as $resource_type_field)
     }
 
 // ---------------------------------------------------------------------------------------------------------------------
-// Step 2.  Migrate any missing resource data only when not already existing in resource_node table
+// Step 2.  Migrate any missing resource_data fixed fields only when not already existing in resource_node table
 // ---------------------------------------------------------------------------------------------------------------------
 
 $resource_type_fields=sql_query('SELECT `ref`,`name` FROM `resource_type_field` WHERE `type` IN (' .
@@ -38,10 +38,12 @@ foreach($resource_type_fields as $resource_type_field)
     $out.=' (' . count($resource_data_entries) . ' rows found)';
     echo str_pad($out,100,' ');
     ob_flush();
-    $sql="INSERT INTO `resource_node`(`resource`,`node`)
+    $sql="INSERT INTO `resource_node`(`resource`,`node`,`hit_count`,`new_hit_count`)
       SELECT
         `resource_data`.`resource`,
-        `node`.`ref`
+        `node`.`ref`,
+        max(`resource_keyword`.`hit_count`),
+        max(`resource_keyword`.`new_hit_count`)
       FROM
         `resource_data`
       JOIN
@@ -50,8 +52,19 @@ foreach($resource_type_fields as $resource_type_field)
         NOT EXISTS(SELECT * FROM `resource_node` WHERE `resource`=`resource_data`.`resource` AND `node`=`node`.`ref`) AND
         `node`.`resource_type_field`={$resource_type_field['ref']} AND
         `resource_data`.`value` REGEXP CONCAT('[\\\^\\\|\\\;,]+', `node`.`name`, '[\\\$\\\|\\\;,]*')
+      LEFT OUTER JOIN
+        `keyword`
+      ON
+        `keyword`.`keyword`=`node`.`name`
+      LEFT OUTER JOIN
+        `resource_keyword`
+      ON
+        `resource_keyword`.`keyword`=`keyword`.`ref` AND `resource_keyword`.`resource`=`resource_data`.`resource`
       WHERE
-        `resource_data`.`resource_type_field`={$resource_type_field['ref']}";
+        `resource_data`.`resource_type_field`={$resource_type_field['ref']}
+      GROUP BY
+        `resource_data`.`resource`,
+        `node`.`ref`";
     sql_query($sql);
     echo mysqli_affected_rows($db);
     echo " rows inserted." . PHP_EOL;
