c0def3fdbcde347db466351f542567e7c249e40c
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-06-07 09:08:26 +0000

    FIX: Newer version of tooltip JS for analytics, fixes styling for tooltip
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8298 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/team/ajax/graph.php
+++ pages/team/ajax/graph.php
@@ -178,15 +178,15 @@ else
 	legend: {
         show: false
 	},
-	tooltip: true,
-	tooltipOpts: {
-	    content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
-	    shifts: {
-		x: 20,
-		y: 0
-	    },
-	    defaultTheme: false
-	}
+    tooltip: {
+        show: true,
+        content: '%p.0%, %s',
+        shifts: {
+            x: 20,
+            y: 0
+        }
+    }
+
     });
     });
 
@@ -241,15 +241,15 @@ else
 	legend: {
         show: false
 	},
-	tooltip: true,
-	tooltipOpts: {
-	    content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
-	    shifts: {
-		x: 20,
-		y: 0
-	    },
-	    defaultTheme: false
-	}
+    tooltip: {
+        show: true,
+        content: '%p.0%, %s',
+        shifts: {
+            x: 20,
+            y: 0
+        }
+    }
+
     });
     });
 
