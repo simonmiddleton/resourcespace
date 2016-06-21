4cb00b823b156d21980d32fd8d3da9550a7339a1
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-06-21 13:15:54 +0000

    FIX: Removed variable that was not needed and causing a crash
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8416 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/view_alternative_files.php
+++ pages/view_alternative_files.php
@@ -85,7 +85,7 @@ if ($alt_access)
 		<td class="DownloadButton"><?php
 			if ($request_adds_to_collection && ($k=="" || $internal_share_access) && !checkperm('b')) // We can't add to a collection if we are accessing an external share, unless we are a logged in user
 				{
-				echo add_to_collection_link($ref,$search,"alert('" . addslashes($lang["requestaddedtocollection"]) . "');",$size_info["id"]);
+				echo add_to_collection_link($ref,$search,"alert('" . addslashes($lang["requestaddedtocollection"]) . "');");
 				}
 			else
 				{
