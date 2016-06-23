simonmiddleton/resourcespace

commit a8e2744673c664a63cd2fea58e5ba100c54cf2da

NEW: Added option $collection_block_restypes. Resource types specified in this array cannot be added to a collection. Will not affect existing resources in collections
git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8418 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/collections_functions.php
@@ -142,11 +142,24 @@ function get_collection_resources($collection)
 	return sql_array("select resource value from collection_resource where collection='$collection' order by sortorder asc, date_added desc, resource desc"); 
 	}
 	
-function add_resource_to_collection($resource,$collection,$smartadd=false,$size="")
+function add_resource_to_collection($resource,$collection,$smartadd=false,$size="",$addtype="")
 	{
-	global $collection_allow_not_approved_share;
-	if (collection_writeable($collection)||$smartadd)
-		{	
+	global $collection_allow_not_approved_share, $collection_block_restypes;	
+	$addpermitted=collection_writeable($collection) && !$smartadd;
+	if ($addpermitted &&(count($collection_block_restypes)>0))
+		{
+		if($addtype=="")
+			{
+			$addtype=sql_value("select resource_type value from resource where ref='" . $resource . "'",0);
+			}
+		if(in_array($addtype,$collection_block_restypes))
+			{
+			$addpermitted=false;
+			}
+		}
+		
+	if ($addpermitted)	
+		{
 		# Check if this collection has already been shared externally. If it has, we must fail if not permitted or add a further entry
 		# for this specific resource, and warn the user that this has happened.
 		$keys=get_collection_external_access($collection);
@@ -1300,6 +1313,7 @@ function get_search_title($searchstring){
 
 function add_saved_search_items($collection)
 	{
+	global $collection_share_warning, $collection_allow_not_approved_share, $userref, $collection_block_restypes;
 	# Adds resources from a search to the collection.
 	$results=do_search(getvalescaped("addsearch",""), getvalescaped("restypes",""), "relevance", getvalescaped("archive","",true),-1,'',false,getvalescaped("starsearch",""),false,false,getvalescaped("daylimit",""));
 
@@ -1307,39 +1321,50 @@ function add_saved_search_items($collection)
 	# for this specific resource, and warn the user that this has happened.
 	$keys=get_collection_external_access($collection);
 	$resourcesnotadded=array(); # record the resources that are not added so we can display to the user
-	if (count($keys)>0)
+	$blockedtypes=array();# Record the resource types that are not added 
+	
+	for ($r=0;$r<count($results);$r++)
 		{
-		# Set the flag so a warning appears.
-		global $collection_share_warning, $collection_allow_not_approved_share;
-		$collection_share_warning=true;
+		$resource=$results[$r]["ref"];
+		$archivestatus=$results[$r]["archive"];
 		
-		for ($n=0;$n<count($keys);$n++)
+		if(in_array($results[$r]["resource_type"],$collection_block_restypes))
 			{
-			# Insert a new access key entry for this resource/collection.
-			global $userref;
-			
-			for ($r=0;$r<count($results);$r++)
+			$blockedtypes[] = $results[$r]["resource_type"];
+			continue;
+			}
+
+		if (count($keys)>0)
+			{			
+			if ($archivestatus<0 && !$collection_allow_not_approved_share)
 				{
-				$resource=$results[$r]["ref"];
-				$archivestatus=$results[$r]["archive"];
-				if ($archivestatus<0 && !$collection_allow_not_approved_share) {$resourcesnotadded[$resource] = $results[$r];continue;}
+				$resourcesnotadded[$resource] = $results[$r];
+				continue;
+				}
+			for ($n=0;$n<count($keys);$n++)
+				{
+				# Insert a new access key entry for this resource/collection.
 				sql_query("insert into external_access_keys(resource,access_key,user,collection,date) values ('$resource','" . escape_check($keys[$n]["access_key"]) . "','$userref','$collection',now())");
 				#log this
-				collection_log($collection,"s",$resource, $keys[$n]["access_key"]);
+				collection_log($collection,"s",$resource, $keys[$n]["access_key"]);	
+				
+				# Set the flag so a warning appears.
+				$collection_share_warning=true;	
 				}
 			}
-		}
-
+		}	
+		
+		
 	if (is_array($results))
-		{
+		{		
 		$modifyNotAdded = hook('modifynotaddedsearchitems', '', array($results, $resourcesnotadded));
 		if (is_array($modifyNotAdded))
 			$resourcesnotadded = $modifyNotAdded;
 
 		for ($n=0;$n<count($results);$n++)
 			{
-            $resource=$results[$n]["ref"];
-			if (!isset($resourcesnotadded[$resource]))
+			$resource=$results[$n]["ref"];
+			if (!isset($resourcesnotadded[$resource]) && !in_array($results[$n]["resource_type"],$collection_block_restypes))
 				{
 				sql_query("delete from collection_resource where resource='$resource' and collection='$collection'");
 				sql_query("insert into collection_resource(resource,collection) values ('$resource','$collection')");
@@ -1347,7 +1372,7 @@ function add_saved_search_items($collection)
 			}
 		}
 
-	if (!empty($resourcesnotadded))
+	if (!empty($resourcesnotadded) || count($blockedtypes)>0)
 		{
 		# Translate to titles only for displaying them to the user
 		global $view_title_field;
@@ -1356,7 +1381,16 @@ function add_saved_search_items($collection)
 			{
 			$titles[] = i18n_get_translated($resource['field' . $view_title_field]);
 			}
+		if(count($blockedtypes)>0)
+			{
+			$blocked_restypes=array_unique($blockedtypes);
+			// Return a list of blocked resouce types
+			$titles["blockedtypes"]=$blocked_restypes;
+			}
 		return $titles;
+		}
+	if(count($blockedtypes)>0)
+		{
 		}
 	return array();
 	}

include/config.default.php
@@ -3040,3 +3040,6 @@ $collection_bar_hide_empty=false;
 # Should the alternative files be visible to restricted users (they must still request access to download however)
 $alt_files_visible_when_restricted=true;
 
+# Option to prevent resource types specified in array from being added to collections. Will not affect existing resources in collections
+# e.g. $collection_block_restypes=array(3,4);
+$collection_block_restypes=array();

languages/en.php
@@ -2502,3 +2502,4 @@ $lang["report_external_option1"]="...are combined with the user group of the sha
 $lang["report_external_option2"]="...are reported as a separate user group";
 $lang["report_external_share"]="External share";
 $lang["report_add_to_dash"]="Add to dash";
+$lang["collection_restype_blocked"]="The following resource types cannot be added";

lib/js/global.js
@@ -921,11 +921,15 @@ function isJson(str) {
     return true;
 }
 
-function styledalert(title,text){
+function styledalert(title,text,minwidth){
+ if(typeof minwidth === 'undefined') {
+        minwidth = 300;
+    }
     jQuery("#modal_dialog").html(text);
     jQuery("#modal_dialog").dialog({
         title: title,
         resizable: false,
+	minWidth: minwidth,
         dialogClass: 'no-close',
          buttons: [{
                   text: oktext,

pages/collections.php
@@ -463,7 +463,31 @@ if ($addsearch!=-1)
             $resourcesnotadded=add_saved_search_items($usercollection);
             if (!empty($resourcesnotadded))
                 {
-                ?><script language="Javascript">alert("<?php echo $lang["notapprovedresources"] . implode(", ",$resourcesnotadded);?>");</script><?php
+		$warningtext="";
+		//exit($resourcesnotadded["blockedtypes"]);
+		if(isset($resourcesnotadded["blockedtypes"]))
+			{
+			// There are resource types blocked due to $collection_block_restypes
+			$warningtext = $lang["collection_restype_blocked"] . "<br /><br />";
+			//$restypes=get_resource_types(implode(",",$collection_block_restypes));
+			$blocked_types=get_resource_types(implode(",",$resourcesnotadded["blockedtypes"]));
+			foreach($blocked_types as $blocked_type)
+				{
+				if($warningtext==""){$warningtext.="<ul>";}
+				$warningtext.= "<li>" . $blocked_type["name"] . "</li>";
+				}
+			$warningtext.="</ul>";
+			unset($resourcesnotadded["blockedtypes"]);
+			}
+			
+		if (!empty($resourcesnotadded))	
+			{
+			// There are resources blocked from being added due to archive state
+			if($warningtext==""){$warningtext.="<br /><br />";}
+			$warningtext .= $lang["notapprovedresources"] . implode(", ",$resourcesnotadded);
+			}
+		
+                ?><script language="Javascript">styledalert("<?php echo $lang["status-warning"]; ?>","<?php echo $warningtext; ?>",600);</script><?php
                 }
             # Log this
             daily_stat("Add saved search items to collection",0);

pages/edit.php
@@ -302,7 +302,7 @@ if ((getval("autosave","")!="") || (getval("tweak","")=="" && getval("submitted"
                 $ref=copy_resource(0-$userref);
                 if($collection_add!="")
                     {
-                    add_resource_to_collection($ref, $collection_add);
+                    add_resource_to_collection($ref, $collection_add,false,"",$resource_type);
                     set_user_collection($userref, $collection_add);
                     }
                 redirect($baseurl_short."pages/view.php?ref=". urlencode($ref) . '&refreshcollectionframe=true');

pages/preview.php
@@ -151,7 +151,7 @@ include "../include/header.php";
 <?php } /*end hook replacepreviewbacktoview*/ ?>
 <?php if ($k=="") { ?>
 
-<?php if (!checkperm("b")) { ?>
+<?php if (!checkperm("b") && !(($userrequestmode==2 || $userrequestmode==3) && $basket_stores_size) && !in_array($resource["resource_type"],$collection_block_restypes)) { ?>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <?php echo add_to_collection_link(htmlspecialchars($ref),htmlspecialchars($search))?><i class="fa fa-plus-circle"></i>&nbsp;<?php echo $lang["action-addtocollection"]?></a><?php } ?>
 <?php if ($search=="!collection" . $usercollection) { ?>&nbsp;&nbsp;<?php echo remove_from_collection_link(htmlspecialchars($ref),htmlspecialchars($search))?><i class="fa fa-minus-circle"></i>&nbsp;<?php echo $lang["action-removefromcollection"]?></a><?php } ?>

pages/resource_email.php
@@ -61,7 +61,7 @@ if (getval("save","")!="")
 		$allow_changes=(getval("allow_changes","")!=""?1:0);
 		$sharedcollection=create_collection($userref,i18n_get_translated($resource["field".$view_title_field]) . " Share " . nicedate(date("Y-m-d H:i:s")),$allow_changes);
 		
-		add_resource_to_collection($ref,$sharedcollection);
+		add_resource_to_collection($ref,$sharedcollection,false,"",$resource["resource_type"]);
 		if($sharing_related)
 			{
 			foreach($relatedshares as $relatedshare)

commit 0c1cc567ee2cae14280a32d5f481adc0bbd68e8f

FIX: copy_collection broken by r8418
git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8431 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/collections_functions.php
@@ -145,7 +145,7 @@ function get_collection_resources($collection)
 function add_resource_to_collection($resource,$collection,$smartadd=false,$size="",$addtype="")
 	{
 	global $collection_allow_not_approved_share, $collection_block_restypes;	
-	$addpermitted=collection_writeable($collection) && !$smartadd;
+	$addpermitted=collection_writeable($collection) || $smartadd;
 	if ($addpermitted &&(count($collection_block_restypes)>0))
 		{
 		if($addtype=="")