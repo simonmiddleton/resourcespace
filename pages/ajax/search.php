20fe79d3028010d517e2be24c85997c265358a85
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-06-28 12:52:31 +0000

    NEW: ability to break a search string into search tags / pills
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160427_montala_nodes_phase_3@8444 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/ajax/search.php
+++ pages/ajax/search.php
@@ -10,20 +10,60 @@ if(!$ajax)
     die('AJAX only accepted!');
     }
 
+
 /* Variables that should be available for any cases below, otherwise 
    they should be put in that use case only */
 $return        = array();
 $search_string = getvalescaped('search_string', '');
 
+
+
+// Generate search tags based on a search string
 if(filter_var(getval('generate_tags', false), FILTER_VALIDATE_BOOLEAN))
     {
+    /*
+    Space is not part of config separators so we have to make sure we have it for this case
+    Double space is used due to removal of quoted search strings which can lead to double spaces left
+    in the search string
+    */
+    $tag_delimiters = array_merge(array(' ', '  '), $config_separators);
+    // echo "{$search_string}<br>";
+
     // Quoted search detected, so anything within double quotes should allow for white spaces
-    if(false !== strpos($search_string, '"'))
+    $double_quotes_pos = strpos($search_string, '"');
+    if(false !== $double_quotes_pos)
         {
-        // 
+        $double_quotes_end_pos = strpos(substr($search_string, $double_quotes_pos + 1), '"');
+
+        $quoted_text   = substr($search_string, $double_quotes_pos + 1, $double_quotes_end_pos);
+        $search_string = str_replace("\"{$quoted_text}\"", '', $search_string);
+
+        $return[] = $quoted_text;
         }
 
-// $config_separators
+    $is_special_search = (false !== strpos($search_string, ':') ? true : false);
+    if($is_special_search || false !== strpos($search_string, ','))
+        {
+        foreach(explode(',', $search_string) as $comma_split_keywords)
+            {
+            // Special search found, add that to return
+            if(false !== strpos($comma_split_keywords, ':'))
+                {
+                $return[] = cleanse_string($comma_split_keywords, true);
+                continue;
+                }
+
+            $return = array_merge($return, split_keywords($comma_split_keywords));
+            }
+        }
+
+    if(!$is_special_search)
+        {
+        $return = array_merge($return, split_keywords($search_string));
+        }
+
+    // echo '<pre>';print_r($return);echo '</pre>';die('<br>You died in ' . __FILE__ . ' @' . __LINE__);
+
     echo json_encode($return);
     exit();
     }
