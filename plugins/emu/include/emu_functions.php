27b95f7b31c8f8f75b3a9327e632a64e3bc3b2e5
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-07-11 13:02:50 +0000

    NEW: function to get all resources that will need to be updated
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160704_acota_emu@8521 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/emu/include/emu_functions.php
+++ plugins/emu/include/emu_functions.php
@@ -1,4 +1,11 @@
 <?php
+/**
+* Check if script last ran more than the failure notification days
+* 
+* @param string $emu_scrip_last_ran Datetime (string format) when script was last run
+* 
+* @return boolean
+*/
 function check_script_last_ran(&$emu_script_last_ran = '')
     {
     global $lang, $emu_script_failure_notify_days;
@@ -16,4 +23,30 @@ function check_script_last_ran(&$emu_script_last_ran = '')
         }
 
     return false;
+    }
+
+
+/**
+* Function to retrieve all resources that have their IRN field set to a value
+* and that are within the allowed resource types for an EMu update
+* 
+* @return array
+*/
+function get_emu_resources()
+    {
+    global $emu_irn_field, $emu_resource_types;
+
+    $resource_types_list = '\'' . implode('\', \'', $emu_resource_types) . '\'';
+
+    $emu_resources = sql_query("
+            SELECT rd.resource AS resource,
+                   rd.value AS object_irn
+              FROM resource_data AS rd
+        RIGHT JOIN resource AS r ON rd.resource = r.ref AND r.resource_type IN ({$resource_types_list})
+             WHERE rd.resource > 0
+               AND rd.resource_type_field = '{$emu_irn_field}'
+          ORDER BY rd.resource;
+    ");
+
+    return $emu_resources;
     }
