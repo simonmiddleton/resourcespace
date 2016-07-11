29e6f2557efe5879ee9efe549a26c62f7f068d9f
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-07-11 10:58:13 +0000

    NEW: check process locks on emu_script
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160704_acota_emu@8519 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/emu/pages/emu_script.php
+++ plugins/emu/pages/emu_script.php
@@ -1,5 +1,7 @@
 <?php
-if('cli' != php_sapi_name())
+$php_sapi_name = php_sapi_name();
+
+if('cli' != $php_sapi_name)
     {
     header('HTTP/1.1 401 Unauthorized');
     exit('Access denied');
@@ -27,9 +29,46 @@ if('' != $emu_email_notify)
 $emu_script_last_ran = '';
 if(!check_script_last_ran($emu_script_last_ran))
     {
-    $emu_script_failed_subject = ($tms_link_test_mode ? 'TESTING MODE ' : '') . 'EMu Import script - WARNING';
+    $emu_script_failed_subject = ($emu_test_mode ? 'TESTING MODE: ' : '') . 'EMu Import script - WARNING';
     send_mail($email_notify, $emu_script_failed_subject, "WARNING: The EMu Import Script has not completed since '{$emu_script_last_ran}'.\r\n\r\nYou can safely ignore this warning only if you subsequently received notification of a successful script completion.", $email_from);
     }
+
+if('cli' == $php_sapi_name && 2 == $argc)
+    {
+    if(in_array($argv[1], array('--help', '-help', '-h', '-?')))
+        {
+        echo 'To clear the lock after a failed run, pass in "--clearlock", "-clearlock", "-c" or "--c"' . PHP_EOL;
+        exit('Bye!');
+        }
+    else if(in_array($argv[1], array('--clearlock', '-clearlock', '-c', '--c')))
+        {
+        if(is_process_lock('emu_import'))
+            {
+            clear_process_lock('emu_import');
+            }
+        }
+    else
+        {
+        exit("Unknown argv: {$argv[1]}" . PHP_EOL);
+        }
+    }
+
+# Check for a process lock
+if(is_process_lock('emu_import')) 
+    {
+    echo 'EMu script lock is in place. Deferring.' . PHP_EOL . 'To clear the lock after a failed run use --clearlock flag.' . PHP_EOL;
+
+    $emu_script_failed_subject = ($emu_test_mode ? 'TESTING MODE: ' : '') . 'EMu Import script - FAILED';
+    send_mail($email_notify, $emu_script_failed_subject, "The EMu script failed to run because a process lock was in place. This indicates that the previous run did not complete.\r\n\r\nIf you need to clear the lock after a failed run, run the script as follows:\r\n\r\nphp emu_script.php --clearlock\r\n", $email_from);
+    exit();
+    }
+set_process_lock('emu_import');
+
+
+
+
+
+
 
 
 
