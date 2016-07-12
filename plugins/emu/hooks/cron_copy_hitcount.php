357be08496563593779d7e756c4387e5d30be535
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-07-12 16:12:34 +0000

    FIX: EMu script should run either every time cron jobs are running or at the interval specified by Admins
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160704_acota_emu@8543 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/emu/hooks/cron_copy_hitcount.php
+++ plugins/emu/hooks/cron_copy_hitcount.php
@@ -1,11 +1,37 @@
 <?php
+include_once dirname(__FILE__) . '/../include/emu_functions.php';
+
 function HookEmuCron_copy_hitcountAddplugincronjob()
     {
-    global $php_path, $config_windows, $emu_enable_script;
+    global $lang, $php_path, $config_windows, $emu_enable_script, $emu_interval_run;
 
     if(!$emu_enable_script)
         {
         return false;
+        }
+
+    // Make sure we run this at an interval specified by Admins, otherwise run this every time cron jobs run
+    if('' != $emu_interval_run)
+        {
+        $emu_script_last_ran        = '';
+        $check_script_last_ran      = check_script_last_ran($emu_script_last_ran);
+        $emu_script_future_run_date = new DateTime();
+
+        // Use last date the script was run, if available
+        if($check_script_last_ran || (!$check_script_last_ran && $lang['status-never'] != $emu_script_last_ran))
+            {
+            $emu_script_future_run_date = DateTime::createFromFormat('l F jS Y @ H:m:s', $emu_script_last_ran);
+            }
+
+        $emu_script_future_run_date->modify($emu_interval_run);
+
+        // Calculate difference between dates and establish whether it should run or not
+        $date_diff = $emu_script_future_run_date->diff(new DateTime());
+
+        if(0 < $date_diff->days)
+            {
+            return false;
+            }
         }
 
     if(isset($php_path) && (file_exists("{$php_path}/php") || ($config_windows && file_exists("{$php_path}/php.exe"))))
