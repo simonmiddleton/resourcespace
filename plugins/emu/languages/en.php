6b18945a47ddcc9af9875f3ed0629708f9c5f3f5
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-07-12 16:16:01 +0000

    FIX: examples given for setting up
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160704_acota_emu@8545 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/emu/languages/en.php
+++ plugins/emu/languages/en.php
@@ -13,7 +13,7 @@ $lang['emu_script_header'] = 'Enable script that will automatically update the E
 $lang['emu_last_run_date'] = '<div class="Question"><label><strong>Script last run</strong></label><input name="script_last_ran" type="text" value="%script_last_ran%" disabled style="width: 300px;"></div><div class="clearerleft"></div>';
 $lang['emu_enable_script'] = 'Enable EMu script';
 $lang['emu_test_mode'] = 'Test mode - Set to true and script will run but not update resources';
-$lang['emu_interval_run'] = 'Run script at the following interval (e.g. 1 day, 2 weeks, fortnight). Leave blank and it will run everytime cron_copy_hitcount.php runs)';
+$lang['emu_interval_run'] = 'Run script at the following interval (e.g. +1 day, +2 weeks, fortnight). Leave blank and it will run everytime cron_copy_hitcount.php runs)';
 $lang['emu_log_directory'] = 'Directory to store script logs in. If this is left blank or is invalid then no logging will occur.';
 
 $lang['emu_settings_header'] = 'EMu settings';
