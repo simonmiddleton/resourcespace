6f61f5b389c024b267848540f058cd8b7a718dd8
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-07-20 13:16:56 +0000

    CHG: Replacing Text_Diff with Diff (by Stephen Morley) due to being superseded (now Horde_Text_Diff) and not supporting PHP 7. Diff class does the same thing and has a much more smaller foot print. FIX: Code clean-up as per ResourceSpace coding standards (http://wiki.resourcespace.org/index.php/Coding_Standards)
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8574 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -3075,84 +3075,62 @@ function filter_match($filter,$name,$value)
 		}
 	return 0;
 	}
-	
-function log_diff($fromvalue,$tovalue)	
-	{
-	# Forumlate descriptive text to describe the change made to a metadata field.
-
-	# Remove any database escaping
-	$fromvalue=str_replace("\\","",$fromvalue);
-	$tovalue=str_replace("\\","",$tovalue);
-	
-	if (substr($fromvalue,0,1)==",")
-		{
-		# Work a different way for checkbox lists.
-		$fromvalue=explode(",",i18n_get_translated($fromvalue));
-		$tovalue=explode(",",i18n_get_translated($tovalue));
-		
-		# Get diffs
-		$inserts=array_diff($tovalue,$fromvalue);
-		$deletes=array_diff($fromvalue,$tovalue);
-
-		# Process array diffs into meaningful strings.
-		$return="";
-		if (count($deletes)>0)
-			{
-			$return.="- " . join("\n- " , $deletes);
-			}
-		if (count($inserts)>0)
-			{
-			if ($return!="") {$return.="\n";}
-			$return.="+ " . join("\n+ ", $inserts);
-			}
-		
-		#debug($return);
-		return $return;
-		}
-
-	# For standard strings, use Text_Diff
-		
-	require_once dirname(__FILE__).'/../lib/Text_Diff/Diff.php';
-	require_once dirname(__FILE__).'/../lib/Text_Diff/Diff/Renderer/inline.php';
-
-	$lines1 = explode("\n",$fromvalue);
-	$lines2 = explode("\n",$tovalue);
-
-	$diff     = new Text_Diff('native', array($lines1, $lines2));
-	$renderer = new Text_Diff_Renderer_inline();
-	$diff=$renderer->render($diff);
-	
-	$return="";
-
-	# The inline diff syntax places inserts within <ins></ins> tags and deletes within <del></del> tags.
-
-	# Handle deletes
-	if (strpos($diff,"<del>")!==false)
-		{
-		$s=explode("<del>",$diff);
-		for ($n=1;$n<count($s);$n++)
-			{
-			$t=explode("</del>",$s[$n]);
-			if ($return!="") {$return.="\n";}
-			$return.="- " . trim(i18n_get_translated($t[0]));
-			}
-		}
-	# Handle inserts
-	if (strpos($diff,"<ins>")!==false)
-		{
-		$s=explode("<ins>",$diff);
-		for ($n=1;$n<count($s);$n++)
-			{
-			$t=explode("</ins>",$s[$n]);
-			if ($return!="") {$return.="\n";}
-			$return.="+ " . trim(i18n_get_translated($t[0]));
-			}
-		}
 
 
-	#debug ($return);
-	return $return;
-	}
+/**
+* Check changes made to a metadata field and create a nice user friendly summary
+* 
+* @uses Diff::compare()
+* @uses Diff::toString()
+* 
+* @param string $fromvalue
+* @param string $tovalue
+* 
+* @return string
+*/
+function log_diff($fromvalue, $tovalue)
+    {
+    $return = '';
+
+    // Remove any database escaping
+    $fromvalue = str_replace("\\", '', $fromvalue);
+    $tovalue   = str_replace("\\", '', $tovalue);
+
+    // Work a different way for fixed lists
+    if(',' == substr($fromvalue, 0, 1))
+        {
+        $fromvalue = explode(',', i18n_get_translated($fromvalue));
+        $tovalue   = explode(',', i18n_get_translated($tovalue));
+
+        // Get diffs
+        $inserts = array_diff($tovalue, $fromvalue);
+        $deletes = array_diff($fromvalue, $tovalue);
+
+        // Process array diffs into meaningful strings
+        if(0 < count($deletes))
+            {
+            $return .= '- ' . join("\n- " , $deletes);
+            }
+
+        if(0 < count($inserts))
+            {
+            if('' != $return)
+                {
+                $return .= "\n";
+                }
+
+            $return .= '+ ' . join("\n+ ", $inserts);
+            }
+
+        return $return;
+        }
+
+    // For standard strings, use Diff library
+    require_once dirname(__FILE__) . '/../lib/Diff/class.Diff.php';
+    $return = Diff::toString(Diff::compare($fromvalue, $tovalue));
+
+    return $return;
+    }
 	
 function update_xml_metadump($resource)
 	{
