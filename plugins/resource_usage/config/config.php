9ca595bb0995c6f457ae9e68b393bb8c692fd6c8
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-09-07 14:08:29 +0000

    NEW: Added resource_usage plugin developed for MS Society
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8725 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/resource_usage/config/config.php
@@ -0,0 +1,13 @@
+<?php
+$resource_usage_mediums = array(
+'Press',
+'Social media',
+'Direct Marketing',
+'Website',
+'Campaigns',
+'Corporates or Trusts',
+'Internal Comms',
+'Info resources',
+'AV',
+'Other'
+);
