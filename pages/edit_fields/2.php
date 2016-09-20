c15376dadf82dd2b0867cd8d495a7c751db129e8
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-09-20 15:11:19 +0000

    FIX: some code cleaning based on RS coding standards
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160427_montala_nodes_phase_3@8779 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/2.php
+++ pages/edit_fields/2.php
@@ -1,36 +1,43 @@
-<?php /* -------- Check box list ------------------ */ 
-
-if(!hook("customchkboxes","",array($field))):
+<?php
+/* -------- Check box list ------------------ */ 
+if(!hook("customchkboxes", "", array($field))):
 
 # Translate all options
+$modified_options = hook("modify_field_options", "", array($field));
+if($modified_options != "")
+    {
+    $field['node_options'] = $modified_options;
+    }
 
-$modified_options=hook("modify_field_options","",array($field));
-if($modified_options!=""){$field['node_options']=$modified_options;}
+$option_trans        = array();
+$option_trans_simple = array();
 
-$option_trans=array();
-$option_trans_simple=array();
-for ($m=0;$m<count($field['node_options']);$m++)
-	{
-	$trans=i18n_get_translated($field['node_options'][$m]);
-	if ($trans!=""){
-		$option_trans[$field['node_options'][$m]]=$trans;
-		$option_trans_simple[]=$trans;
-	}
-	}
+for ($m = 0; $m < count($field['node_options']); $m++)
+    {
+    $trans = i18n_get_translated($field['node_options'][$m]);
 
-if ($auto_order_checkbox && !hook("ajust_auto_order_checkbox","",array($field))) {
-	if($auto_order_checkbox_case_insensitive){natcasesort($option_trans);}
-	else{natsort($option_trans);}
-}
-$field['node_options']=array_keys($option_trans); # Set the options array to the keys, so it is now effectively sorted by translated string	
-$field['node_options']=array_diff($field['node_options'], array(''));
-//$set=trim_array(explode(",",$value));
-$set=array_unique(preg_split('/,|\~\w+\:/',$value));        // this will remove language variants such as "~en:my option in english"
-$wrap=0;
+    if($trans!="")
+        {
+        $option_trans[$field['node_options'][$m]]=$trans;
+        $option_trans_simple[]=$trans;
+        }
+    }
+
+if($auto_order_checkbox && !hook("ajust_auto_order_checkbox","",array($field)))
+    {
+    if($auto_order_checkbox_case_insensitive){natcasesort($option_trans);}
+    else{natsort($option_trans);}
+    }
+
+$field['node_options'] = array_keys($option_trans); # Set the options array to the keys, so it is now effectively sorted by translated string	
+$field['node_options'] = array_diff($field['node_options'], array(''));
+
+$set  = array_unique(preg_split('/,|\~\w+\:/',$value));        // this will remove language variants such as "~en:my option in english"
+$wrap = 0;
 
 # Work out an appropriate number of columns based on the average length of the options.
-$l=average_length($option_trans_simple);
-switch ($l)
+$l = average_length($option_trans_simple);
+switch($l)
 	{
 	case ($l>40): 	$cols=1; break;	
 	case ($l>25): 	$cols=2; break;
@@ -40,9 +47,11 @@ switch ($l)
 	default: 	$cols=10;
 	}
 
-$height=ceil(count($field['node_options'])/$cols);
+$height = ceil(count($field['node_options']) / $cols);
 
-if ($edit_autosave) { ?>
+if($edit_autosave)
+    {
+    ?>
 	<script type="text/javascript">
 		// Function to allow checkboxes to save automatically when $edit_autosave from config is set: 
 		function checkbox_allow_save() {
@@ -53,7 +62,8 @@ if ($edit_autosave) { ?>
 		    }, 500);
 		}
 	</script>
-<?php }
+    <?php
+    }
 
 array_filter($field['node_options']);
 array_filter($option_trans);
