348d8a1df82a71f22d6750793c86569b452dcd6e
aantonopoulos <aantonopoulos@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-10-04 10:36:57 +0000

    NEW: Basic google analytics plugin to track pageviews
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8878 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/google_analytics/pages/setup.php
@@ -0,0 +1,26 @@
+<?php
+#
+# Google Analytics Key setup page, requires System Setup permission
+#
+
+// Do the include and authorization checking ritual.
+include '../../../include/db.php';
+include '../../../include/general.php';
+include '../../../include/authenticate.php'; if (!checkperm('a')) {exit ($lang['error-permissiondenied']);}
+
+// Specify the name of this plugin, the heading to display for the page.
+$plugin_name = 'google_analytics';
+$page_heading = 'Google Analytics Key Configuration';
+
+// Build the $page_def array of descriptions of each configuration variable the plugin uses.
+$page_def[] = config_add_text_list_input('google_analytics_key','Google Analytics Key');
+
+
+// Do the page generation ritual
+$upload_status = config_gen_setup_post($page_def, $plugin_name);
+include '../../../include/header.php';
+config_gen_setup_html($page_def, $plugin_name, $upload_status, $page_heading);
+include '../../../include/footer.php';
+
+
+
