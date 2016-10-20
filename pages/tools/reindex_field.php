f474f2e38b8c3ffebcd31e95e2d1b995b16ef7f3
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-10-20 09:11:11 +0000

    FIX: Updated reindex_field.php to perform the reindex in chunks and accept a starting resource id by adding '&startid=<ref>' to the URL
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@8953 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/reindex_field.php
+++ pages/tools/reindex_field.php
@@ -13,6 +13,7 @@ include "../../include/resource_functions.php";
 include "../../include/image_processing.php";
 
 set_time_limit(0);
+$reindex_chunk_size=100; // Number of resources to reindex in each batch to prevent wiping out the index.
 
 # Reindex a single field
 $field=getvalescaped("field","");
@@ -21,6 +22,9 @@ if ($field=="") {exit("Specify field with ?field=");}
 # Reindex only resources in specified collection
 $collectionid=getvalescaped("col", "");
 
+# Start reindex from a specific resource ID 
+$startid=getvalescaped("startid", "");
+
 # Fetch field info
 $fieldinfo=sql_query("select * from resource_type_field where ref='$field'");$fieldinfo=$fieldinfo[0];
 if (!$fieldinfo["keywords_index"]) {exit("Field is not set to be indexed.");}
@@ -28,62 +32,68 @@ if (!$fieldinfo["keywords_index"]) {exit("Field is not set to be indexed.");}
 if (getval("submit","")!="")
 	{
 	echo "<pre>";
-	
-	$joinkeyword="";
-	$joindata="";
-	$condition = "";
-	$conditionand = "";
-	if ($collectionid != "")
-			{
-			$joinkeyword=" inner join collection_resource on collection_resource.resource=resource_keyword.resource "; 
-			$joindata=" inner join collection_resource on collection_resource.resource=resource_data.resource "; 
-			$condition = "where collection_resource.collection = '$collectionid' ";
-			$conditionand = "and collection_resource.collection = '$collectionid' ";
-			}
-	
-	
-	# Delete existing keywords index for this field
-	sql_query("delete resource_keyword.* from resource_keyword $joinkeyword where resource_type_field='$field' $conditionand");
-	
-	# Index fields
-	$data=sql_query("select * from resource_data $joindata where resource_type_field='$field' and length(value)>0 $conditionand and value is not null");
-	$n=0;
-	$total=count($data);
-
-    db_begin_transaction();
-
-	foreach ($data as $row)
+	$resourcecount = 0;
+	$todo = $reindex_chunk_size;
+	while($todo>0)
 		{
-		$n++;
-		$ref=$row["resource"];
-		$value=$row["value"];
-	
-		if ($fieldinfo["type"]==3 || $fieldinfo["type"]==2 || $fieldinfo["type"]==9)
+		if ($collectionid != "")
 			{
-			# Prepend a comma when indexing dropdowns to ensure full value is also indexed
-			$value="," . $value;
+			$resources=sql_array("select resource value from collection_resource where collection_resource.collection = '" . $collectionid . "' order by resource asc limit " . $resourcecount . "," . $reindex_chunk_size);
 			}
-		
-		# Date field? These need indexing differently.
-		$is_date=($fieldinfo["type"]==4 || $fieldinfo["type"]==6);
-		
-		$is_html=($fieldinfo["type"]==8);	
-		
-	    # function add_keyword_mappings($ref,$string,$resource_type_field,$partial_index=false,$is_date=false)		
-		add_keyword_mappings($ref,i18n_get_indexable($value),$field,$fieldinfo["partial_index"],$is_date,'','',$is_html);		
-	
-		hook("reindexfieldtooladditional","",array($ref,$value,$fieldinfo));
-		
-		echo "Done $ref - " . htmlspecialchars(substr($value,0,50)) . "... ($n/$total)\n";
-		
-		if (($n / 20 == floor($n/20)) || $n==$total) # Scroll down every now and again, and at the end.
+		else
 			{
-			?><script>window.scroll(0,document.height);</script><?php
+			$resources=sql_array("select ref value from resource where ref >='" . $startid . "' order by ref asc limit " . $resourcecount . "," . $reindex_chunk_size);
 			}
-		flush();
-		}
+		$todo=count($resources);
+		if($todo>0)
+			{
+			# Delete existing keywords index for this field
+			sql_query("delete from resource_keyword where resource in (" . implode(",",$resources) . ") and resource_type_field='$field'");
+			
+			# Index data
+			$data=sql_query("select * from resource_data rd where resource in (" . implode(",",$resources) . ") and resource_type_field='$field' and length(rd.value)>0 and rd.value is not null order by rd.resource asc");
+			$n=0;
+			$total=count($data);
+			
+			db_begin_transaction();
 
-    db_end_transaction();
+			foreach ($data as $row)
+				{
+				$n++;
+				$ref=$row["resource"];
+				$value=$row["value"];
+			
+				if (in_array($fieldinfo['type'], $FIXED_LIST_FIELD_TYPES) && strpos(",",$value)!==FALSE)
+					{
+					# Prepend a comma when indexing dropdowns to ensure full value is also indexed
+					$value="," . $value;
+					}
+				
+				# Date field? These need indexing differently.
+				$is_date=($fieldinfo["type"]==4 || $fieldinfo["type"]==6);
+				
+				$is_html=($fieldinfo["type"]==8);	
+				
+				# function add_keyword_mappings($ref,$string,$resource_type_field,$partial_index=false,$is_date=false)		
+				add_keyword_mappings($ref,i18n_get_indexable($value),$field,$fieldinfo["partial_index"],$is_date,'','',$is_html);		
+			
+				hook("reindexfieldtooladditional","",array($ref,$value,$fieldinfo));
+				
+				echo "Done $ref - " . htmlspecialchars(substr($value,0,50)) . "... ($n/$total)\n";
+				
+				if (($n / 20 == floor($n/20)) || $n==$total) # Scroll down every now and again, and at the end.
+					{
+					?><script>window.scroll(0,document.height);</script><?php
+					}
+				flush();
+				}
+
+			db_end_transaction();
+			
+			$resourcecount = $resourcecount + $reindex_chunk_size;
+			}
+	
+		}
 
 	echo "Reindex complete\n\n\n";
 	}
@@ -95,10 +105,14 @@ else
 		$collectionname=sql_value("select name as value from collection where ref='$collectionid'",'');
 		$extratext=" for collection '" . $collectionname .  "'";
 		}
+	if ($startid != "")
+		{
+		$extratext=" for resource ID #" . $startid .  " and above";
+		}
 	?>
-	<form method="post" action="reindex_field.php">
-	<input type="hidden" name="field" value="<?php echo $field ?>">
+	<form method="post" action="reindex_field.php?field=<?php echo $field ?>">
 	<input type="hidden" name="col" value="<?php echo $collectionid ?>">
+	<input type="hidden" name="startid" value="<?php echo $startid ?>">
 	<input type="submit" name="submit" value="Reindex field '<?php echo $fieldinfo["title"] . "'" . $extratext ?>">
 	</form>
 	<?php
