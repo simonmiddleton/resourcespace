09807df9efdc0e97c10918a156aee1215c652a57
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-11-03 09:33:45 +0000

    FIX: Updated resource_node check script to ensure key always added
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160427_montala_nodes_phase_3@9001 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- upgrade/scripts/002_check_resource_node_table.php
+++ upgrade/scripts/002_check_resource_node_table.php
@@ -9,11 +9,9 @@ include_once __DIR__ . "/../../include/general.php";
 
 set_sysvar(SYSVAR_UPGRADE_PROGRESS_SCRIPT,"Checking primary key and removing duplicates from resource_node table");
 
-$dupes=sql_query("SELECT count(*) c FROM resource_node GROUP BY resource,node HAVING count(c)>1; ");
+sql_query("SET session old_alter_table=1");
+sql_query("ALTER IGNORE TABLE resource_node ADD PRIMARY KEY(resource,node)");
+    
+    
 
-if(count($dupes>0));
-	{
-	sql_query("SET session old_alter_table=1");
-	sql_query("ALTER IGNORE TABLE resource_node ADD PRIMARY KEY(resource,node)");
-	}
 
