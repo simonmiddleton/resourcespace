f31b0cd69d7301a0ea34ab182c0fe8a96ce2c8bc
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-11-17 08:47:35 +0000

    FIX: Manual merge of r8787 and r8791 from trunk
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20160427_montala_nodes_phase_3@9102 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -1375,42 +1375,33 @@ function update_field($resource, $field, $value, array $errors = array())
     # If this is a 'joined' field we need to add it to the resource column
     $joins = get_resource_table_joins();
 
-    if(in_array($fieldinfo['ref'],$joins))
-        {
-        if ($value!="null")
-            {
-            global $resource_field_column_limit;
-
-            $truncated_value = truncate_join_field_value($value);
-
+   if(in_array($fieldinfo['ref'],$joins))
+		{
+		if ($value!="null")
+			{
+			global $resource_field_column_limit;
+			$truncated_value = truncate_join_field_value($value);
             // Remove backslashes from the end of the truncated value
             if(substr($truncated_value, -1) === '\\')
                 {
                 $truncated_value = substr($truncated_value, 0, strlen($truncated_value) - 1);
-                }
-
-            if(substr($truncated_value, -1) !== '\'')
-                {
-                $truncated_value .= '\'';
-                }
-            }
-        else
-            {
-            $truncated_value="null";
-            }
-
-        sql_query("update resource set field".$field."=" . $truncated_value . " where ref='$resource'");
-        }
-
-    // Add any onchange code
+				}
+			}
+		else
+			{
+			$truncated_value="null";
+			}		
+		sql_query("update resource set field".$field."=" . (($value=="")?"NULL":"'" . $truncated_value . "'") ." where ref='$resource'");
+		}			
+	
+    # Add any onchange code
     if($fieldinfo["onchange_macro"]!="")
         {
         eval($fieldinfo["onchange_macro"]);    
         }
-
-    // Allow plugins to perform additional actions.
+    
+    # Allow plugins to perform additional actions.
     hook("update_field","",array($resource,$field,$value,$existing));
-
     return true;
     }
 
