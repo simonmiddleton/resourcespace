3886358031eac30e10cb9510900832a565b54bc1
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-11-18 13:41:43 +0000

    CHG: Upgrade script that removes unused keywords is a little more verbose
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9148 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- upgrade/scripts/003_flush_unused_keywords.php
+++ upgrade/scripts/003_flush_unused_keywords.php
@@ -4,5 +4,12 @@ include_once __DIR__ . "/../../include/db.php";
 // After migrating to nodes it is prefereable to delete resource_keyword rows for fixed list data
 // as this is now stored in normalised form in resource_node and node_keyword
 
-sql_query("delete from resource_keyword where resource_type_field in (select ref from resource_type_field where type in ('" . join("','",$FIXED_LIST_FIELD_TYPES) . "'))");
+$count_query="select count(*) value from resource_keyword where resource_type_field in (select ref from resource_type_field where type in ('" . join("','",$FIXED_LIST_FIELD_TYPES) . "')) ";
+$c=sql_value($count_query,0);
 
+while ($c>0)
+	{
+	sql_query("delete from resource_keyword where resource_type_field in (select ref from resource_type_field where type in ('" . join("','",$FIXED_LIST_FIELD_TYPES) . "')) limit 1000");
+	$c=sql_value($count_query,0);
+	set_sysvar(SYSVAR_UPGRADE_PROGRESS_SCRIPT,$c . " rows remaining to delete");
+	}
