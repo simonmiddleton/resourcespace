a590b55d9bad7bcd034ba4ea1abe22d6298ca461
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-11-18 15:40:05 +0000

    FIX: Error with last commit
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9155 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- upgrade/scripts/002_check_resource_node_table.php
+++ upgrade/scripts/002_check_resource_node_table.php
@@ -17,8 +17,8 @@ if(!in_array("resource",$rnkeys) || !in_array("node",$rnkeys))
     // Copy to a temporary table and then rename to remove duplicates. Alows use of INSERT  - ON DUPLICATE KEY syntax when inserting new nodes
     db_begin_transaction();
     sql_query("CREATE TABLE resource_node_deduped like resource_node");
-    sql_query("ALTER IGNORE TABLE resource_node_deduped ADD PRIMARY KEY(resource,node)");
-    sql_query("INSERT IGNORE resource_node_deduped SELECT * FROM resource_node");
+    sql_query("ALTER TABLE resource_node_deduped ADD PRIMARY KEY(resource,node)");
+    sql_query("INSERT INTO resource_node_deduped SELECT * FROM resource_node ON DUPLICATE KEY UPDATE resource_node_deduped.hit_count=resource_node.hit_count");
     sql_query("RENAME TABLE resource_node TO resource_node_with_dupes");
     sql_query("RENAME TABLE resource_node_deduped TO resource_node");
     sql_query("DROP TABLE resource_node_with_dupes");
