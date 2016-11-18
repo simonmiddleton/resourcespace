1a1b8bea224678ce8e2cc66604994a601b530a0a
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-11-18 15:23:28 +0000

    FIX: Replaced ALTER TABLE IGNORE from upgrade script as now unsupported. Replaced with dedupe script.
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9154 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- upgrade/scripts/002_check_resource_node_table.php
+++ upgrade/scripts/002_check_resource_node_table.php
@@ -9,9 +9,22 @@ include_once __DIR__ . "/../../include/general.php";
 
 set_sysvar(SYSVAR_UPGRADE_PROGRESS_SCRIPT,"Checking primary key and removing duplicates from resource_node table");
 
-sql_query("SET session old_alter_table=1");
-sql_query("ALTER IGNORE TABLE resource_node ADD PRIMARY KEY(resource,node)");
-    
+$rnindexes =sql_query("SHOW KEYS FROM resource_node WHERE Key_name = 'PRIMARY'");
+$rnkeys = array_column($rnindexes,"Column_name");
+
+if(!in_array("resource",$rnkeys) || !in_array("node",$rnkeys))    
+    {
+    // Copy to a temporary table and then rename to remove duplicates. Alows use of INSERT  - ON DUPLICATE KEY syntax when inserting new nodes
+    db_begin_transaction();
+    sql_query("CREATE TABLE resource_node_deduped like resource_node");
+    sql_query("ALTER IGNORE TABLE resource_node_deduped ADD PRIMARY KEY(resource,node)");
+    sql_query("INSERT IGNORE resource_node_deduped SELECT * FROM resource_node");
+    sql_query("RENAME TABLE resource_node TO resource_node_with_dupes");
+    sql_query("RENAME TABLE resource_node_deduped TO resource_node");
+    sql_query("DROP TABLE resource_node_with_dupes");
+    db_end_transaction();
+    }
+
     
 
 
