ec9f4ee4c7a103a39ef1cfa4d1dca311a89856be
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-12-06 14:55:06 +0000

    NEW: test for strip_tags_and_attributes()
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9273 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ tests/test_list/001000_strip_tags_and_attributes.php
@@ -0,0 +1,105 @@
+<?php
+if('cli' !== php_sapi_name())
+    {
+    exit('This utility is command line only.');
+    }
+
+// Case 1: 
+if(false !== strip_tags_and_attributes(''))
+    {
+    return false;
+    }
+
+
+// Case 2: 
+$html_input  = '<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>';
+$html_output = '';
+
+if($html_output !== strip_tags_and_attributes($html_input))
+    {
+    return false;
+    }
+
+
+// Case 3: 
+$html_input  = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+$html_output = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+
+if($html_output !== strip_tags_and_attributes($html_input))
+    {
+    return false;
+    }
+
+
+// Case 4: 
+$html_input  = '<p id="testID">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+$html_output = '<p id="testID">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+
+if($html_output !== strip_tags_and_attributes($html_input))
+    {
+    return false;
+    }
+
+
+// Case 5: 
+$html_input  = '<p id="testID" data-test="testData" onmousedown="this.style.color=\'#FF0000\';" onmouseup="this.style.color=\'#000000\';">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+$html_output = '<p id="testID">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+
+if($html_output !== strip_tags_and_attributes($html_input))
+    {
+    return false;
+    }
+
+
+// Case 5.1: 
+$html_input  = '<p id="testID" onmousedown="this.style.color=\'#FF0000\';" onmouseup="this.style.color=\'#000000\';">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<script>console.log(true);</script></p>';
+$html_output = '<p id="testID">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+
+if($html_output !== strip_tags_and_attributes($html_input))
+    {
+    return false;
+    }
+
+
+// Case 5.2: 
+$html_input  = '<p id="testID" data-test="testData" onmousedown="this.style.color=\'#FF0000\';" onmouseup="this.style.color=\'#000000\';">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+$html_output = '<p id="testID" data-test="testData">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+$allow_attrs = array('data-test');
+
+if($html_output !== strip_tags_and_attributes($html_input, array(), $allow_attrs))
+    {
+    return false;
+    }
+
+
+// Case 5.3: 
+$html_input  = '<p id="testID" onmousedown="this.style.color=\'#FF0000\';">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<script>console.log(true);</script></p>';
+$html_output = '<p id="testID">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<script>console.log(true);</script></p>';
+$allow_tags  = array('script');
+
+if($html_output !== strip_tags_and_attributes($html_input, $allow_tags))
+    {
+    return false;
+    }
+
+
+// Case 6: 
+$html_input  = '<p onmousedown="this.style.color=\'#FF0000\';" onmouseup="this.style.color=\'#000000\';">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+$html_output = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+
+if($html_output !== strip_tags_and_attributes($html_input))
+    {
+    return false;
+    }
+
+
+// Case 7: 
+$html_input  = '<p onmousedown="this.style.color=\'#FF0000\';" onmouseup="this.style.color=\'#000000\';">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+$html_output = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+
+if($html_output !== strip_tags_and_attributes($html_input))
+    {
+    return false;
+    }
+
+return true;
