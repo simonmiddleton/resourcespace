968becf149355b8738057efed381d156e7373615
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-12-06 09:34:13 +0000

    CHG: Slight adjustment to montala_assist so it appears more quickly.
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9257 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/montala_assist/hooks/all.php
+++ plugins/montala_assist/hooks/all.php
@@ -1,20 +1,16 @@
 <?php
 function HookMontala_assistAllSearchbarbottomtoolbar()
-	{
+        {
     # Load HTML from the ResourceSpace site, passing the base URL. The base URL allows Montala to feed appropriate options based
     # on whether the installation is a registered trial system, user installation, etc.
     global $baseurl;
-   	?>
+        ?>
     <script>
     jQuery(document).ready(function()
         {
-        jQuery('#montala_assist').load('https://resourcespace.montala.com/montala_assist_plugin.php?baseurl=<?php echo base64_encode($baseurl) ?>',function ()
-            {
-            jQuery('#montala_assist').fadeIn("fast");
-            });
+        if (jQuery('montala_assist').children().length==0) {jQuery('#montala_assist').load('https://resourcespace.montala.com/montala_assist_plugin.php?baseurl=<?php echo base64_encode($baseurl) ?>');}
         });
     </script>
-    <div id="montala_assist" style="display:none;"></div>
-	<?php
-	}
-
+    <div id="montala_assist"></div>
+        <?php
+        }
