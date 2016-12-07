335a0fcd57795a96379b60ecbe52b9dd7f94d044
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2016-12-07 15:01:04 +0000

    CHG: Rename montala_assist plugin to be more generic.
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9285 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/remote_assist/hooks/all.php
@@ -0,0 +1,16 @@
+<?php
+function HookRemote_assistAllSearchbarbottomtoolbar()
+        {
+    # Load HTML from the ResourceSpace site, passing the base URL. The base URL allows the ResourceSpace website to feed appropriate options based
+    # on whether the installation is a registered trial system, user installation, etc.
+    global $baseurl;
+        ?>
+    <script>
+    jQuery(document).ready(function()
+        {
+        if (jQuery('remote_assist').children().length==0) {jQuery('#remote_assist').load('https://resourcespace.montala.com/remote_assist_plugin.php?baseurl=<?php echo base64_encode($baseurl) ?>');}
+        });
+    </script>
+    <div id="remote_assist"></div>
+        <?php
+        }
