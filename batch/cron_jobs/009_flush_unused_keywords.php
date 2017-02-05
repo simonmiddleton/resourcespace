f40236486d5fb7e7360bfc0a7f48c23ee5a0c29e
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-02-05 23:17:31 +0000

    FIX: Amended cron as issues with fliushing buffer on some servers and broke down cleanup of resource_keyword to prevent MySQL timeouts
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9597 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- batch/cron_jobs/009_flush_unused_keywords.php
+++ batch/cron_jobs/009_flush_unused_keywords.php
@@ -4,12 +4,18 @@ include_once __DIR__ . "/../../include/db.php";
 // After migrating to nodes it is prefereable to delete resource_keyword rows for fixed list data
 // as this is now stored in normalised form in resource_node and node_keyword
 
-$count_query="select count(*) value from resource_keyword where resource_type_field in (select ref from resource_type_field where type in ('" . join("','",$FIXED_LIST_FIELD_TYPES) . "')) ";
-$c=sql_value($count_query,0);
+$fixed_fields=sql_array("select ref value from resource_type_field where type in ('" . join("','",$FIXED_LIST_FIELD_TYPES) . "') ");
 
-while ($c>0)
+foreach($fixed_fields as $fixed_field)
 	{
-	sql_query("delete from resource_keyword where resource_type_field in (select ref from resource_type_field where type in ('" . join("','",$FIXED_LIST_FIELD_TYPES) . "')) limit 1000");
+	echo "Deleting resource_keyword data for field ref#" . $fixed_field . PHP_EOL;
+	$count_query="select count(*) value from resource_keyword where resource_type_field='" . $fixed_field . "'";
 	$c=sql_value($count_query,0);
-	echo ($c . " rows remaining to delete");
+	while ($c>0)
+		{
+		sql_query("delete from resource_keyword where resource_type_field ='" . $fixed_field . "' limit 1000");
+		$c=sql_value($count_query,0);
+		echo ($c . " rows remaining to delete" . PHP_EOL);
+		}
+	
 	}
