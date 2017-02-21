e5cb6706517fa151fb4c093481589b693ca729fd
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-02-21 17:29:19 +0000

    NEW: AJAX page to help with nodes search by ID or name (fuzzy search). Note: names are being translated (i18l)
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20170110_acota_annotate_v2@9650 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ pages/ajax/get_nodes.php
@@ -0,0 +1,72 @@
+<?php
+include dirname(__FILE__) . '/../../include/db.php';
+include_once dirname(__FILE__) . '/../../include/general.php';
+include dirname(__FILE__) . '/../../include/authenticate.php';
+include_once dirname(__FILE__) . '/../../include/node_functions.php';
+
+/*
+This allows Asynchronous searches for nodes, either by: node ID or simply by searching for a name (fuzzy search)
+
+Expected functionality:
+If we search by node ID, then if found we get its details back
+Otherwise, we get all results back based on the name we've searched for.
+*/
+$node                = getvalescaped('node', 0, true);
+$resource_type_field = getvalescaped('resource_type_field', 0, true);
+$name                = getvalescaped('name', '');
+$rows                = getvalescaped('rows', 10, true);
+
+// Prevent access to fields to which user does not have access to
+if(!(checkperm("f{$resource_type_field}") || (checkperm('f*') && !checkperm("f-{$resource_type_field}"))))
+    {
+    header('HTTP/1.1 401 Unauthorized');
+    exit($lang['error-permissiondenied']);
+    }
+
+$return               = array();
+$found_node_by_ref    = array();
+$current_node_pointer = 0;
+
+if(0 == $node && '' == $name)
+    {
+    echo json_encode($return);
+    exit();
+    }
+
+if(0 < $node && get_node($node, $found_node_by_ref))
+    {
+    $found_node_by_ref['name'] = i18n_get_translated($found_node_by_ref['name']);
+
+    echo json_encode($found_node_by_ref);
+    exit();
+    }
+
+// Fuzzy search by node name:
+// Translate (i18l) all options and return those that have a match for what client code searched (fuzzy searching still applies)
+foreach(get_nodes($resource_type_field, null, true, null, $rows, $name) as $node)
+    {
+    if($rows == $current_node_pointer)
+        {
+        break;
+        }
+
+    $i18l_name = i18n_get_translated($node['name']);
+
+    // Skip any translated (i18l) names that don't contain what client code searched for
+    if(false === mb_strpos(mb_strtolower($i18l_name), $name))
+        {
+        continue;
+        }
+
+    $node['name'] = $i18l_name;
+
+    $return[] = $node;
+
+    // Increment only when valid nodes have been added to the result set
+    $current_node_pointer++;
+    }
+
+echo count($return);
+
+echo json_encode($return);
+exit();
