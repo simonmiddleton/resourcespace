054638368f6495e2e79e9b2c1a2bdf7a1463c093
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-02-28 18:23:59 +0000

    NEW: ability to delete annotations from database via RSTagging plugin
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20170110_acota_annotate_v2@9687 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/annotation_functions.php
+++ include/annotation_functions.php
@@ -1,5 +1,32 @@
 <?php
 /**
+* Get annotation by ID
+* 
+* @param integer $ref Annotation ID
+* 
+* @return array
+*/
+function getAnnotation($ref)
+    {
+    if(0 >= $ref)
+        {
+        return array();
+        }
+
+    $ref = escape_check($ref);
+
+    $return = sql_query("SELECT * FROM annotation WHERE ref = '{$ref}'");
+
+    if(0 < count($return))
+        {
+        $return = $return[0];
+        }
+
+    return $return;
+    }
+
+
+/**
 * Get annotations for a specific resource
 * 
 * @param integer $resource Resource ID
@@ -106,4 +133,50 @@ function getAnnotationTags(array $annotation)
               FROM node AS n
              WHERE ref IN (SELECT node FROM annotation_node WHERE annotation = '{$annotation_ref}');
         ");
+    }
+
+
+/**
+* Delete annotation
+* 
+* @see getAnnotation()
+* 
+* @uses annotationEditable()
+* @uses getAnnotationTags()
+* @uses delete_resource_nodes()
+* @uses db_begin_transaction()
+* @uses db_end_transaction()
+* 
+* @param array $annotation Annotation array as returned by getAnnotation()
+* 
+* @return boolean
+*/
+function deleteAnnotation(array $annotation)
+    {
+    if(!annotationEditable($annotation))
+        {
+        return false;
+        }
+
+    $annotation_ref = escape_check($annotation['ref']);
+
+    $nodes_to_remove = array();
+    foreach(getAnnotationTags($annotation) as $tag)
+        {
+        $nodes_to_remove[] = $tag['ref'];
+        }
+
+    db_begin_transaction();
+
+    if(0 < count($nodes_to_remove))
+        {
+        delete_resource_nodes(escape_check($annotation['resource']), $nodes_to_remove);
+        }
+
+    sql_query("DELETE FROM annotation_node WHERE annotation = '{$annotation_ref}'");
+    sql_query("DELETE FROM annotation WHERE ref = '{$annotation_ref}'");
+
+    db_end_transaction();
+
+    return true;
     }
--- pages/ajax/annotations.php
+++ pages/ajax/annotations.php
@@ -11,10 +11,44 @@ if(!$annotate_enabled)
     exit($lang['error-permissiondenied']);
     }
 
-$return   = array();
-$resource = getvalescaped('resource', 0, true);
+$return        = array();
 
-$return = getAnnotoriousResourceAnnotations($resource);
+$action        = getvalescaped('action', '');
+$resource      = getvalescaped('resource', 0, true);
+
+// Get annotation data if an ID has been provided
+$annotation_id = getvalescaped('annotation_id', 0, true);
+if(0 < $annotation_id)
+    {
+    $annotation = getAnnotation($annotation_id);
+    }
+
+if('get_resource_annotations' == $action)
+    {
+    $return['data'] = getAnnotoriousResourceAnnotations($resource);
+    }
+
+/*if('add' == $action)
+    {
+    // 
+    }*/
+
+if('delete' == $action && isset($annotation))
+    {
+    $return['data'] = deleteAnnotation($annotation);
+    }
+
+
+
+// If by this point we still don't have a response for the request,
+// create one now telling client code this is a bad request
+if(0 === count($return))
+    {
+    $return['error'] = array(
+        'status' => 400,
+        'title'  => 'Bad Request',
+        'detail' => 'The request could not be handled by annotations.php. This is the default response!');
+    }
 
 echo json_encode($return);
 exit();
