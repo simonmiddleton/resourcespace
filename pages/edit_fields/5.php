4d99f31be5b3f54670555b7e734f85b5c5c66367
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-03-28 10:29:51 +0000

    FIX: XSS vulnerability for text fields
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@9850 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/5.php
+++ pages/edit_fields/5.php
@@ -1,4 +1,4 @@
 <?php /* -------- Text Box (large multi-line) ----- */ ?>
 <textarea class="stdwidth" rows=20 cols=80 name="<?php echo $name?>" id="<?php echo $name?>" <?php echo $help_js; ?>
 <?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"]?>');"<?php } ?>
-><?php echo htmlspecialchars($value)?></textarea>
+><?php echo htmlspecialchars(strip_tags(strip_tags_and_attributes($value))); ?></textarea>
