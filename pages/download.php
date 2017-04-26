5b447737a7fefa6c7d05689bc53dc47f009b38ab
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-04-26 17:26:12 +0000

    CHG: download.php and added the ability to download/stream a file and resume it later if needed
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20170405_acota_hide_filepaths@9933 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/download.php
+++ pages/download.php
@@ -10,6 +10,8 @@ include_once dirname(__FILE__) . '/../include/search_functions.php';
 ob_end_clean(); 
 
 
+debug('PAGES/DOWNLOAD.PHP:');
+
 if($download_no_session_cache_limiter)
     {
     session_cache_limiter(false);
@@ -117,19 +119,32 @@ else
         }
     }
 
+debug("PAGES/DOWNLOAD.PHP: Preparing to download/ stream file '{$path}'");
 
+// File does not exist
 if(!file_exists($path))
     {
-    error_alert($lang['downloadfile_nofile'], true);
+    debug("PAGES/DOWNLOAD.PHP: File does not exist!");
 
+    header('HTTP/1.0 404 Not Found');
     exit();
     }
 
 hook('modifydownloadfile'); 
 
-$filesize = filesize_unlimited($path);
+$file_size   = filesize_unlimited($path);
+$file_handle = fopen($path, 'rb');
 
-header("Content-Length: {$filesize}");
+debug("PAGES/DOWNLOAD.PHP: \$file_size = {$file_size}");
+
+// File could not be opened
+if(!$file_handle)
+    {
+    debug("PAGES/DOWNLOAD.PHP: File could not be opened!");
+
+    header('HTTP/1.0 500 Internal Server Error');
+    exit();
+    }
 
 // Log this activity (download only, not preview)
 if('' == $noattach)
@@ -150,40 +165,115 @@ if('' == $noattach)
     
     // We compute a file name for the download.
     $filename = get_download_filename($ref, $size, $alternative, $ext);
+    }
 
-    if(!$direct)
+// Set appropriate headers for attachment or streamed file
+if(!$direct && isset($filename))
+    {
+    header("Content-Disposition: attachment; filename=\"{$filename}\"");
+
+    debug("PAGES/DOWNLOAD.PHP: Set header for attachment file");
+    }
+else
+    {
+    header('Content-Disposition: inline;');
+    header('Content-Transfer-Encoding: binary');
+
+    debug("PAGES/DOWNLOAD.PHP: Set header for streamed file");
+    }
+
+// We declare the downloaded content mime type
+$mime = get_mime_type($path);
+header("Content-Type: {$mime}");
+
+debug("PAGES/DOWNLOAD.PHP: Set MIME type to '{$mime}'");
+
+// Check if http_range is sent by browser (or download manager)
+if(isset($_SERVER['HTTP_RANGE']))
+    {
+    debug("PAGES/DOWNLOAD.PHP: HTTP_RANGE is set to '{$_SERVER['HTTP_RANGE']}'");
+
+    list($size_unit, $range_orig) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+
+    if('bytes' == $size_unit)
         {
-        header("Content-Disposition: attachment; filename=\"{$filename}\"");
+        /* Multiple ranges could be specified at the same time, but for simplicity only serve the first range
+        http://tools.ietf.org/id/draft-ietf-http-range-retrieval-00.txt
+
+        IMPORTANT: If multiple ranges are not specified, PHP can return an error for "Undefined offset: 1",
+        so we pad the array with an empty string */
+        list($range, $extra_ranges) = array_pad(explode(',', $range_orig, 2), 2, '');
+        }
+    else
+        {
+        debug("PAGES/DOWNLOAD.PHP: Requested range was not valid");
+
+        header('HTTP/1.1 416 Requested Range Not Satisfiable');
+        header("Content-Range: */{$file_size}");
+        exit();
         }
     }
-
-// We assign a default mime-type, in case we can find the one associated to the file extension.
-$mime = 'application/octet-stream';
-
-if('' == $noattach)
+else
     {
-    $mime = get_mime_type($path);
+    debug("PAGES/DOWNLOAD.PHP: HTTP_RANGE is not set!");
+
+    $range = '';
     }
 
-// We declare the downloaded content mime type.
-header("Content-Type: {$mime}");
+debug("PAGES/DOWNLOAD.PHP: \$range = {$range}");
+
+// Figure out download piece from range (if set)
+list($seek_start, $seek_end) = array_pad(explode('-', $range, 2), 2, '');
+
+// Set start and end based on range (if set), else set defaults
+// also check for invalid ranges.
+$seek_end   = (empty($seek_end)) ? ($file_size - 1) : min(abs(intval($seek_end)), ($file_size - 1));
+$seek_start = (empty($seek_start) || $seek_end < abs(intval($seek_start))) ? 0 : max(abs(intval($seek_start)), 0);
+
+debug("PAGES/DOWNLOAD.PHP: \$seek_start = {$seek_start}");
+debug("PAGES/DOWNLOAD.PHP: \$seek_end = {$seek_end}");
+
+// Only send partial content header if downloading a piece of the file (IE workaround)
+if(0 < $seek_start || $seek_end < ($file_size - 1))
+    {
+    header('HTTP/1.1 206 Partial Content');
+    header("Content-Range: bytes {$seek_start}-{$seek_end}/{$file_size}");
+    header('Content-Length: ' . ($seek_end - $seek_start + 1));
+
+    debug("PAGES/DOWNLOAD.PHP: Content-Range: bytes {$seek_start}-{$seek_end}/{$file_size}");
+    debug('PAGES/DOWNLOAD.PHP: Content-Length: ' . ($seek_end - $seek_start +1));
+    }
+else
+    {
+    header("Content-Length: {$file_size}");
+
+    debug("PAGES/DOWNLOAD.PHP: Content-Length: {$file_size}");
+    }
+
+header('Accept-Ranges: bytes');
 
 set_time_limit(0);
 
 if(!hook('replacefileoutput'))
     {
-    $sent   = 0;
-    $handle = fopen($path, 'r');
+    $sent = (0 == fseek($file_handle, $seek_start) ? $seek_start : 0);
 
-    // Now we need to loop through the file and echo out chunks of file data
-    while($sent < $filesize)
+    while($sent < $file_size)
         {
-        echo fread($handle, $download_chunk_size);
+        echo fread($file_handle, $download_chunk_size);
 
         ob_flush();
+        flush();
 
         $sent += $download_chunk_size;
+
+        if(0 != connection_status()) 
+            {
+            break;
+            }
         }
+
+    fclose($file_handle);
     }
 
 // Deleting Exiftool temp File:
