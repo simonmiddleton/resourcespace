be48cb228bf61511ccf4c09dac0acc40347fae4e
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-05-18 14:36:11 +0000

    NEW: [acota_facial_recognition] Adding new function to use when preparing face images for FaceRecognizer
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20170504_acota_facial_recognition@9987 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/facial_recognition_functions.php
+++ include/facial_recognition_functions.php
@@ -40,3 +40,72 @@ function initFacialRecognition()
 
     return true;
     }
+
+
+/**
+* Crops out a selected area of an image and makes it ready to be used by FaceRecognizer.
+* 
+* Note: The selected area should follow the normalized coordinate system.
+* 
+* @uses get_utility_path()
+* @uses debug()
+* 
+* @param string $image_path          Path of the source image
+* @param string $prepared_image_path 
+* @param float  $x                   X position
+* @param float  $y                   Y position
+* @param float  $width               Width
+* @param float  $height              Height
+* 
+* @return boolean
+*/
+function prepareFaceImage($image_path, $prepared_image_path, $x, $y, $width, $height)
+    {
+    if(!file_exists($image_path))
+        {
+        debug("FACIAL_RECOGNITION: Could not find image at '{$image_path}'");
+        return false;
+        }
+
+    // Use existing prepared image if one is found
+    if(file_exists($prepared_image_path))
+        {
+        return true;
+        }
+
+    // X, Y, width and height MUST be numeric
+    if(!is_numeric($x) || !is_numeric($y) || !is_numeric($width) || !is_numeric($height))
+        {
+        return false;
+        }
+
+    $convert_fullpath = get_utility_path('im-convert');
+    if(false === $convert_fullpath)
+        {
+        debug('FACIAL_RECOGNITION: Could not find ImageMagick "convert" utility!');
+        return false;
+        }
+
+    list($image_width, $image_height) = getimagesize($image_path);
+
+    $image_path_escaped          = escapeshellarg($image_path);
+    $prepared_image_path_escaped = escapeshellarg($prepared_image_path);
+
+    $x      = escapeshellarg(round($x * $image_width, 0));
+    $y      = escapeshellarg(round($y * $image_height, 0));
+    $width  = escapeshellarg(round($width * $image_width, 0));
+    $height = escapeshellarg(round($height * $image_height, 0));
+
+    $cmd  = $convert_fullpath;
+    $cmd .= " {$image_path_escaped} -colorspace gray -depth 8";
+    $cmd .= " -crop {$width}x{$height}+{$x}+{$y}";
+    $cmd .= " -resize 90x90\>";
+    $cmd .= " +repage {$prepared_image_path_escaped}";
+
+    if('' !== run_command($cmd))
+        {
+        return false;
+        }
+
+    return true;
+    }
