25f607a47f4c631dbbdc95a56e8254b5000bbcd7
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-05-30 14:32:33 +0000

    FIX: simplesaml plugin issue where keys were allowed to bypass SSO even if invalid
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@10016 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/simplesaml/hooks/all.php
+++ plugins/simplesaml/hooks/all.php
@@ -27,10 +27,21 @@ function HookSimplesamlAllPreheaderoutput()
 	if (!$simplesaml_site_block){return true;}
 
 	// Check for exclusions
-	if ($simplesaml_allow_public_shares && getvalescaped("k","")!="") 
-		{
-		return true;
-		}
+    $k = getvalescaped('k', '');
+    if(
+        $simplesaml_allow_public_shares &&
+        '' != $k &&
+        (
+            // Hard to determine at this stage what we consider a collection/ resource ID so we
+            // use the most general ones
+            check_access_key_collection(str_replace('!collection', '', getvalescaped('search', '')), $k) && 
+            check_access_key(getvalescaped('ref', ''), $k)
+        )
+    )
+        {
+        return true;
+        }
+
 	$url=str_replace("\\","/", $_SERVER["PHP_SELF"]);
 
 	foreach ($simplesaml_allowedpaths as $simplesaml_allowedpath)
