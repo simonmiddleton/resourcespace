76f2f763843376d0516d3ac305ba7eed5e7dc0f1
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-07-26 12:50:48 +0000

    FIX: Missing file
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@10202 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ pages/search_views/xlthumbs_footer.php
@@ -0,0 +1,7 @@
+<script>
+
+// Register events
+jQuery(document).ready(function () {StripResizeResults(220);});
+jQuery(window).resize(function () {window.setTimeout("StripResizeResults(220);",1000);});
+
+</script>
