3267d5b3eb5e09570534cecc1391431b5b184f62
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-08-01 10:16:26 +0000

    FIX: Pull 10240 back to trunk - disable maps for ResourceConnect
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@10241 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/resourceconnect/hooks/all.php
+++ plugins/resourceconnect/hooks/all.php
@@ -19,9 +19,14 @@ function HookResourceconnectAllCheck_access_key($resource,$key)
 	global $is_resourceconnect;
 	$is_resourceconnect=true;
 
+	# Disable collections - not needed when accessed remotely
 	global $collections_footer;	
 	$collections_footer=false;
 	
+	# Disable maps
+	global $disable_geocoding;
+	$disable_geocoding=true;
+
 	return true;
 	}
 
