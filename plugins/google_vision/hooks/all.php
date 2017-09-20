bb1dbaf3b525def3354150994eb009b79c39de10
astec <astec@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-09-20 16:08:11 +0000

    FIX: plugin was not being called during staticsync upload. Possibly specific to non-ingest method.
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@10537 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/google_vision/hooks/all.php
+++ plugins/google_vision/hooks/all.php
@@ -6,3 +6,9 @@ function HookGoogle_visionAllUploadfilesuccess($resource)
     include_once __DIR__ . "/../include/google_vision_functions.php";
     google_visionProcess($resource);
     }
+
+function HookGoogle_visionAllAfter_update_resource($resource)
+	{
+	include_once __DIR__ . "/../include/google_vision_functions.php";
+    google_visionProcess($resource);
+	}
