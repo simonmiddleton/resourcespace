705342500dd7c384cdb7602c3661dfd7b43181d1
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-10-18 09:19:24 +0000

    FIX: [8.3] Missing permission check for creating featured collections [t15225]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/8.3@10714 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/render_functions.php
+++ include/render_functions.php
@@ -2128,7 +2128,7 @@ function renderCallToActionTile($link)
     {
     global $themes_simple_view;
 
-    if(!$themes_simple_view || checkperm('b'))
+    if(!$themes_simple_view || checkperm('b') || !checkperm('h'))
         {
         return;
         }
