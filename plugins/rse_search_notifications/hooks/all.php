77338036a73d11c96f79bfd2308bad2c520954d1
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2017-11-28 17:32:14 +0000

    FIX: XSS in rse_search_notifications plugin [t15659]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@10826 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/rse_search_notifications/hooks/all.php
+++ plugins/rse_search_notifications/hooks/all.php
@@ -3,23 +3,36 @@
 include_once __DIR__ . '/../include/search_notifications_functions.php';
 
 function HookRse_search_notificationsAllRender_search_actions_add_option($options)
-	{
-	global $lang,$watched_searches_url,$search,$restypes,$archive,$k;
-	if ($k!="" || !isset($search) || $search=="")
-	{				
-		return array();		
-	}	
-	$option=array(
-		'value' => 'watch_this_search',
-		'label' => $lang['search_notifications_watch_this_search'],
-		'data_attr' => array
-			(
-			'url' => "{$watched_searches_url}?callback=add&search={$search}&restypes={$restypes}&archive={$archive}"
-			)
-	);
-	array_push($options,$option);
-	return $options;
-	}
+    {
+    global $lang, $watched_searches_url, $search, $restypes, $archive, $k;
+
+    if($k != '' || !isset($search) || $search == '')
+        {
+        return array();
+        }
+
+    $data_attr_url = generateURL(
+        $watched_searches_url,
+        array(
+            'callback' => 'add',
+            'search'   => $search,
+            'restypes' => $restypes,
+            'archive'  => $archive,
+        )
+    );
+
+    $option = array(
+        'value'     => 'watch_this_search',
+        'label'     => $lang['search_notifications_watch_this_search'],
+        'data_attr' => array(
+            'url' => $data_attr_url
+        )
+    );
+
+    array_push($options, $option);
+
+    return $options;
+    }
 
 function HookRse_search_notificationsAllRender_actions_add_collection_option($top_actions,array $options)
 	{
--- plugins/rse_search_notifications/hooks/search.php
+++ plugins/rse_search_notifications/hooks/search.php
@@ -3,13 +3,24 @@
 include_once __DIR__ . '/../include/search_notifications_functions.php';
 
 function HookRse_search_notificationsSearchafterResulthints()
-	{
-	global $lang, $search, $restypes, $archive, $watched_searches_url,$k;
-	if($k=="")
-		{
-		?><a href="<?php echo $watched_searches_url; ?>?callback=add&search=<?php echo $search; ?>&restypes=<?php echo $restypes; ?>&archive=<?php echo $archive; ?>" onClick="return CentralSpaceLoad(this,true);"><?php
-			echo $lang['search_notifications_notify_me'];
-		?></a>
-		<?php
-		}
-	}
+    {
+    global $lang, $search, $restypes, $archive, $watched_searches_url, $k;
+
+    $href = generateURL(
+        $watched_searches_url,
+        array(
+            'callback' => 'add',
+            'search'   => $search,
+            'restypes' => $restypes,
+            'archive'  => $archive,
+        )
+    );
+
+    if($k == '')
+        {
+        ?><a href="<?php echo $href; ?>" onClick="return CentralSpaceLoad(this, true);"><?php
+            echo $lang['search_notifications_notify_me'];
+        ?></a>
+        <?php
+        }
+    }
