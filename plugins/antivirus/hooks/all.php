5cf8c8ba85237e769d45c04523b258f753e31232
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-01-08 11:53:21 +0000

    FIX: [8.4] Refactoring Antivirus plugin to provide a better UX with styled alerts if file is unsafe. Also moved silent options to keep it consistent with antivirus path since neither of these can be set from the UI (a change I made a few revisions back). [t15785]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/8.4@10931 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/antivirus/hooks/all.php
+++ plugins/antivirus/hooks/all.php
@@ -1,15 +1,20 @@
 <?php
 function HookAntivirusAllAfterpluploadfile($resource_id, $file_extension)
     {
-    global $lang, $resource_deletion_state, $antivirus_action, $antivirus_silent_options, $antivirus_quarantine_state;
+    global $lang, $resource_deletion_state, $antivirus_action, $antivirus_quarantine_state;
+
+    // This will show both as a styledalert and as a message in the log
+    $plupload_json_response = array(
+        'jsonrpc' => '2.0',
+        'error'   => array(
+            'code'    => $lang['antivirus_label'],
+            'message' => ''
+        ),
+        'id'      => 'id'
+    );
 
     $resource_path = get_resource_path($resource_id, true, '', false, $file_extension);
-
-    echo PHP_EOL . $lang['antivirus_scanning_file'];
-
-    $scan_output = antivirus_scan($resource_path, $antivirus_silent_options);
-
-    echo PHP_EOL . $scan_output . PHP_EOL;
+    $scan_output   = antivirus_scan($resource_path);
 
     if('UNSAFE' === $scan_output)
         {
@@ -19,7 +24,12 @@ function HookAntivirusAllAfterpluploadfile($resource_id, $file_extension)
                 // Delete permanently
                 delete_resource($resource_id);
                 delete_resource($resource_id);
-                echo $lang['antivirus_deleting_file'] . PHP_EOL;
+
+                $plupload_json_response['error']['message'] = str_replace(
+                    '[%resource_id%]',
+                    $resource_id,
+                    $lang['antivirus_deleting_file']
+                );
                 break;
 
             case ANTIVIRUS_ACTION_QUARANTINE:
@@ -28,11 +38,17 @@ function HookAntivirusAllAfterpluploadfile($resource_id, $file_extension)
                 $resource_data = get_resource_data($resource_id);
 
                 update_archive_status($resource_id, $to_state, $resource_data['archive']);
-                echo "{$lang['antivirus_moving_file']} {$to_state}" . PHP_EOL;
+
+                $plupload_json_response['error']['message'] = str_replace(
+                    array('[%resource_id%]', '[%archive_state%]'),
+                    array($resource_id, $to_state),
+                    $lang['antivirus_moving_file']
+                );
                 break;
             }
 
-        exit();
+        header('Content-Type: application/json');
+        die(json_encode($plupload_json_response));
         }
 
     return true;
--- plugins/antivirus/languages/en.php
+++ plugins/antivirus/languages/en.php
@@ -2,14 +2,14 @@
 $lang['antivirus_configuration'] = 'Antivirus configuration';
 
 // Setup labels
+$lang['antivirus_label'] = 'Antivirus';
 $lang['antivirus_action_label'] = 'Choose appropriate action to be taken on discovery of malicious files';
 $lang['antivirus_quarantine_status_label'] = 'Select an archive state to be used as quarantine holding area';
 
 $lang['antivirus_action_delete'] = 'Delete permanently';
 $lang['antivirus_action_quarantine'] = 'Move to quarantine';
-$lang['antivirus_scanning_file'] = 'Scanning file...';
-$lang['antivirus_deleting_file'] = 'Deleting resource and any associated files!';
-$lang['antivirus_moving_file'] = 'Moving resource to archive state';
+$lang['antivirus_deleting_file'] = 'Deleting resource [%resource_id%] and any associated files!';
+$lang['antivirus_moving_file'] = 'Moving resource [%resource_id%] to archive state [%archive_state%]';
 
 // Errors:
 $lang['antivirus_av_not_setup_error'] = 'Antivirus path not set up!';
