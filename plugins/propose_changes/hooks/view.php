aa8b93b355a40b1b779daa6da84857c07f3bd9e4
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-01-19 11:45:43 +0000

    FIX: Update font awesome icon for propose changes button [t16220]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@10973 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/propose_changes/hooks/view.php
+++ plugins/propose_changes/hooks/view.php
@@ -13,7 +13,7 @@ function HookPropose_changesViewAfterresourceactions()
 			{
 			global $baseurl, $lang;
 			?>
-			<li><a href="<?php echo $baseurl ?>/plugins/propose_changes/pages/propose_changes.php?ref=<?php echo urlencode($ref)?>&amp;search=<?php echo urlencode($search)?>&amp;search_offset=<?php echo urlencode($offset)?>&amp;order_by=<?php echo urlencode($order_by)?>&amp;sort=<?php echo urlencode($sort)?>&amp;archive=<?php echo urlencode($archive)?>" onClick="return CentralSpaceLoad(this,true);"><?php echo "<i class='fa fa-idea'></i>&nbsp;" . $lang["propose_changes_review_proposed_changes"]?></a></li>
+			<li><a href="<?php echo $baseurl ?>/plugins/propose_changes/pages/propose_changes.php?ref=<?php echo urlencode($ref)?>&amp;search=<?php echo urlencode($search)?>&amp;search_offset=<?php echo urlencode($offset)?>&amp;order_by=<?php echo urlencode($order_by)?>&amp;sort=<?php echo urlencode($sort)?>&amp;archive=<?php echo urlencode($archive)?>" onClick="return CentralSpaceLoad(this,true);"><?php echo "<i class='fa fa-lightbulb-o'></i>&nbsp;" . $lang["propose_changes_review_proposed_changes"]?></a></li>
 			<?php 
 			}
 		}
@@ -48,7 +48,7 @@ function HookPropose_changesViewAfterresourceactions()
 			{
 			global $baseurl, $lang;
 			?>
-			<li><a href="<?php echo $baseurl ?>/plugins/propose_changes/pages/propose_changes.php?ref=<?php echo urlencode($ref)?>&amp;search=<?php echo urlencode($search)?>&amp;search_offset=<?php echo urlencode($offset)?>&amp;order_by=<?php echo urlencode($order_by)?>&amp;sort=<?php echo urlencode($sort)?>&amp;archive=<?php echo urlencode($archive)?>" onClick="return CentralSpaceLoad(this,true);"><?php echo "<i class='fa fa-idea'></i>&nbsp;".$lang["propose_changes_short"]?></a></li>
+			<li><a href="<?php echo $baseurl ?>/plugins/propose_changes/pages/propose_changes.php?ref=<?php echo urlencode($ref)?>&amp;search=<?php echo urlencode($search)?>&amp;search_offset=<?php echo urlencode($offset)?>&amp;order_by=<?php echo urlencode($order_by)?>&amp;sort=<?php echo urlencode($sort)?>&amp;archive=<?php echo urlencode($archive)?>" onClick="return CentralSpaceLoad(this,true);"><?php echo "<i class='fa fa-lightbulb-o'></i>&nbsp;".$lang["propose_changes_short"]?></a></li>
 			<?php            
 			}
 		}
