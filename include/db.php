92963efc025c580d2d5194afa32ae69a306faca8
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-01-24 10:34:15 +0000

    FIX: System breaks if $use_plugins_manager set to false. [t16258]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@10984 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/db.php
+++ include/db.php
@@ -264,10 +264,11 @@ $querylog=array();
 # -----------LANGUAGES AND PLUGINS-------------------------------
 
 # Setup plugin configurations
+include_once "config_functions.php";
+include_once "plugin_functions.php";
+
 if ($use_plugins_manager)
 	{
-	include 'config_functions.php';
-	include "plugin_functions.php";
 	$legacy_plugins = $plugins; # Make a copy of plugins activated via config.php
 	# Check that manually (via config.php) activated plugins are included in the plugins table.
 	foreach($plugins as $plugin_name)
