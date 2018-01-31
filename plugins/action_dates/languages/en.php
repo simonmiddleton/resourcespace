6b1819f20e24f2ed6beb325682e0cb0c030cc3a7
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-01-31 09:52:00 +0000

    FIX: Tweaked text in action_dates setup page to be clearer [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11013 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/action_dates/languages/en.php
+++ plugins/action_dates/languages/en.php
@@ -8,13 +8,13 @@
 $lang['action_dates_restrictsettings']="Automatic restriction settings";
 $lang['action_dates_deletesettings']="Automatic resource deletion settings - use with caution";
 $lang['action_dates_configuration']="Select the fields that will be used to automatically perform the actions specified.";
-$lang['action_dates_delete']="Automatically delete resources when the date in this field is reached";
+$lang['action_dates_delete']="Automatically delete or change state of resources when the date in this field is reached";
 $lang['action_dates_restrict']="Automatically restrict access to resources when the date in this field is reached. This will only apply to resources currently in the open state.";
 $lang['action_dates_delete_logtext']=" - Automatically deleted by action_dates plugin";
 $lang['action_dates_restrict_logtext']=" - Automatically restricted by action_dates plugin";
-$lang['action_dates_reallydelete']="Fully delete resource when deletion data passed? Resource will be marked as deleted or use configured resource_deletion_state if this is set to false";
+$lang['action_dates_reallydelete']="Fully delete resource when deletion data passed? If set to false resources will be moved to the configured resource_deletion_state and thus recoverable";
 $lang['action_dates_email_admin_days']="Email administrator a set number of days before this date is reached. Leave blank for no email.";
 $lang['action_dates_email_text']="The following resources are due to be restricted in %%DAYS days.";
 $lang['action_dates_email_subject']="Notification of resources due to be restricted";
-$lang['action_dates_new_state'] = 'New state to move to (if fully deleting resources, than this is not taken into account)';
+$lang['action_dates_new_state'] = 'New state to move to (if above option is set to fully delete resources this will be ignored)';
 $lang['action_dates_notification_subject'] = 'Notification from action dates plugin';
