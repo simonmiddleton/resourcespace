96faeadfae967b87b6dc7102ebe426dd50d3d971
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-02-06 16:38:41 +0000

    CHG: Added extra tests for searching html and within quoted text [t16159]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11034 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ tests/test_list/000925_do_search_quotes.php
@@ -0,0 +1,73 @@
+<?php
+
+include_once(__DIR__ . '/../../include/search_functions.php');
+if (php_sapi_name()!=="cli") {exit("This utility is command line only.");}
+
+// Check indexing of quoted text works ok
+
+// First use non-fixed list data field
+$resourcea=create_resource(1,0);
+
+// create new field
+$quotefield = create_resource_type_field("Quoted text test",0,FIELD_TYPE_TEXT_BOX_MULTI_LINE,"quotetest",1);
+
+$quotedata = "\"romeo lima foxtrot.\" said the author";
+
+update_field($resourcea,$htmlfield,$quotedata);
+
+// Do search for 'lima romeo' (should return resource a)
+$results=do_search("lima romeo");
+if(!is_array($results) || !in_array($resourcea,array_column($results,"ref")))
+    {
+    echo "ERROR - SUBTEST A\n";
+    return false;
+    }
+
+// Do quoted search for quoted 'romeo lima' (should include resource a)
+$results=do_search("\"romeo lima\"");
+if(!is_array($results) || !in_array($resourcea,array_column($results,"ref")))
+    {
+    echo "ERROR - SUBTEST B\n";
+    return false;
+    }
+    
+// Now test using nodes
+$resourceb=create_resource(1,0);
+$quotenode = set_node(NULL, 3, $quotedata,'',1000);
+
+// Add node to resource b
+add_resource_nodes($resourceb,array($quotenode));
+
+// Do search for 'lima romeo' (should return resource b)
+$results=do_search("lima romeo");
+if(!is_array($results) || !in_array($resourceb,array_column($results,"ref")))
+    {
+    echo "ERROR - SUBTEST C\n";
+    return false;
+    }
+
+// Do search for quoted 'romeo lima' (should return resource b)
+$results=do_search("\"romeo lima\"");
+if(!is_array($results) || !in_array($resourceb,array_column($results,"ref")))
+    {
+    echo "ERROR - SUBTEST D\n";
+    return false;
+    }
+    
+// Do search for quoted 'romeo foxtrot' (shouldn't return resource a or b)
+$results=do_search("\"romeo foxtrot\"");
+if(is_array($results) && (in_array($resourcea,array_column($results,"ref")) || in_array($resourceb,array_column($results,"ref"))))
+    {
+    echo "ERROR - SUBTEST E\n";
+    return false;
+    }
+    
+// Do search for quoted 'romeo lima foxtrot said the author' should return resource a and b)
+$results=do_search("\"romeo lima foxtrot said the author\"");
+if(!is_array($results) || !in_array($resourceb,array_column($results,"ref")) || !in_array($resourceb,array_column($results,"ref")))
+    {
+    echo "ERROR - SUBTEST F\n";
+    return false;
+    }
+
+return true;
