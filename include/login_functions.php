9cfab546397cace54e83b35e7ce38cfd09e12870
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-02-13 15:54:38 +0000

    FIX: Tweak to r11065 to make language parameter optional for set_login_cookies [q9992]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11067 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/login_functions.php
+++ include/login_functions.php
@@ -168,16 +168,19 @@ function generate_session_hash($password_hash)
 		
 	}
 
-function set_login_cookies($user, $session_hash, $language, $user_preferences)
+function set_login_cookies($user, $session_hash, $language = "", $user_preferences)
     {
-    global $baseurl, $baseurl_short, $allow_keep_logged_in, $default_res_types;
+    global $baseurl, $baseurl_short, $allow_keep_logged_in, $default_res_types, $language;
     $expires=0;
     if ($allow_keep_logged_in && getval("remember","")!="") {$expires = 100;} # remember login for 100 days
 
-    # Store language cookie
-    rs_setcookie("language", $language, 1000); # Only used if not global cookies
-    rs_setcookie("language", $language, 1000, $baseurl_short . "pages/");
-
+    if($language != "")
+        {
+        # Store language cookie
+        rs_setcookie("language", $language, 1000); # Only used if not global cookies
+        rs_setcookie("language", $language, 1000, $baseurl_short . "pages/");
+        }
+        
     # Set the session cookie. Do this for all paths that nay set the cookie as otherwise we can end up with a valid cookie at e.g. pages/team or pages/ajax
     rs_setcookie("user", "", 0);
     rs_setcookie("user", "", 0,"/pages");
