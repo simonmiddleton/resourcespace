e0f54db992c23548710a0acbcd2df3868fe7f9f5
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-02-20 15:51:51 +0000

    FIX: [8.4] XSS issues on the Manage my collections page [t16420]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/8.4@11083 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collection_manage.php
+++ pages/collection_manage.php
@@ -326,9 +326,9 @@ for ($n=$offset;(($n<count($collections)) && ($n<($offset+$per_page)));$n++)
     $count_result = $collections[$n]["count"];
 	?><tr <?php hook("collectionlistrowstyle");?>>
 	<td class="name"><div class="ListTitle">
-		<a <?php if ($collections[$n]["public"]==1 && (strlen($collections[$n]["theme"])>0)) { ?>style="font-style:italic;"<?php } ?> href="<?php echo $baseurl_short?>pages/search.php?search=<?php echo urlencode("!collection" . $collections[$n]["ref"])?>" onClick="return CentralSpaceLoad(this);"><?php echo highlightkeywords(i18n_get_collection_name($collections[$n]),$find) ?></a></div></td>
-	<td class="fullname"><?php echo highlightkeywords($colusername,$find) ?></td>
-	<td class="ref"><?php echo highlightkeywords($collection_prefix . $collections[$n]["ref"],$find) ?></td>
+		<a <?php if ($collections[$n]["public"]==1 && (strlen($collections[$n]["theme"])>0)) { ?>style="font-style:italic;"<?php } ?> href="<?php echo $baseurl_short?>pages/search.php?search=<?php echo urlencode("!collection" . $collections[$n]["ref"])?>" onClick="return CentralSpaceLoad(this);"><?php echo strip_tags_and_attributes(highlightkeywords(htmlspecialchars_decode(i18n_get_collection_name($collections[$n])), $find)); ?></a></div></td>
+	<td class="fullname"><?php echo strip_tags_and_attributes(highlightkeywords($colusername, $find)); ?></td>
+	<td class="ref"><?php echo strip_tags_and_attributes(highlightkeywords($collection_prefix . $collections[$n]["ref"], $find)); ?></td>
 	<td class="created"><?php echo nicedate($collections[$n]["created"],true) ?></td>
 	<td class="count"><?php echo $collections[$n]["count"] ?></td>
 <?php if (! $hide_access_column){ ?>	<td class="access"><?php
