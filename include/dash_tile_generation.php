da8ed580622679f55bd5ff90e36e8166d4b803cb
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-04-18 09:49:13 +0000

    FIX: Dash_tile_generation was failing in php 5.3, corrected logic to check if object is array instead of checking if part of the array was set as it created an undefined offset error [t16794]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11308 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/dash_tile_generation.php
+++ include/dash_tile_generation.php
@@ -197,7 +197,7 @@ function tile_config_pending($tile,$tile_id,$tile_width,$tile_height)
 	$tile_search=do_search($search,$restypes,$order_by,$archive,$count,$sort,false,0,false,false,"",false,false);
 	$found_resources=true;
 	$count=count($tile_search);
-	if(!isset($tile_search[0]["ref"]))
+	if(!is_array($tile_search))
 		{
 		$found_resources=false;
 		$count=0;
