2bc2df7b7bc4ffb66b72c06e42ac49fcead0fc22
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-05-15 22:20:13 +0000

    CHG: Updated migrate_data_to_fixed.php to set field id if passed in URL. Also added progress and logging [q10246]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11462 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/migrate_data_to_fixed.php
+++ pages/tools/migrate_data_to_fixed.php
@@ -1,4 +1,10 @@
 <?php
+if(isset($_POST["submit"]))
+    {
+    $suppress_headers=true;
+    $nocache = true;
+    }
+
 include "../../include/db.php";
 include_once "../../include/general.php";
 include_once "../../include/resource_functions.php";
@@ -6,8 +12,10 @@ include_once "../../include/search_functions.php";
 include_once "../../include/authenticate.php";
 if(!checkperm("a")){exit("Access denied");}
 
+// Set flag to indicate whether we can show progress using server side events (SSE)
+$showprogress = strpos(strtoupper($_SERVER['HTTP_USER_AGENT']),"TRIDENT") === false && strpos(strtoupper($_SERVER['HTTP_USER_AGENT']),"MSIE") === false;
 $migrate_field = getvalescaped("field",0,true);
-if($migrate_field == 0){$errortext=("No field specified");}
+$field_info = get_resource_type_field($migrate_field);
 $splitvalue = getvalescaped("splitchar","");
 $modal = (getval("modal","")=="true");
 $dryrun = getval("dryrun","")!="";
@@ -19,75 +27,47 @@ if($backurl=="")
     $backurl=$baseurl . "/pages/admin/admin_resource_type_field_edit.php?ref=" . $migrate_field;
     }
 
-include_once "../../include/header.php";
-
-if (isset($error_text)) { ?><div class="PageInformal"><?php echo $error_text?></div><?php }
-
-?>
-<div class="BasicsBox">
-	<p>    
-	<a href="<?php echo $backurl ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo LINK_CARET_BACK ?><?php echo $lang["back"]?></a>
-	</p>
-	<h1><?php echo $lang["admin_resource_type_field_migrate_data"] ?></h1>
-
-	<form method="post" action="<?php echo $baseurl_short ?>pages/tools/migrate_data_to_fixed.php" >
-        <?php generateFormToken("migrate_data_to_fixed"); ?>
-	<div class="Question" >
-		<label for="field" ><?php echo $lang["field"] ?></label>
-		<input type="number" name="field" value="<?php echo $migrate_field ?>">
-	</div>
-	<div class="Question" >
-		<label for="splitchar" ><?php echo $lang["admin_resource_type_field_migrate_separator"] ?></label>
-		<input type="text" name="splitchar" value=",">
-	</div>
-	<div class="Question" >
-		<label for="dryrun" ><?php echo $lang["admin_resource_type_field_migrate_dry_run"] ?></label>
-		<input type="checkbox" name="dryrun" value="true">
-	</div>
-	<div class="Question" >
-		<label for="deletedata" ><?php echo $lang["admin_resource_type_field_migrate_delete_data"] ?></label>
-		<input type="checkbox" name="deletedata" value="true">
-	</div>
-	<div class="Question" >
-		<input type="hidden" name="submit" value="true">
-		<input type="submit" name="submit" value="<?php echo $lang["action-submit-button-label"] ?>">
-	</div>
-	</form>
-	<textarea id="migration_log" rows=20 cols=100 style="width: 100%; border: solid 1px;" ></textarea>
-        
-</div>
-<?php
-
-
-include_once "../../include/footer.php";
-
-if(getval("submit","") != "" && enforcePostRequest(false))
+function send_event_update($message,$progress,$url="")
     {
+    $output = array('message' => $message, 'progress' => $progress);
+    $output['url'] = $url;
+    echo "id: " . json_encode(time()) . PHP_EOL;
+    echo "data: " . json_encode($output) . PHP_EOL;
+    echo PHP_EOL;
+    // Added to force flush as finding a way to do this seems to have varied results
+    echo str_pad('',4096).PHP_EOL;
+    ob_flush();
+    flush();
+    }
+       
+if(getval("submit","") != "")
+    {
+    ob_start();
     
     $valid_fields = sql_array("SELECT ref value FROM resource_type_field WHERE type IN ('" . implode("','", $FIXED_LIST_FIELD_TYPES) . "')");
+    $messages = array();
     
+    if($showprogress)
+        {
+        header('Content-Type: text/event-stream');
+        }
+        
     if(!in_array($migrate_field,$valid_fields))
         {
-        ?>
-		<script>
-		jQuery('#migration_log').append('Invalid field specified. Only fixed type field types can be specified');
-		</script>
-		<?php
-		exit();
+        $messages[] = "Invalid field specified. Only fixed type field types can be specified";
+		//exit();
         }
-    
-	?>
-	<script>
-	<?php
+        
     $resdata = sql_query(
         "SELECT resource,
                 `value` 
            FROM resource_data 
           WHERE resource_type_field = '{$migrate_field}'
             AND `value` IS NOT NULL
-            AND `value` != ''"
+            AND `value` != ''
+            ORDER BY resource ASC"
     );
-    
+        
     // Get all existing nodes
     $existing_nodes = get_nodes($migrate_field, NULL, TRUE);
 	if($dryrun)
@@ -95,7 +75,22 @@ if(getval("submit","") != "" && enforcePostRequest(false))
 		// Set start value for dummy node refs 
 		$newnoderef = (count($existing_nodes) > 0) ? max(array_column($existing_nodes,"ref")) + 1: 0;
 		}
-		
+    
+	$migrated = 0;
+    $total = count($resdata);
+    $lastcompletion = 0;
+    $completion = 0;
+    $now = date(time());
+    
+    // Set up logging
+    $logfile = get_temp_dir(false,'') . "/migrate-data_" . $userref . "_" . md5($username . $now . $scramble_key) . ".txt";
+    $logurl = $baseurl . "/pages/download.php?tempfile=migrate-data_" . $userref . "_" . $now . ".txt";
+    $fp = fopen($logfile, 'a');
+    fwrite($fp, "<pre>Script started at " . date("Y-m-d H:i",time()) . PHP_EOL);
+    fwrite($fp, "Migrating data from text field '" . $field_info["title"] . "' ID #" . $migrate_field . PHP_EOL);
+    fclose($fp);
+    
+    // Process each data row
     foreach($resdata as $resdata_row)
         {
         // No need to process any further if no data is found set for this resource
@@ -104,10 +99,10 @@ if(getval("submit","") != "" && enforcePostRequest(false))
             continue;
             }
 
+        $logtext = "";
         $nodes_to_add = array();
         $resource = $resdata_row["resource"];
-		$log = array();
-        $log[] = "Checking data for resource id #" . $resource . "";
+        $logtext .= ($dryrun?"TESTING: ":"") . "Checking data for resource id #" . $resource . PHP_EOL;
 
         if($splitvalue != "")
             {
@@ -125,14 +120,12 @@ if(getval("submit","") != "" && enforcePostRequest(false))
                 {
                 continue;
                 }
-
-            $log[] = "- value: " . $data_value . "";
        
             $nodeidx = array_search($data_value,array_column($existing_nodes,"name"));
 
             if($nodeidx !== false)
                 {
-                $log[] = " - found matching node. ref:" . $existing_nodes[$nodeidx]["ref"] ;
+                $logtext .= ($dryrun?"TESTING: ":"") . " - Found matching field node option. ref:" . $existing_nodes[$nodeidx]["ref"] . PHP_EOL;
                 $nodes_to_add[] = $existing_nodes[$nodeidx]["ref"];      
                 }
             else
@@ -140,7 +133,7 @@ if(getval("submit","") != "" && enforcePostRequest(false))
                 if(!$dryrun)
 					{
 					$newnode = set_node(NULL, $migrate_field, escape_check($data_value), NULL, '',true);
-			        $log[] = " - Created new node. ref:" . $newnode ;
+			        $logtext .= " - New option added for '" . htmlspecialchars($data_value) . "' - ref: " . $newnode . PHP_EOL;
 					$nodes_to_add[] = $newnode;
 					$newnodecounter = count($existing_nodes);
 					$existing_nodes[$newnodecounter]["ref"] = $newnode;
@@ -149,48 +142,212 @@ if(getval("submit","") != "" && enforcePostRequest(false))
 				else 
 					{
 					$newnode = $newnoderef;
+			        $logtext .= ($dryrun?"TESTING: ":"") . " - New option added for '" . htmlspecialchars($data_value) . "' - ref: " . $newnoderef . PHP_EOL;
 					$newnodecounter = count($existing_nodes);
 					$existing_nodes[$newnodecounter]["ref"] = $newnoderef;
 					$existing_nodes[$newnodecounter]["name"] = $data_value;
 					$newnoderef++;							
 					}
                 }
-            }            
+            }           
         
         if(count($nodes_to_add) > 0)
             {
-            $log[] = "Adding nodes: " . implode(",", $nodes_to_add) . "";
+            $logtext .= ($dryrun?"TESTING: ":"") . "Adding nodes to resource ID #" . $resource . ": " . implode(",", $nodes_to_add) . PHP_EOL;
+
 			if(!$dryrun)
 				{
 				add_resource_nodes($resource,$nodes_to_add);
 				}
             }
-			                                            
-		foreach ($log as $logtext)
-			{
-			?>
-			jQuery('#migration_log').append("<?php echo ($dryrun?"TESTING: ":"") . str_pad($logtext,2048); ?>");
-			<?php
-			}
-			?>
-		<?php
-		ob_flush();flush();
+		    
+        $migrated++;
+        
+        $completion = floor($migrated/$total*100);        
+            
+        if($showprogress && $lastcompletion != $completion)
+            {               
+            send_event_update("Resource " . $migrated . "/" . $total . PHP_EOL, $completion,$logurl);
+            $lastcompletion = $completion;
+            }
+            
+        // Update log
+        $fp = fopen($logfile, 'a');
+        fwrite($fp, $logtext);
+        fclose($fp);
         }
 	
 	if($deletedata)
-		{?>
-		jQuery('#migration_log').append("\n<?php echo ($dryrun?"TESTING: ":"") ?>DELETING EXISTING DATA\n");
-		<?php
-		if(!$dryrun)
+		{
+        $logtext = ($dryrun?"TESTING: ":"") . "DELETING EXISTING DATA". PHP_EOL;
+        $fp = fopen($logfile, 'a');
+        fwrite($fp, $logtext);
+        fclose($fp);
+        if($showprogress)
+            {
+            send_event_update(($dryrun?"TESTING: ":"") . "DELETING EXISTING DATA". PHP_EOL, "100",$logurl);
+            }
+            
+        if(!$dryrun)
 			{
 			sql_query("delete from resource_data where resource_type_field='" . $migrate_field . "'");
 			sql_query("delete from resource_keyword where where resource_type_field='" . $migrate_field . "'");
 			}
 		}
-
-	?>
-	jQuery('#migration_log').append('\nCOMPLETED');
-	</script>
-	</div>
-	<?php
+        
+    $logtext = "Completed at " . date("Y-m-d H:i",time()) . ". " . $total . " rows migrated" . PHP_EOL;
+    // Update log
+    $fp = fopen($logfile, 'a');
+    fwrite($fp, $logtext);
+    fclose($fp);
+    
+    $completemessage = ($dryrun?"TESTING: ":"") . "Completed at " . date("Y-m-d H:i",time()) . ". " . $total . " rows migrated</pre>";
+    
+    // Send a message to the user
+    message_add($userref,$lang["admin_resource_type_field_migrate_data"] . ": " . $completemessage , $logurl);
+    
+    // Always send the completion event
+    if($showprogress)
+        {
+        send_event_update($completemessage . PHP_EOL, "100",$logurl);
+        }
+    else
+        {
+        echo json_encode(array("message"=>$completemessage,"url"=>$logurl));
+        }
+    exit();
     }
+    
+include_once "../../include/header.php";
+
+
+?>
+<div class="BasicsBox">
+	<p>    
+	<a href="<?php echo $backurl ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo LINK_CARET_BACK ?><?php echo $lang["back"]?></a>
+	</p>
+	<h1><?php echo $lang["admin_resource_type_field_migrate_data"] ?></h1>
+
+	<form method="post" class="FormWide" action="<?php echo $baseurl_short ?>pages/tools/migrate_data_to_fixed.php" onsubmit="start_task(this);return false;">
+     <?php generateFormToken("migrate_data_to_fixed"); ?>    
+	<div class="Question" >
+		<label for="field" ><?php echo $lang["field"] ?></label>
+        <?php if($migrate_field == 0)
+            {?>
+            <input class="medwidth" type="number" name="field" value="<?php echo $migrate_field ?>">
+            <?php }
+        else
+            {?>
+            <input type="hidden" name="field" value="<?php echo htmlspecialchars($migrate_field); ?>">
+            <div class="Fixed" id="field" ><?php echo htmlspecialchars($field_info["title"]) . " (" . htmlspecialchars($migrate_field) . ")"; ?></div>
+            <?php } ?>
+        <div class="clearerleft"> </div>
+	</div>
+	<div class="Question" >
+		<label for="splitchar" ><?php echo $lang["admin_resource_type_field_migrate_separator"] ?></label>
+		<input class="medwidth" type="text" name="splitchar" value=",">
+        <div class="clearerleft"> </div>
+	</div>
+	<div class="Question" >
+		<label for="dryrun" ><?php echo $lang["admin_resource_type_field_migrate_dry_run"] ?></label>
+		<input class="medwidth" type="checkbox" name="dryrun" value="true">
+        <div class="clearerleft"> </div>
+	</div>
+	<div class="Question" >
+		<label for="deletedata" ><?php echo $lang["admin_resource_type_field_migrate_delete_data"] ?></label>
+		<input class="medwidth" type="checkbox" name="deletedata" value="true">
+        <div class="clearerleft"> </div>
+	</div>
+    <div class="Question" >
+		<label for="progress"><?php echo $lang["progress"] ?></label>
+		<div class="Fixed" id="progress" >0%</div>
+        <div class="clearerleft"> </div>
+	</div>
+    <div class="Question" >
+		<label for="progress_log"><?php echo $lang["status"] ?></label>
+        <div class="Fixed medwidth" id="progress_log" ></div>
+        <div class="clearerleft"> </div>
+	</div>
+    <div class="Question" >
+		<label for="view_log"><?php echo $lang["log"] ?></label>
+        <div class="Fixed medwidth" id="view_log" ><a style="display:none;" id="log_url" href="#" target="_blank"><?php echo $lang["action-log"] ?></a></div>
+        <div class="clearerleft"> </div>
+	</div>
+    <div class="Question" >
+		<input type="hidden" name="submit" value="true">
+		<input type="submit" name="submit" value="<?php echo $lang["action-submit-button-label"] ?>">
+        <div class="clearerleft"> </div>
+	</div>
+    
+	<div class="clearerleft"> </div>
+    
+	</form>
+    <script>
+    //var taskurl = '<?php echo $baseurl_short ?>pages/tools/migrate_data_to_fixed.php'';
+        function start_task(form)
+        <?php        
+        if($showprogress)
+            {?>
+            {
+            source = new EventSource(form.action + '?' + jQuery(form).serialize());
+            jQuery('#progress_log').html('Running...\n');
+            source.addEventListener('message' , function(e) 
+                {
+                var result = JSON.parse( e.data );
+                add_log(result.message);                
+                jQuery('#progress').html(result.progress + '%');
+                jQuery('#log_url').attr('href',result.url);
+                jQuery('#log_url').show();
+                    
+                if(e.data.search('Completed') != -1)
+                    {               
+                    add_log(result.message);
+                    source.close();
+                    }
+                
+                });
+                 
+            source.addEventListener('error' , function(e)
+                {
+                jQuery('#progress_log').append('<?php echo $lang["error"]; ?> ' . result.message);
+                source.close();
+                });
+                
+            function add_log(message)
+                {
+                jQuery('#progress_log').html(message);
+                jQuery('#progress_log').scrollTop(jQuery('#progress_log').prop('scrollHeight'));
+                } 
+            }
+            <?php
+            }
+        else
+            {
+            ?>
+            {
+            jQuery('#progress_log').html('Running. Please do not leave this page. You will be notified when the migration has completed.\n');
+            formdata = jQuery(form).serialize();
+            jQuery.ajax({
+                url: form.action + '?' + formdata,
+                dataType: "json"
+                }).done(function(data)
+                    {
+                    jQuery('#progress_log').html(data.message);
+                    jQuery('#progress').html("100%");
+                    jQuery('#log_url').attr('href',data.url);
+                    jQuery('#log_url').show();
+                    });
+                  
+            return false;
+            }           
+            <?php
+            } ?>       
+    </script>
+        
+</div>
+<?php
+
+
+include_once "../../include/footer.php";
+
+
