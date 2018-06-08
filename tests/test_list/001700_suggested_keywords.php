e4b1b4d2818889b524989255a265f4ee14d25a8c
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-06-08 14:53:10 +0000

    FIX: [8.5] Corrected node field restriction code in get_suggested_keywords function and added unit test to check permissions are honoured [t16712]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/8.5@11556 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ tests/test_list/001700_suggested_keywords.php
@@ -0,0 +1,54 @@
+<?php
+include_once(__DIR__ . '/../../include/search_functions.php');
+
+// Check that get_Suggested keywords does not return data from fields that user does not have access to
+
+if (php_sapi_name()!=="cli") {exit("This utility is command line only.");}
+
+$resourcea=create_resource(1,0);
+$resourceb=create_resource(1,0);
+$hidefield = create_resource_type_field("Sensitive data",0,FIELD_TYPE_TEXT_BOX_SINGLE_LINE,"sensitive",true);
+
+// Set text for resource
+$sensitivename = "Lordlucan";
+update_field($resourcea,$hidefield,$sensitivename);
+
+// Ensure that field is not visible to user
+$userpermissions[]='f-' . $hidefield;
+
+// Set variables that could cause false results
+unset($hidden_fields_cache);
+$autocomplete_search_min_hitcount = 1;
+
+$input = substr($sensitivename,0,5);
+$keywords=get_suggested_keywords($input);
+if(in_array(strtolower($sensitivename),$keywords))
+   {
+   echo " - Subtest A ";
+   return false;
+   }
+
+// Same test but for node field
+$hidenodefield = create_resource_type_field("Sensitive node data",0,FIELD_TYPE_CHECK_BOX_LIST,"sensitivenode",true);
+$sensitivenodename = "Chupacabra";
+$sensitivenode = set_node(null, $hidenodefield, $sensitivenodename, null, null);
+add_resource_nodes($resourceb,array($sensitivenode));
+
+// Ensure that this new field is not visible to user
+$userpermissions[]='f-' . $hidenodefield;
+
+// Set variables that could cause false results
+unset($hidden_fields_cache);
+$autocomplete_search_min_hitcount = 1;
+
+$input = substr($sensitivenodename,0,5);
+$keywords=get_suggested_keywords($input);
+
+if(in_array(strtolower($sensitivenodename),$keywords))
+   {
+   echo " - Subtest B ";
+   return false;
+   }
+
+return true;
+
