4663889e4f49b1286aa8148e7f7d6db2d4781f55
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-06-22 12:53:24 +0000

    NEW: Added pages/tools/fix_resource_field_column.php to correct/populate resource 'fieldXX' columns [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11627 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ pages/tools/fix_resource_field_column.php
@@ -0,0 +1,43 @@
+<?php
+include "../../include/db.php";
+include_once "../../include/general.php";
+include_once "../../include/resource_functions.php";
+include_once "../../include/search_functions.php";
+include_once "../../include/authenticate.php";
+if(!checkperm("a")){exit("Access denied");}
+
+$field = getval("field",0,true);
+
+if($field > 0)
+    {
+    $fieldinfo = get_resource_type_field($field);
+    $allresources = sql_array("SELECT ref value from resource where ref>0 order by ref ASC",0);
+    if(in_array($fieldinfo['type'],$NODE_FIELDS))
+            {
+            foreach($allresources as $resource)
+                {
+                $resnodes = get_resource_nodes($resource, $field, true);
+                $resvals = array_column($resnodes,"name");
+                $resdata = implode(",",$resvals);
+                $value = truncate_join_field_value(strip_leading_comma($resdata));
+                sql_query("update resource set field" . $field . "='".escape_check($value)."' where ref='$resource'");
+                echo "Updated resource " . $resource . ". Value: " . $value . "<br />";
+                }
+            }
+    else
+            {
+            foreach($allresources as $resource)
+                {
+                $resdata = get_data_by_field($resource,$field);
+                $value = truncate_join_field_value(strip_leading_comma($resdata));
+                sql_query("update resource set field" . $field . "='".escape_check($value)."' where ref='$resource'");
+                echo "Updated resource " . $resource . ". Value: " . $value . "<br />";
+                }
+            
+            }
+
+     }
+else
+    {
+    exit("Please pass a valid metadata field ref as the 'field' parameter in the query string e.g. https://yoururl.com/pages/tools/fix_resource_field_column.php?field=8");    
+    }
