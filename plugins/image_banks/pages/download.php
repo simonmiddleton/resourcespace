265ef481bcc4fd5ac6a0604e399748e8dd060b7a
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-08-09 14:48:28 +0000

    NEW: log user attempt to download a file from local system [q10108][image_banks plugin][WORK_IN_PROGRESS]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11779 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/image_banks/pages/download.php
+++ plugins/image_banks/pages/download.php
@@ -8,6 +8,16 @@ ob_end_clean();
 
 $id = getval("id", ""); # ID can be a string or integer based on how each provider implemented it
 $file_path = getval("file", "");
+
+// Prevent this from being used to get access to other files on this server
+// For external URL resources this should return false.
+if(file_exists($file_path))
+    {
+    $log_activity_note = str_replace("%FILE", $file_path, $lang["image_banks_local_download_attempt"]);
+    log_activity($log_activity_note, LOG_CODE_SYSTEM, null, 'user', null, null, null, null, $userref, false);
+    exit();
+    }
+
 $file_ext = pathinfo($file_path, PATHINFO_EXTENSION);
 
 $file = file_get_contents($file_path);
