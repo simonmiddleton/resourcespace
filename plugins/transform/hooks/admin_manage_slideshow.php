1fe8e2002c7ac0c2f3a7c6f5f6969f821da6f430
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-08-10 15:59:42 +0000

    FIX: code was calculating incorrectly the new slideshow ID [q10131]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11791 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/transform/hooks/admin_manage_slideshow.php
+++ plugins/transform/hooks/admin_manage_slideshow.php
@@ -6,9 +6,7 @@ function HookTransformAdmin_manage_slideshowRender_new_element_for_manage_slides
     $return_to_url = $baseurl . '/pages/admin/admin_manage_slideshow.php';
 
     // Calculate the next slideshow image ID (ie. filename will be ID.jpg)
-    $new_slideshow_id = 1;
-    end($slideshow_files);
-    $new_slideshow_id = key($slideshow_files) + 1;
+    $new_slideshow_id = count($slideshow_files) + 1;
     ?>
     <div id="add_new_slideshow" class="Question">
         <label></label>
