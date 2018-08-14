d5b05017cdbb7a385b9e6a0f43d321e408a82a6f
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-08-14 08:49:03 +0000

    CHG: Login background image enabled for new installs [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11800 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/config.new_installs.php
+++ include/config.new_installs.php
@@ -22,7 +22,7 @@ $home_slideshow_width=1400;
 $home_slideshow_height=900;
 
 $homeanim_folder = 'filestore/system/slideshow';
-
+$login_background=true;
 
 $themes_simple_view=true;
 $themes_category_split_pages=true;
