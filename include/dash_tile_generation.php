2ba39fb4776556d69dfe35282d868be1dc2c5213
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-08-15 15:15:36 +0000

    FIX: Fixed count error for dash_tile_generation.php [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11811 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/dash_tile_generation.php
+++ include/dash_tile_generation.php
@@ -301,11 +301,15 @@ function tile_search_thumbs($tile,$tile_id,$tile_width,$tile_height,$promoted_im
 	$sort = isset($search_string["sort"]) ? $search_string["sort"] : "";
 	$tile_search=do_search($search,$restypes,$order_by,$archive,$count,$sort,false,0,false,false,"",false,false);
 	$found_resources=true;
-	$count=count($tile_search);
-	if(!isset($tile_search[0]["ref"]))
+	if(!is_array($tile_search))
 		{
 		$found_resources=false;
 		$count=0;
+		}
+	else
+		{
+		$found_resources=true;
+		$count=count($tile_search);
 		}
 
 	if($found_resources)
@@ -528,10 +532,15 @@ function tile_search_blank($tile,$tile_id,$tile_width,$tile_height)
 	$archive = isset($search_string["archive"]) ? $search_string["archive"] : "";
 	$sort = isset($search_string["sort"]) ? $search_string["sort"] : "";
 	$tile_search=do_search($search,$restypes,$order_by,$archive,$count,$sort,false,0,false,false,"",false,false);
-	$count=count($tile_search);
-	if(!isset($tile_search[0]["ref"]))
+	if(!is_array($tile_search))
 		{
+		$found_resources=false;
 		$count=0;
+		}
+	else
+		{
+		$found_resources=true;
+		$count=count($tile_search);
 		}
 	
 	$icon = ""; 
