a248a87565c96df689a4d3aaf4fbebaeecdaec8e
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-08-22 14:54:46 +0000

    FIX: [RC 8.6] collection bar not behaving as expected when adding new resources to a collection (e.g. new resources should appear at the start of the list and if a resource was already in the collection, it should be moved to the first position). Removed the asadded order by since this was exactly the same as resourceid which is being used by Recently added (also configurable via order_by_resource_id) [t17864][t17732]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/8.6_RC@11866 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_do.php
+++ include/search_do.php
@@ -81,8 +81,7 @@ function do_search(
     $orig_order=$order_by;
     global $date_field;
     $order = array(
-        "asadded"         => "r.ref $sort",  
-        "collection"      => "c.sortorder $sort,c.date_added DESC,r.ref DESC",
+        "collection"      => "c.sortorder $sort,c.date_added $sort,r.ref $sort",
         "relevance"       => "score $sort, user_rating $sort, total_hit_count $sort, field$date_field $sort,r.ref $sort",
         "popularity"      => "user_rating $sort,total_hit_count $sort,field$date_field $sort,r.ref $sort",
         "rating"          => "r.rating $sort, user_rating $sort, score $sort,r.ref $sort",
