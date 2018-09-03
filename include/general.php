7a5b8b758786e62e94226a4ec637f5926dc53ee6
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-09-03 13:21:07 +0000

    FIX: Added constant RS_TEST_MODE to test.php to indicate test mode as r11847 broke unit test due to a permissions check override [q10386]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11926 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/general.php
+++ include/general.php
@@ -7029,7 +7029,7 @@ function findDuplicates(array $data, $search)
 
 function metadata_field_view_access($field)
     {
-    return (PHP_SAPI == 'cli' || ((checkperm("f*") || checkperm("f" . $field)) && !checkperm("f-" . $field)));
+    return ((PHP_SAPI == 'cli' && !defined("RS_TEST_MODE")) || ((checkperm("f*") || checkperm("f" . $field)) && !checkperm("f-" . $field)));
     }
 
 
