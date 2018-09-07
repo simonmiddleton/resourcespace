d4ccd644e3bd388422ed4cd7c732d21b78be98a4
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-09-07 11:01:27 +0000

    FIX: Redirect failing when accessing password protected share [q10236]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@11985 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/general.php
+++ include/general.php
@@ -4096,7 +4096,7 @@ function check_access_key($resource,$key)
     
     # Option to plugin in some extra functionality to check keys
     if (hook("check_access_key","",array($resource,$key))===true) {return true;}
-    global $external_share_view_as_internal, $is_authenticated, $baseurl;
+    global $external_share_view_as_internal, $is_authenticated, $baseurl, $baseurl_short;
         if($external_share_view_as_internal && (isset($_COOKIE["user"]) && validate_user("session='" . escape_check($_COOKIE["user"]) . "'", false) && !(isset($is_authenticated) && $is_authenticated))){return false;} // We want to authenticate the user if not already authenticated so we can show the page as internal
     
     $keys=sql_query("select user,usergroup,expires,password_hash from external_access_keys where resource='$resource' and access_key='$key' and (expires is null or expires>now())");
@@ -4114,7 +4114,7 @@ function check_access_key($resource,$key)
             $check = check_share_password($key,"",$share_access_cookie);
             if(!$check)
                 {
-                $url = generateURL($baseurl . "/pages/share_access.php",array("k"=>$key,"resource"=>$resource,"return_url" => $baseurl . (isset($_SERVER["REQUEST_URI"]) ? urlencode($_SERVER["REQUEST_URI"]) : "/r=" . $resource . "&k=" . $key)));
+                $url = generateURL($baseurl . "/pages/share_access.php",array("k"=>$key,"resource"=>$resource,"return_url" => $baseurl . (isset($_SERVER["REQUEST_URI"]) ? urlencode(str_replace($baseurl_short,"/",$_SERVER["REQUEST_URI"])) : "/r=" . $resource . "&k=" . $key)));
                 redirect($url);
                 exit();
                 }
