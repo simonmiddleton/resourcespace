a9d476e0890a46301b450cbdf3ef1100ebf583fc
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-09-10 21:40:52 +0000

    FIX: Adjusted code to strip ajax when redirecting to login page after auto logout [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12017 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/authenticate.php
+++ include/authenticate.php
@@ -188,7 +188,7 @@ if (!$valid && !isset($system_login))
 	$_SERVER['REQUEST_URI'] : $_SERVER['SCRIPT_NAME'] . (( isset($_SERVER
 	['QUERY_STRING']) ? '?' . $_SERVER['QUERY_STRING'] : '')));
     $path=$_SERVER["REQUEST_URI"];
-    $path=str_replace("ajax","ajax_disabled",$path);# Disable forwarding of the AJAX parameter if this was an AJAX load, otherwise the redirected page will be missing the header/footer.
+    $path=str_replace("ajax=","ajax_disabled=",$path);# Disable forwarding of the AJAX parameter if this was an AJAX load, otherwise the redirected page will be missing the header/footer.
 	?>
 	<script type="text/javascript">
 	<?php 
