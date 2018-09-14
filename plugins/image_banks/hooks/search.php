879c23a9d4bf4373ddc8ca0f71f3836adc592f9b
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-09-14 16:33:45 +0000

    FIX: detecting what is considered an image bank search [t18089]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12053 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/image_banks/hooks/search.php
+++ plugins/image_banks/hooks/search.php
@@ -14,11 +14,12 @@ function HookImage_banksSearchSearchaftersearchcookie()
     // Allow pixabay search using saved params using the Search results link that appears in the header links
     // This is not passing anything in the params other than ajax argument
     parse_str($_SERVER['QUERY_STRING'], $qs_params);
+    debug("image_banks_search_hook: \$_SERVER['QUERY_STRING'] = {$_SERVER['QUERY_STRING']}");
     if(
         $image_bank_provider_id > 0
         && (
-                // Search results
-                count($qs_params) == 2
+                // Search results (for image bank)
+                count($qs_params) == 2 && !isset($qs_params['search'])
                 // User searched external IB providers from simple search
                 || $posting
                 // Normal search in IB (e.g changing per page or going through pages)
@@ -29,9 +30,13 @@ function HookImage_banksSearchSearchaftersearchcookie()
         $search_image_banks = true;
         }
 
+    debug("image_banks_search_hook: ");
     debug("image_banks_search_hook: \$offset = {$offset}");
     debug("image_banks_search_hook: \$image_bank_provider_id = {$image_bank_provider_id}");
+    debug("image_banks_search_hook: count(\$qs_params) == 2 ==> " . (count($qs_params) == 2 ? 'true': 'false'));
     debug("image_banks_search_hook: \$posting = " . ($posting ? "true" : "false"));
+    debug("image_banks_search_hook: (count(\$qs_params) > 2 && \$search_image_banks) ==> " . ((count($qs_params) > 2 && $search_image_banks) ? 'true': 'false'));
+    debug("image_banks_search_hook: ===============================");
     debug("image_banks_search_hook: \$search_image_banks = " . ($search_image_banks ? "true" : "false"));
 
     if(!$search_image_banks)
