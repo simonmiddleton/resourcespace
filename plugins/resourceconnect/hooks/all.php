5cafaec9b26b817ac7f3f79f545cae8be368ddef
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-09-19 14:50:14 +0000

    FIX: Updated prototype.js to jQuery [t18213]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12082 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/resourceconnect/hooks/all.php
+++ plugins/resourceconnect/hooks/all.php
@@ -104,8 +104,8 @@ function HookResourceConnectAllSearchfiltertop()
 	if (!checkperm("resourceconnect")) {return false;}
 	?>
 <script>
-  $( function() {
-    $( document ).tooltip();
+  jQuery(document).ready(function(){
+    jQuery( document ).tooltip();
   } );
   </script>
 	<div class="SearchItem ResourceConnectSearch"><?php echo $lang["resourceconnect_search_database"];?>&nbsp;<a href="#" onClick="styledalert('<?php echo $lang["resourceconnect_search_database"] ?>','<?php echo $lang["resourceconnect_search_info"] ?>');" title="<?php echo $lang["resourceconnect_search_info"] ?>"><i class="fa fa-info-circle"></i></a><br />
