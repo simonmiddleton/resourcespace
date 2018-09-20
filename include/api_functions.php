2da1e1ffeed6c5c59aeec0698b775ea1a3de60cc
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-09-20 14:37:58 +0000

    FIX: Issue with api incorrectly populating default values [t18082]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12090 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/api_functions.php
+++ include/api_functions.php
@@ -47,24 +47,34 @@ function execute_api_call($query)
     
     // Construct an array of the real params, setting default values as necessary
     $setparams = array();
-    $n=1;
+    $n = 0;    
     $fct = new ReflectionFunction("api_" . $function);
     foreach ($fct->getParameters() as $fparam)
         {
-        if (array_key_exists("param" . $n,$params))
+        $paramkey = $n + 1;
+        debug ("API Checking for parameter " . $fparam->getName() . " (param" . $paramkey . ")");
+        if (array_key_exists("param" . $paramkey,$params))
             {
-            $setparams[$n] = $params["param" . $n];
+            debug ("API " . $fparam->getName() . " -   value has been passed : '" . $params["param" . $paramkey] . "'");
+            $setparams[$n] = $params["param" . $paramkey];
             }
         
-        if ($fparam->isOptional() && $params["param" . $n] == '')
+        elseif ($fparam->isOptional())
             {
             // Set default value if nothing passed e.g. from API test tool
+            debug ("API " . $fparam->getName() . " -  setting default value = '" . $fparam->getDefaultValue() . "'");
             $setparams[$n] = $fparam->getDefaultValue();
+            }
+        else
+            {
+             // Set as empty
+            debug ("API " . $fparam->getName() . " -  setting null value = '" . $fparam->getDefaultValue() . "'");
+            $setparams[$n] = "";    
             }
         $n++;
         }
     
-    debug("API - calling api_" . $function);        
+    debug("API - calling api_" . $function);
     $result = call_user_func_array("api_" . $function, $setparams);
     return json_encode($result,(defined('JSON_PRETTY_PRINT')?JSON_PRETTY_PRINT:0));
     }
