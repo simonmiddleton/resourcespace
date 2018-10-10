cf7025f470a3b406c5e0356642a050d5c438787c
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-10-10 11:29:02 +0000

    FIX: [8.6] ResourceBot no longer interferes with Lightbox/CentralSpace [t18350]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/8.6@12155 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/resourcebot/hooks/all.php
+++ plugins/resourcebot/hooks/all.php
@@ -6,7 +6,7 @@ function HookResourcebotAllAdditionalheaderjs()
     if (isset($useremail) && $useremail!="")
         {
         ?>
-        <script src="https://hive.montala.com/ResourceBot/bot.php?uid=<?php echo md5($useremail) ?>" type="text/javascript" />
+        <script src="https://hive.montala.com/ResourceBot/bot.php?uid=<?php echo md5($useremail) ?>" type="text/javascript"></script>
         <?php
         }
     return true;
