2ac8ed7983feca1557eb11da09e06aa16075cfab
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-10-10 09:51:57 +0000

    FIX: Fix styling of some select elements on the plugin setup pages and remove box shadow for no preview icons [t18236]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12152 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/view.php
+++ pages/view.php
@@ -1075,7 +1075,7 @@ else
     {
     ?>
     <div id="previewimagewrapper">
-        <img src="<?php echo $baseurl ?>/gfx/<?php echo get_nopreview_icon($resource["resource_type"],$resource["file_extension"],false)?>" alt="" class="Picture" style="border:none;" id="previewimage" />
+        <img src="<?php echo $baseurl ?>/gfx/<?php echo get_nopreview_icon($resource["resource_type"],$resource["file_extension"],false)?>" alt="" class="Picture NoPreview" style="border:none;" id="previewimage" />
     <?php
     hook('aftersearchimg', '', array($ref));
     if(isset($previewcaption))
