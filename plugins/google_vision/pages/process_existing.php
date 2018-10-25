90d34bba4c28408b32169aeba8a2ef157005136b
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-10-25 10:26:59 +0000

    NEW: ability to process resources using Google Vision per collections [q10445]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12237 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/google_vision/pages/process_existing.php
+++ plugins/google_vision/pages/process_existing.php
@@ -1,8 +1,9 @@
 <?php
-// Do the include and authorization checking ritual
 include '../../../include/db.php';
 include_once '../../../include/general.php';
 include_once '../../../include/resource_functions.php';
+include_once '../../../include/collections_functions.php';
+
 if('cli' != PHP_SAPI)
     {
     header('HTTP/1.1 401 Unauthorized');
@@ -10,12 +11,57 @@ if('cli' != PHP_SAPI)
     }
 include __DIR__ . "/../include/google_vision_functions.php";
 
+logScript("Google Vision plugin - processing_existing.php script...");
 
-$resources=sql_array("SELECT ref value FROM resource WHERE (google_vision_processed IS NULL OR google_vision_processed=0) AND ref>0 AND has_image=1 AND resource_type IN ('" . join("','",$google_vision_restypes) . "')");
+$collections = array();
+
+$cli_short_options = 'c:';
+$cli_long_options  = array('collection:');
+$cli_options = getopt($cli_short_options, $cli_long_options);
+if($cli_options !== false)
+    {
+    foreach($cli_options as $option_name => $option_value)
+        {
+        if(in_array($option_name, array('c', 'collection')))
+            {
+            if(is_array($option_value))
+                {
+                $collections = $option_value;
+                continue;
+                }
+
+            $collections[] = $option_value;
+            }
+        }
+    }
+
+if(empty($collections))
+    {
+    $sql_gv_restypes = join("','", $google_vision_restypes);
+    $resources = sql_array("
+        SELECT ref AS `value`
+          FROM resource
+         WHERE (google_vision_processed IS NULL OR google_vision_processed = 0)
+           AND ref > 0
+           AND has_image = 1
+           AND resource_type IN ('{$sql_gv_restypes}')");
+    }
+else
+    {
+    foreach($collections as $collection)
+        {
+        if(!is_numeric($collection))
+            {
+            logScript("Warning: Collection ID '{$collection}' is not numeric!");
+            continue;
+            }
+
+        $resources = get_collection_resources($collection);
+        }
+    }
 
 foreach ($resources as $resource)
     {
-    echo "\n\nProcessing resource " . $resource;
-    google_visionProcess($resource,true);
+    logScript("Processing resource #{$resource}");
+    google_visionProcess($resource, true);
     }
-
