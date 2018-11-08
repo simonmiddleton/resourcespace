dd82713cf5311e1bbe7b38f9563f7fc99fac8475
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-11-08 14:42:47 +0000

    FIX: Errors in geo_setcoords_from_country.php when country empty or unknown [t18520]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12306 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/geo_setcoords_from_country.php
+++ pages/tools/geo_setcoords_from_country.php
@@ -69,10 +69,11 @@ else
         
 function update_country_coords($refs,$latlong) 
 {
-sql_query("update resource set geo_lat='" . $latlong[0] . "', geo_long='" . $latlong[1] . "' " . 
-"where ref in('" . join("','",$refs) . "')");
-// echo "update resource set geo_lat='" . $latlong[0] . "', geo_long='" . $latlong[1] . "' " . 
-//      "where ref in('" . join("','",$refs) . "')";
+if(count($latlong)==2)
+    {
+    sql_query("update resource set geo_lat='" . $latlong[0] . "', geo_long='" . $latlong[1] . "' " . 
+    "where ref in('" . join("','",$refs) . "')");
+    }
 }
 
 function fetch_country_coords($country_name,$codes,$coords)
