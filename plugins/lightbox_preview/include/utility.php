decbfe0c1d194810300d05a3dbd7b92c3d74ae14
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-11-09 11:40:28 +0000

    FIX: Lightbox was trying to use the scr image for previews without checking the user had permissions. [t18433]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12310 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/lightbox_preview/include/utility.php
+++ plugins/lightbox_preview/include/utility.php
@@ -42,7 +42,7 @@ function getPreviewURL($resource, $alternative = -1, $page = 1)
 
 	// Try 'scr' first
 	$url = getPreviewURLForType($resource, 'scr', $alternative, $page);
-	if ($url == false)
+	if ($url == false || !resource_download_allowed($resource,'scr',$resource['resource_type'],$alternative))
 		{
 		// and then 'pre'
 		$url = getPreviewURLForType($resource, 'pre', $alternative, $page);
