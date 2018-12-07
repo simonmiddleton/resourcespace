b29e10f14124e690141c8ebdd8e90b5047f56157
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-12-07 15:50:31 +0000

    FIX: \!contributionsX special search [q10092]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12433 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_functions.php
+++ include/search_functions.php
@@ -1361,7 +1361,16 @@ function search_special($search,$sql_join,$fetchrows,$sql_prefix,$sql_suffix,$or
         
         $select=str_replace(",rca.access group_access,rca2.access user_access ",",null group_access, null user_access ",$select);
         $sql=$sql_prefix . "SELECT DISTINCT r.hit_count score, $select FROM resource r $sql_join WHERE created_by='" . $cuser . "' AND r.ref > 0 AND $sql_filter GROUP BY r.ref ORDER BY $order_by" . $sql_suffix;
-        return $returnsql ? $sql : sql_query(resource_table_joins_sql($joins, $sql), false, $fetchrows);
+
+        if($returnsql)
+            {
+            return $sql;
+            }
+
+        $sql = str_replace("ORDER BY {$order_by}", '', $sql);
+        $resource_table_joins_order_by = str_replace('r.ref', 'ss.ref', $order_by);
+
+        return sql_query(resource_table_joins_sql($joins, $sql, $resource_table_joins_order_by), false, $fetchrows);
         }
     
     # Search for resources with images
