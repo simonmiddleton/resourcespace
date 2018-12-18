43d5e63db7eded3200b03bdcf9c79ff4fa191943
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2018-12-18 12:03:53 +0000

    FIX: CSRF triggering bad requests [t18569]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12464 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/general.php
+++ include/general.php
@@ -6967,7 +6967,7 @@ function generateCSRFToken($session_id, $form_id)
         "form_id"   => $form_id
     ));
 
-    return rsEncrypt($data, $session_id);
+    return urlencode(rsEncrypt($data, $session_id));
     }
 
 /**
@@ -6992,7 +6992,7 @@ function isValidCSRFToken($token_data, $session_id)
         return false;
         }
 
-    $plaintext = rsDecrypt($token_data, $session_id);
+    $plaintext = rsDecrypt(urldecode($token_data), $session_id);
 
     if($plaintext === false)
         {
