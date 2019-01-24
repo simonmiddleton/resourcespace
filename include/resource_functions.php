aa058b4c8376fde86065ba0cf97dbd943e5692df
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-01-24 10:55:30 +0000

    FIX: Extra escape filepath in get_page_count() [q10656]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12579 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -4252,7 +4252,7 @@ function get_page_count($resource,$alternative=-1)
         {
         $command = $exiftool_fullpath;
     	
-        $command=$command." -sss -pagecount $file";
+        $command=$command." -sss -pagecount " . escapeshellarg($file);
         $output=run_command($command);
         $pages=str_replace("Page Count","",$output);
         $pages=str_replace(":","",$pages);
