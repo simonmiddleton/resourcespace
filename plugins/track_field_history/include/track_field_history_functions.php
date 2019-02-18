a5fd6970d187b65d2f41c2e2f24ec665e003d327
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-02-18 11:29:32 +0000

    FIX: track_field_history_get_field_log() not following coding standards for quoting and escaping [t19161]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12726 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/track_field_history/include/track_field_history_functions.php
+++ plugins/track_field_history/include/track_field_history_functions.php
@@ -3,19 +3,19 @@ if (!function_exists('track_field_history_get_field_log')) {
     
     function track_field_history_get_field_log($resource_id, $field_id) {
     
-        $query = sprintf('
+        $query = sprintf("
                    SELECT resource_log.date AS date,
                           IFNULL(user.fullname, user.username) AS user,
                           resource_log.diff
                      FROM resource_log
                 LEFT JOIN user ON user.ref = resource_log.user
-                    WHERE (type = "e" OR type="m")
-                      AND resource = %d
-                      AND resource_type_field = %d
+                    WHERE (type = 'e' OR type='m')
+                      AND resource = '%d'
+                      AND resource_type_field = '%d'
                  ORDER BY resource_log.date DESC;
-            ',
-            $resource_id,
-            $field_id
+            ",
+            escape_check($resource_id),
+            escape_check($field_id)
         );
         return sql_query($query);
 
