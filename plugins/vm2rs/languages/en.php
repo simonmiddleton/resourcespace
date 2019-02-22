f21de27a52e83d9b74b254c32085b4cd68f7d791
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-02-22 10:57:32 +0000

    NEW: Added new plugin vm2rs to show Vimeo videos on the resource view page similar to the yt2rs plugin. Credit to Tony W, Mariano B and OMF. [t19283]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12766 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/vm2rs/languages/en.php
@@ -0,0 +1,10 @@
+<?php
+# English
+# Language File for the vm2rs Plugin
+# -------
+#
+#
+$lang['vm2rs_configuration']="Please state the field id that will support the Vimeo url";
+$lang['vm2rs_field_id_l']="Field ID (Vimeo path)";
+$lang['vm2rs_thumb']="Thumbnail Path";
+
