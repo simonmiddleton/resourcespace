727d383441d9a9101651b351ba252be0917a21e5
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-02-25 10:59:55 +0000

    FIX: [9.0 RC] Regression test - Search contributed by [t19255]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.0_RC@12774 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_functions.php
+++ include/search_functions.php
@@ -1580,7 +1580,14 @@ function search_special($search,$sql_join,$fetchrows,$sql_prefix,$sql_suffix,$or
             }
             
         $sql=$sql_prefix . "SELECT DISTINCT r.hit_count score, $select FROM resource r $sql_join WHERE r.ref > 0 AND $sql_filter GROUP BY r.ref ORDER BY $order_by" . $sql_suffix;
-        return $returnsql ? $sql : sql_query(resource_table_joins_sql($joins, $sql), false, $fetchrows);
+        if($returnsql)
+            {
+            return $sql;
+            }
+        $sql = str_replace("ORDER BY {$order_by}", '', $sql);
+        $resource_table_joins_order_by = str_replace('r.ref', 'ss.ref', $order_by);
+
+        return sql_query(resource_table_joins_sql($joins, $sql, $resource_table_joins_order_by), false, $fetchrows);
         }
 
     # Within this hook implementation, set the value of the global $sql variable:
