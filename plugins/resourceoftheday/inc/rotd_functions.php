117756959a57ec2a90c64c3ff5084c6e49e236b9
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-02-27 16:16:25 +0000

    FIX: [9.0 RC] Added is numeric check to Resource of the day functions [t19271]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.0_RC@12804 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/resourceoftheday/inc/rotd_functions.php
+++ plugins/resourceoftheday/inc/rotd_functions.php
@@ -4,14 +4,16 @@ function get_resource_of_the_day()
     {
     global $rotd_field;
 
-    # Search for today's resource of the day.
-    $rotd = sql_value("SELECT resource value FROM resource_data WHERE resource>0 AND resource IN (SELECT ref FROM resource WHERE archive=0 AND access=0) AND resource_type_field=$rotd_field AND value LIKE '" . date("Y-m-d") . "%' limit 1;",0);
-    if ($rotd!=0) {return $rotd;} # A resource was found?
+    if(is_numeric($rotd_field))
+        {
+        # Search for today's resource of the day.
+        $rotd = sql_value("SELECT resource value FROM resource_data WHERE resource>0 AND resource IN (SELECT ref FROM resource WHERE archive=0 AND access=0) AND resource_type_field=$rotd_field AND value LIKE '" . date("Y-m-d") . "%' limit 1;",0);
+        if ($rotd!=0) {return $rotd;} # A resource was found?
 
-    # No resource of the day today. Pick one at random, using today as a seed so the same image will be used all of the day.
-    $rotd = sql_value("SELECT resource value FROM resource_data WHERE resource>0 AND resource IN (SELECT ref FROM resource WHERE archive=0 AND access=0) AND resource_type_field=$rotd_field AND length(value)>0 ORDER BY rand(" . date("d") . ") limit 1;",0);
-    if ($rotd!=0) {return $rotd;} # A resource was found now?
-
+        # No resource of the day today. Pick one at random, using today as a seed so the same image will be used all of the day.
+        $rotd = sql_value("SELECT resource value FROM resource_data WHERE resource>0 AND resource IN (SELECT ref FROM resource WHERE archive=0 AND access=0) AND resource_type_field=$rotd_field AND length(value)>0 ORDER BY rand(" . date("d") . ") limit 1;",0);
+        if ($rotd!=0) {return $rotd;} # A resource was found now?
+        }
     # No resource of the day fields are set. Return to default slideshow functionality.
     return false;
     }
