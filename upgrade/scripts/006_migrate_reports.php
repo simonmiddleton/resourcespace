fbb26a7cddd4173cd85223c4cb76d545122871da
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-03-26 15:51:20 +0000

    FIX: Remove a lot of code that attempts to rewrite SQL to use newer DB structures - uses a new upgrade script to permanently alter the SQL instead. Updated the base scripts in dbstruct so they work verbatim. [t19557]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@12959 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ upgrade/scripts/006_migrate_reports.php
@@ -0,0 +1,60 @@
+<?php
+include_once __DIR__ . "/../../include/db.php";
+include_once __DIR__ . "/../../include/general.php";
+include_once __DIR__ . "/../../include/resource_functions.php";
+include_once __DIR__ . "/../../include/search_functions.php";
+include_once __DIR__ . "/../../include/migration_functions.php";
+
+// Migrate reports away from requiring substitutions at execution time, which was very hacky.
+// Automatically upgrade reports to use new correct syntax.
+
+$reports=sql_query("select * from report order by ref");
+foreach ($reports as $report)
+    {
+    $sql=$report["query"];
+    $ref=$report["ref"];
+    
+    echo "Migrating report: " . $ref . "\n";
+    
+    global $view_title_field;
+    $view_title_field_subquery = "
+        (
+            SELECT rd.value 
+              FROM resource_data AS rd 
+             WHERE rd.resource = r.ref 
+               AND rd.resource_type_field = {$view_title_field}
+             LIMIT 1
+         )";
+    if(strpos($sql, 'r.') === false || $ref==8)
+        {
+        $view_title_field_subquery = str_replace('r.ref', 'ref', $view_title_field_subquery);
+        }
+
+    #Attempt to cater for user created copies of shipped reports which may still reference r.view_title_field
+    if(strpos($sql, 'view_title_field_subselect') === false)
+        {
+        $sql=str_replace("r.view_title_field", "view_title_field_subselect",$sql);
+        }
+
+    if(strpos($sql, 'view_title_field_subselect') === false)
+        {
+        $sql=str_replace("view_title_field", "view_title_field_subselect",$sql);
+        }
+
+    #back compatibility for three default reports, to replace "title" with the view_title_field.
+    #all reports should either use r.title or view_title_field when referencing the title column on the resource table.
+    if ($ref==7||$ref==8||$ref==9){
+        $sql=str_replace(",title",",view_title_field_subselect",$sql);
+    }
+
+    #Attempt to cater for user created copies of shipped reports which may still reference r.title
+    $sql=str_replace("r.title","view_title_field_subselect",$sql);
+
+    #Now crystallize the view title subselect reference with the actual subselect necessary to fulfil it 
+    $sql=str_replace("view_title_field_subselect", $view_title_field_subquery, $sql);
+    
+    #echo $sql;
+    #echo "-----------------------------\n";
+    sql_query("update report set query='" . escape_check($sql) . "' where ref='" . escape_check($ref) . "'");
+    }
+    
