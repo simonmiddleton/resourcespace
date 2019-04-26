13ba3799dd519ef944ceec723c7d1362bd711259
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-04-26 09:29:44 +0000

    FIX: [9.0] Some search paramaters from the URL were being missed when using refine results [t19213]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.0@13063 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/refineresults/hooks/all.php
+++ plugins/refineresults/hooks/all.php
@@ -1,110 +1,139 @@
 <?php
 
 function HookRefineresultsSearchBeforesearchresults()
-	{
-	global $baseurl_short,$result,$lang,$search,$k,$archive,$parameters_string, $collections;
+    {
+    global $baseurl_short,$result,$lang,$search,$k,$archive,$parameters_string, $collections;
 
-	// Only time when this would be needed is when legacy_actions is enabled otherwise we do it through dropdown actions
-	$query = 'SELECT inst_version AS `value` FROM plugins WHERE name = \'legacy_actions\';';
-	if(trim(sql_value($query, '')) === '')
-		{
-		return false;
-		}
+    // Only time when this would be needed is when legacy_actions is enabled otherwise we do it through dropdown actions
+    $query = 'SELECT inst_version AS `value` FROM plugins WHERE name = \'legacy_actions\';';
+    if(trim(sql_value($query, '')) === '')
+        {
+        return false;
+        }
 
-	$results=0;
-	if (is_array($result)) $results=count($result);
-	if (is_array($collections)) $results+=count($collections);
-	#if ($k!="" || $results==0) {return false;}
-	#if ($results==0||$results==1) {return false;}
-	
-	# External sharing search support. Clear search drops back to the collection only search.
-	$default_search="";
-	if ($k!="") {$s=explode(" ",$search);$default_search=$s[0];}
-	
-	# dropping back to a special search seems like appropriate behavior in general.
-	if ($k=="" && substr($search,0,1)=="!") {
-		$s=explode(" ",$search);
-		# Should a second Clear be allowed to blank out the special search? 
-		# if (count($s)>1){  
-			$default_search=$s[0];
-		#}
-	}
-	
+    $results=0;
+    if (is_array($result)) $results=count($result);
+    if (is_array($collections)) $results+=count($collections);
+    #if ($k!="" || $results==0) {return false;}
+    #if ($results==0||$results==1) {return false;}
+    
+    # External sharing search support. Clear search drops back to the collection only search.
+    $default_search="";
+    if ($k!="") {$s=explode(" ",$search);$default_search=$s[0];}
+    
+    # dropping back to a special search seems like appropriate behavior in general.
+    if ($k=="" && substr($search,0,1)=="!") {
+        $s=explode(" ",$search);
+        # Should a second Clear be allowed to blank out the special search? 
+        # if (count($s)>1){  
+            $default_search=$s[0];
+        #}
+    }
+    
 
-	#if (substr($search,0,1)=="!") {return false;} # Only work for normal (non 'special') searches
-	?>
-	<div class="SearchOptionNav"><?php if ($results!=0 && $results!=1){?><a href="#" onClick="
-	if (jQuery('#RefinePlus').html()=='+')
-		{
-		jQuery('#RefineResults').slideToggle();
-		jQuery('#RefinePlus').html('&minus;');
-		jQuery('#refine_keywords').focus();
-		}
-	else
-		{
-		jQuery('#RefineResults').slideToggle();
-		jQuery('#RefinePlus').html('+');
-		}
-	"><span id='RefinePlus'>+</span> <?php echo $lang["refineresults"]?></a>&nbsp;&nbsp;<?php } ?><?php if ($search!=""){?><a href='<?php echo $baseurl_short?>pages/search.php?search=<?php echo $default_search ?><?php echo $parameters_string?>'>&gt;&nbsp;<?php echo $lang["clearsearch"]?></a><?php } ?></div>
-	<?php
-	return true;
-	}
-	
+    #if (substr($search,0,1)=="!") {return false;} # Only work for normal (non 'special') searches
+    ?>
+    <div class="SearchOptionNav"><?php if ($results!=0 && $results!=1){?><a href="#" onClick="
+    if (jQuery('#RefinePlus').html()=='+')
+        {
+        jQuery('#RefineResults').slideToggle();
+        jQuery('#RefinePlus').html('&minus;');
+        jQuery('#refine_keywords').focus();
+        }
+    else
+        {
+        jQuery('#RefineResults').slideToggle();
+        jQuery('#RefinePlus').html('+');
+        }
+    "><span id='RefinePlus'>+</span> <?php echo $lang["refineresults"]?></a>&nbsp;&nbsp;<?php } ?><?php if ($search!=""){?><a href='<?php echo $baseurl_short?>pages/search.php?search=<?php echo $default_search ?><?php echo $parameters_string?>'>&gt;&nbsp;<?php echo $lang["clearsearch"]?></a><?php } ?></div>
+    <?php
+    return true;
+    }
+    
 function HookRefineresultsSearchBeforesearchresultsexpandspace()
-	{
-	global $baseurl_short,$lang,$search,$k,$archive;
+    {
+    global $baseurl_short,$lang,$search,$k,$archive;
 
-	# Slightly different behaviour when allowing external share searching. Show the full search string in the box.
-	$value="";
-	if ($k!="")
-		{
-		$s=explode(" ",$search);
-		if (count($s)>1)
-			{
-			array_shift($s);
-			$value=join(" ",$s);
-			}
-		}
-	?>
-	
-	<div class="RecordBox clearerleft" id="RefineResults" style="display:none;"> 
-	
-	<form method="post" action="<?php echo $baseurl_short?>pages/search.php?search=<?php echo urlencode($search) ?>&amp;k=<?php echo $k ?>" onSubmit="return CentralSpacePost (this,true);">
-	   <?php generateFormToken("refineresults_before_search_results"); ?>
+    # Slightly different behaviour when allowing external share searching. Show the full search string in the box.
+    $value="";
+    if ($k!="")
+        {
+        $s=explode(" ",$search);
+        if (count($s)>1)
+            {
+            array_shift($s);
+            $value=join(" ",$s);
+            }
+        }
+    
+    # Get the url parameters to pass back in to the search
+    $modal          = getvalescaped('modal', '');
+    $display        = getvalescaped('display', '');
+    $order_by       = getvalescaped('order_by', '');
+    $offset         = getvalescaped('offset', '');
+    $per_page       = getvalescaped('per_page', '');
+    $archive        = getvalescaped('archive', '');
+    $sort           = getvalescaped('sort', '');
+    $restypes       = getvalescaped('restypes', '');
+    $recentdaylimit = getvalescaped('recentdaylimit', '');
+    $editable_only  = getval('foredit','')=='true';
+
+    $searchparams= array(
+    'search'            => $search,
+    'k'                 => $k,
+    'modal'             => $modal,
+    'display'           => $display,
+    'order_by'          => $order_by,
+    'offset'            => $offset,
+    'per_page'          => $per_page,
+    'archive'           => $archive,
+    'sort'              => $sort,
+    'restypes'          => $restypes,
+    'recentdaylimit'    => getvalescaped('recentdaylimit', '', true),
+    'foredit'           => ($editable_only?"true":"")
+    );
+
+    $search_url = generateURL('search.php', $searchparams);
+    ?>
+    
+    <div class="RecordBox clearerleft" id="RefineResults" style="display:none;"> 
+    
+    <form method="post" action="<?php echo $search_url ?>" onSubmit="return CentralSpacePost (this,true);">
+       <?php generateFormToken("refineresults_before_search_results"); ?>
     <div class="Question Inline" id="question_refine" style="border-top:none;">
-	<label id="label_refine" for="refine_keywords"><?php echo $lang["additionalkeywords"]?></label>
-	<input class="medwidth Inline" type=text id="refine_keywords" name="refine_keywords" value="<?php echo $value ?>">
-	<input type=hidden name="archive" value="<?php echo $archive?>">
-	<input class="vshrtwidth Inline" name="save" type="submit" id="refine_submit" value="&nbsp;&nbsp;<?php echo $lang["refine"]?>&nbsp;&nbsp;" />
-	<div class="clearerleft"> </div>
-	</div>
+    <label id="label_refine" for="refine_keywords"><?php echo $lang["additionalkeywords"]?></label>
+    <input class="medwidth Inline" type=text id="refine_keywords" name="refine_keywords" value="<?php echo $value ?>">
+    <input type=hidden name="archive" value="<?php echo $archive?>">
+    <input class="vshrtwidth Inline" name="save" type="submit" id="refine_submit" value="&nbsp;&nbsp;<?php echo $lang["refine"]?>&nbsp;&nbsp;" />
+    <div class="clearerleft"> </div>
+    </div>
 
-	</form>
+    </form>
 
-	</div>
-	<?php
-	
-	return true;
-	}
+    </div>
+    <?php
+    
+    return true;
+    }
 
 function HookRefineresultsSearchSearchstringprocessing()
-	{
-	global $search,$k;
-	$refine=trim(getvalescaped("refine_keywords",""));
-	if ($refine!="")
-		{
-		if ($k!="")
-			{
-			# Slightly different behaviour when searching within external shares. There is no search bar, so the provided string is the entirity of the search.
-			$s=explode(" ",$search);
-			$search=$s[0] . " " . $refine;	
-			}
-		else
-			{
-			$search.=", " . $refine;	
-			}
-		}
-	$search=refine_searchstring($search);	
-	}
+    {
+    global $search,$k;
+    $refine=trim(getvalescaped("refine_keywords",""));
+    if ($refine!="")
+        {
+        if ($k!="")
+            {
+            # Slightly different behaviour when searching within external shares. There is no search bar, so the provided string is the entirity of the search.
+            $s=explode(" ",$search);
+            $search=$s[0] . " " . $refine;  
+            }
+        else
+            {
+            $search.=", " . $refine;    
+            }
+        }
+    $search=refine_searchstring($search);   
+    }
 
 ?>
