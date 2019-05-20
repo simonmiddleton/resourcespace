08996cef37c6608c68dbf544e5dce3361bfcded6
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-05-20 14:29:22 +0000

    FIX: [9.0] Corrected styling for news plugin, background not displaying correctly, old styling used in places. [t19646]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.0@13154 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/version.php
+++ include/version.php
@@ -5,4 +5,4 @@ $productversion="SVN 9.0";
 # External items reload key
 # Increment the below value to force a reload of the CSS, JavaScripts and other included items.
 # This is intended for developer use when altering such files. It will force a reload on all client browsers.
-$css_reload_key = 143;
+$css_reload_key = 144;
