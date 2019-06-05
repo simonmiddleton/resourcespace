7971ce60e6c2fe7753e1875478c749d4e2e3a41a
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-06-05 11:26:56 +0000

    FIX: Edit all resources from collection_manage.php was affecting all search results [t20081]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@13182 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/collections_functions.php
+++ include/collections_functions.php
@@ -2245,7 +2245,8 @@ function compile_collection_actions(array $collection_data, $top_actions, $resou
 
     // Collection bar actions should always be a special search !collection[ID] (exceptions might arise but most of the 
     // time it should be handled using the special search)
-    if(isset($collection_data['ref']) && !$top_actions && $pagename == 'collections')
+
+    if(isset($collection_data['ref']) && !$top_actions && in_array($pagename, array('collections','collection_manage')))
         {
         $search = "!collection{$collection_data['ref']}";
         }
