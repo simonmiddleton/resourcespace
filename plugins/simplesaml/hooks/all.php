ccef8e098354c6a68f0b385e75912ebe4b408a94
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-06-06 15:19:58 +0000

    FIX: [9.0] Show the SSO login button when browsing anonymously. [t20079]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.0@13192 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/simplesaml/hooks/all.php
+++ plugins/simplesaml/hooks/all.php
@@ -423,7 +423,7 @@ function HookSimplesamlAllProvideusercredentials()
 		return false;
         }
 
-function HookSimplesamlLoginLoginformlink()
+function HookSimplesamlAllLoginformlink()
         {
         if(!file_exists(simplesaml_get_lib_path() . '/config/config.php'))
             {
