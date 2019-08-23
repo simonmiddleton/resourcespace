33b2dd038aa5e57f2cd146ab9b4c4456b900f7cc
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-08-23 13:59:48 +0000

    FIX :Added missing TMS plugin test file from r12470 [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@13511 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/tms_link/pages/ajax_test.php
@@ -0,0 +1,24 @@
+<?php
+include '../../../include/db.php';
+include '../../../include/authenticate.php'; if (!checkperm('a')) {exit ($lang['error-permissiondenied']);}
+include_once '../../../include/general.php';
+
+$tms_test_dsn = getval("dsn","");
+$tms_test_user = getval("tmsuser","");
+$tms_test_password = getval("tmspass","");
+$conn=@odbc_connect($tms_test_dsn, $tms_test_user, $tms_test_password);
+
+if(!$conn)
+  {
+  $error=odbc_errormsg();
+  $return["result"]     = $lang["error"];
+  $return["message"]    = $lang["tms_link_tms_link_failure"] . ": " . $error;
+  }
+else
+  {
+  $return["result"]     = $lang["ok"];
+  $return["message"]    = $lang["tms_link_tms_link_success"];
+  }
+
+echo json_encode($return);
+exit();
