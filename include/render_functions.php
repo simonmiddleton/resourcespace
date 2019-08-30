d7e0107607b9068afe01f88d95cd453b4a2e9ed0
tgray <tgray@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-08-30 10:50:02 +0000

    FIX: correction to relate all_resources_in_this_collection action [t20720]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@13553 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/render_functions.php
+++ include/render_functions.php
@@ -1158,7 +1158,7 @@ function render_actions(array $collection_data, $top_actions = true, $two_line =
 							data: {<?php echo generateAjaxToken("relate_resources"); ?>},
                             success: function(data) {
 								if (data.trim() == "SUCCESS") {
-									stylerAlert("OK","Related ok");
+									alert("Related ok");
 								}
 							},
 							error: function (err) {
