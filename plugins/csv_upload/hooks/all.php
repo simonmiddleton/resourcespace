8b2cdca1e46918965bebc8d31a0d34ab7b0cb0ea
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-08-30 10:36:22 +0000

    FIX: Add icon to CSV Upload link [t20787]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@13551 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/csv_upload/hooks/all.php
+++ plugins/csv_upload/hooks/all.php
@@ -5,7 +5,7 @@ function HookCsv_uploadAllTopnavlinksafterhome()
     global $baseurl,$lang;
 	if (checkperm("c"))
 		{
-		?><li><a href="<?php echo $baseurl ?>/plugins/csv_upload/pages/csv_upload.php" onClick="CentralSpaceLoad(this,true);return false;"><?php echo $lang["csv_upload_nav_link"]; ?></a></li>
+		?><li><a href="<?php echo $baseurl ?>/plugins/csv_upload/pages/csv_upload.php" onClick="CentralSpaceLoad(this,true);return false;"><?php echo UPLOAD_ICON . $lang["csv_upload_nav_link"]; ?></a></li>
 		<?php
 		}
 	}
