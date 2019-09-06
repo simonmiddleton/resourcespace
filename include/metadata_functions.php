cc66a078ed18567d439eecfc09fce8d5a7ab128b
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-09-06 16:06:52 +0000

    FIX: Metadata field mappings for FITS fields now support XML element data as well as attribute data [t20740]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@13608 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/metadata_functions.php
+++ include/metadata_functions.php
@@ -43,7 +43,7 @@ function runFitsForFile($file_path)
 * Get metadata value for a FITS field
 * 
 * @param SimpleXMLElement $xml  FITS metadata XML
-* @param string $rs_fits_filter A ResourceSpace specific FITS filter which allows ResourceSpace to know exactly where
+* @param string $fits_field A ResourceSpace specific FITS field mapping which allows ResourceSpace to know exactly where
 *                               to look for that value in XML by converting it to an XPath query string.
 * Example:
 * video.mimeType would point to
@@ -58,28 +58,33 @@ function runFitsForFile($file_path)
 * 
 * @return string
 */
-function getFitsMetadataFieldValue(SimpleXMLElement $xml , $rs_fits_filter)
+function getFitsMetadataFieldValue(SimpleXMLElement $xml , $fits_field)
     {
     // IMPORTANT: Not using "fits" namespace (or any for that matter) will yield no results
     // TODO: since there can be multiple namespaces (especially if run with -xc options) we might need to implement the
-    // ability to use namespaces directly from RS FITS filter.
+    // ability to use namespaces directly from RS FITS Field.
     $xml->registerXPathNamespace('fits', 'http://hul.harvard.edu/ois/xml/ns/fits/fits_output');
 
-    // Take out last filter as that is always going to be the actual property we are looking for
-    $filter        = explode('.', $rs_fits_filter);
-    $fits_property = array_pop($filter);
-    $filter        = implode('/fits:', $filter);
+    // Convert fits field mapping from rs format to namespaced XPath format
+    // Example rs field mapping for an xml element value
+    //   rs field is one.two.three which converts to an xpath filter of //fits:one/fits:two/fits:three
+    // Example rs field mapping for an xml attribute value (attributes are not qualified by the namespace)
+    //   rs attribute is one.two.three/@four which converts to an xpath filter of //fits:one/fits:two/fits:three/@four
+    $fits_path = explode('.', $fits_field);
+    // Reassemble with the namespace
+    $fits_filter  = "//fits:".implode('/fits:', $fits_path);
 
-    $result = $xml->xpath("//fits:{$filter}");
+    $result = $xml->xpath($fits_filter);
 
     if(!isset($result) || false === $result || 0 === count($result))
         {
         return '';
         }
 
-    if(isset($result[0]->$fits_property) && !is_array($result[0]->$fits_property))
+    // First result entry carries the element or attribute value
+    if( isset($result[0]) && !is_array($result[0]) )
         {
-        return $result[0]->$fits_property;
+        return $result[0];
         }
 
     return '';
