517a7171fa11e7b2122ca23cce9912c75da7c0af
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-09-30 14:23:23 +0000

    FIX: Improve display of video splice link in collections bar to use LINK_CARET. [t21117]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@13756 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/video_splice/hooks/collections.php
+++ plugins/video_splice/hooks/collections.php
@@ -5,7 +5,7 @@ function HookVideo_spliceCollectionsCollectiontool()
 	# Link in collections bar (maximised)
 	global $usercollection,$lang,$baseurl;
 	?>
-    <li><a href="<?php echo $baseurl ?>/plugins/video_splice/pages/splice.php?collection=<?php echo $usercollection ?>">&gt; <?php echo $lang["action-splice"]?></a></li>
+    <li><a href="<?php echo $baseurl ?>/plugins/video_splice/pages/splice.php?collection=<?php echo $usercollection ?>"><?php echo LINK_CARET . $lang["action-splice"]?></a></li>
 	<?php
 	}
 
