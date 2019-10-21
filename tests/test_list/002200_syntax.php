e0d9b961546c8c21af454bd868597f3290c5343d
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-10-21 15:40:21 +0000

    NEW: New automated test that checks the syntax of all project PHP files. [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@13858 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ tests/test_list/002200_syntax.php
@@ -0,0 +1,34 @@
+<?php
+ 
+# Simple syntax check of pages to ensure they all function at a very basic level and arrive at the footer.
+ 
+# Build array of pages to scan.
+$Directory = new RecursiveDirectoryIterator(dirname(__FILE__) . "/../../");
+$Iterator = new RecursiveIteratorIterator($Directory);
+foreach ($Iterator as $i)
+    {
+    $i=$i->getPathName();
+ 
+    # Parse all PHP files but not those in the lib folder(s) which are external.
+    if (strpos($i,".php")!==false && strpos($i,"/lib/")===false) {$pages[]=$i;}
+    }
+ 
+# Test each page.
+foreach ($pages as $page)
+    {
+    if (!TestPage($page)) {echo $page;return false;}
+    }
+return true;
+ 
+function TestPage($page)
+    {
+    $html="";
+    global $php_path;
+    if (!isset($php_path)) {$php_path="/usr/bin";} # fair assumption, if not specifically set; means it will work on many systems
+ 
+    # echo "\n" . $page;
+ 
+    # Run PHP in lint mode against the file.
+    $result=exec("cd " . escapeshellarg(dirname($page)). ";" . $php_path . "/php -l " . escapeshellarg(basename($page)) );
+    return (strpos($result,"No syntax errors")!==false);
+    }
