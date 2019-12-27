31daf45bb2b5e0f06a610236f8712489a95ee2f2
tgray <tgray@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2019-12-27 14:01:50 +0000

    FIX: display search results link when search is empty string and resources only search [t21565]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@14119 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/header_links.php
+++ include/header_links.php
@@ -14,17 +14,10 @@
 
         <?php if ($search_results_link) { ?>
             <li class="HeaderLink">
-                <?php if((checkperm("s")) && ((isset($_COOKIE["search"]) && strlen($_COOKIE["search"])>0) || (isset($search) && (strlen($search)>0) && (strpos($search,"!")===false)))) { # active search present ?>
-                    <a href="<?php echo $baseurl?>/pages/search.php" onClick="return CentralSpaceLoad(this,true);">
+                      <a href="<?php echo $baseurl?>/pages/search.php" onClick="return CentralSpaceLoad(this,true);">
                         <i aria-hidden="true" class="fa fa-fw fa-search"></i>
                         <?php echo $lang["searchresults"]?>
                     </a>
-                <?php } else { ?>
-                    <a class="SearchResultsDisabled">
-                        <i aria-hidden="true" class="fa fa-fw fa-search"></i>
-                        <?php echo $lang["searchresults"]?>
-                    </a>
-                <?php } ?>
             </li>
         <?php } ?>
 
