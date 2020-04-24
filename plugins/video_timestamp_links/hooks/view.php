7fa7f0bca87d198f4feaa43e871b43d619e20780
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-04-24 14:25:08 +0000

    NEW: Added new video_timestamp_links plugin to make metadata timestamps links to the video [q11540]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@14742 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/video_timestamp_links/hooks/view.php
@@ -0,0 +1,35 @@
+<?php
+
+function HookVideo_timestamp_linksViewFooterbottom()
+    {
+    ?>
+    <script>
+    // Find timestamps in fields and replace with links to jump the video.
+
+    var video=jQuery(".videojscontent video");
+    if (video.length==1)
+        {
+        // There's a video tag.
+
+        // For each field...
+        var mfields=jQuery(".item p,.itemNarrow p");
+        mfields.each(function(index)
+            {
+            var newtext=this.innerHTML.replace(/([0-9][0-9]:[0-9][0-9]:[0-9][0-9])/gim,'<a href="#" onClick="VideoJumpTo(\'$1\');return false;">$1</a>');
+            this.innerHTML=newtext;
+            });   
+        }
+
+    function VideoJumpTo(timestamp)
+        {
+        // Convert timestamp to seconds.
+        var s=timestamp.split(":");
+        var ts=(Number(s[0])*60*60)+(Number(s[1])*60)+Number(s[2]);
+
+        //Target the video and set the time.
+        var video=jQuery(".videojscontent video")[0];
+        video.currentTime=ts;video.play();
+        }
+    </script>
+    <?php
+    }
