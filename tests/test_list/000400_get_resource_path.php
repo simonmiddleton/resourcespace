7fe26e807d8b78a55ff7c7308345df9a7c08a3c5
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-05-06 12:32:10 +0000

    FIX: Updated unit tests to change the way file_modified is checked for because r14823 changed it to use real files. The tests don't apply in many cases now as real files aren't created so there's no real modified time to use. It's sufficient for the existing URL tests to cover generation of URLs without the necessary extra checks for different files. [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@14825 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- tests/test_list/000400_get_resource_path.php
+++ tests/test_list/000400_get_resource_path.php
@@ -4,10 +4,21 @@ if('cli' !== PHP_SAPI)
     exit('This utility is command line only.');
     }
 
+$original_scramble_key = $scramble_key;
+$scramble_key          = 'c8cf6994c288cf9d75c64017c57d16b24a0fdb4f0b826c66bfa7da19541178e9';
+
 $path = get_resource_path(1, true, '');
+$pre_path = get_resource_path(1, true, 'pre');
 
 // Copy the default slideshow image to this location, for future tests to use
 copy(dirname(__FILE__) . '/../../gfx/homeanim/1.jpg', $path);
+copy(dirname(__FILE__) . '/../../gfx/homeanim/1.jpg', $pre_path);
+
+if (!file_exists($path) || !file_exists($pre_path))
+    {
+    echo "File was not copied.";
+    return false;
+    }
 
 if(0 >= strlen($path))
     {
@@ -18,15 +29,11 @@ if(0 >= strlen($path))
 // Check actual paths/ URLs
 $file_modified =  filemtime($path);
 
-$original_scramble_key = $scramble_key;
-$scramble_key          = 'c8cf6994c288cf9d75c64017c57d16b24a0fdb4f0b826c66bfa7da19541178e9';
-
-// Reset cache because otherwise we will not pick up the file_modified we set for this test
-$get_resource_data_cache = array();
 
 // For most of these cases, we are expecting either a physical path/ URL to filestore,
 // so set this config option to false. Enable it only when testing hiding the real file path
 $hide_real_filepath = false;
+
 
 // Original file path/ URL
 if("{$storagedir}/1_6326bb8314c6c21/1_71a3211b5d04a88.jpg" != get_resource_path(1, true, '')
@@ -36,6 +43,9 @@ if("{$storagedir}/1_6326bb8314c6c21/1_71a3211b5d04a88.jpg" != get_resource_path(
     echo 'Case: Original file path/ URL -- ';
     return false;
     }
+
+
+$file_modified =  filemtime($pre_path);
 
 // Get specific size (e.g: Preview) path/ URL
 if("{$storagedir}/1_6326bb8314c6c21/1pre_cf33a61f47b5982.jpg" != get_resource_path(1, true, 'pre')
@@ -60,9 +70,7 @@ if(!file_exists(get_resource_path(100000, true, '', false)))
     }
 
 // Check looking for specific preview by extension (like videos/ audio)
-if("{$storagedir}/1_6326bb8314c6c21/1pre_cf33a61f47b5982.mp4" != get_resource_path(1, true, 'pre', false, 'mp4')
-    || "{$storageurl}/1_6326bb8314c6c21/1pre_cf33a61f47b5982.mp4?v={$file_modified}" != get_resource_path(1, false, 'pre', false, 'mp4')
-)
+if("{$storagedir}/1_6326bb8314c6c21/1pre_cf33a61f47b5982.mp4" != get_resource_path(1, true, 'pre', false, 'mp4'))
     {
     echo 'Case: Check looking for specific preview by extension (like videos/ audio) -- ';
     return false;
@@ -85,27 +93,21 @@ if(
     }
 
 // Check getting a non-scrambled version of the path/ URL
-if("{$storagedir}/1/1pre.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', false)
-    || "{$storageurl}/1/1pre.jpg?v={$file_modified}" != get_resource_path(1, false, 'pre', false, 'jpg', false)
-)
+if("{$storagedir}/1/1pre.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', false))
     {
     echo 'Case: Check getting a non-scrambled version of the path/ URL -- ';
     return false;
     }
 
 // Check getting a page preview of a document
-if("{$storagedir}/1_6326bb8314c6c21/1pre_3_0092923182f54ea.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', true, 3)
-    || "{$storageurl}/1_6326bb8314c6c21/1pre_3_0092923182f54ea.jpg?v={$file_modified}" != get_resource_path(1, false, 'pre', false, 'jpg', true, 3)
-)
+if("{$storagedir}/1_6326bb8314c6c21/1pre_3_0092923182f54ea.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', true, 3))
     {
     echo 'Case: Check getting a page preview of a document -- ';
     return false;
     }
 
 // Check getting the watermarked version of a preview
-if("{$storagedir}/1_6326bb8314c6c21/1pre_wm_349473228947cd0.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', true, 1, true)
-    || "{$storageurl}/1_6326bb8314c6c21/1pre_wm_349473228947cd0.jpg?v={$file_modified}" != get_resource_path(1, false, 'pre', false, 'jpg', true, 1, true)
-)
+if("{$storagedir}/1_6326bb8314c6c21/1pre_wm_349473228947cd0.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', true, 1, true))
     {
     echo 'Case: Check getting the watermarked version of a preview -- ';
     return false;
@@ -120,9 +122,7 @@ if("{$storageurl}/1_6326bb8314c6c21/1pre_cf33a61f47b5982.jpg?v=2017-04-06+17%3A5
     }
 
 // Check getting alternative paths/ URLs
-if("{$storagedir}/1_6326bb8314c6c21/1pre_alt_10_9ef1f74b56b64ed.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', true, 1, false, '', 10)
-    || "{$storageurl}/1_6326bb8314c6c21/1pre_alt_10_9ef1f74b56b64ed.jpg?v={$file_modified}" != get_resource_path(1, false, 'pre', false, 'jpg', true, 1, false, '', 10)
-)
+if("{$storagedir}/1_6326bb8314c6c21/1pre_alt_10_9ef1f74b56b64ed.jpg" != get_resource_path(1, true, 'pre', false, 'jpg', true, 1, false, '', 10))
     {
     echo 'Case: Check getting alternative paths/ URLs -- ';
     return false;
