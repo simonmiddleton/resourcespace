fc1003f4d96df75af1c4cf926d1a0d53199cad9b
tgray <tgray@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-05-27 10:48:32 +0000

    FIX: order resource_types in manage_metadata_fields form [t23083]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@14963 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/admin/admin_resource_type_fields.php
+++ pages/admin/admin_resource_type_fields.php
@@ -90,7 +90,7 @@ function addColumnHeader($orderName, $labelKey)
     }
  
 $fields=get_resource_type_fields($restypesfilter, $field_order_by, $field_sort, $find, array(),true);
-$resource_types=sql_query("select ref, name from resource_type", "schema");
+$resource_types=sql_query("select ref, name from resource_type order by order_by,ref", "schema");
 $arr_restypes=array();
 foreach($resource_types as $resource_type)
     {
