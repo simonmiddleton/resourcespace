f1048cddd8844ec4654cdce94b1b2e528d401bd7
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-06-10 10:12:07 +0000

    FIX: Prevent changing to a new resource type if resource file is not an allowed extension [t23263]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15125 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit.php
+++ pages/edit.php
@@ -1375,12 +1375,17 @@ if(!$multiple)
           
         for($n = 0; $n < count($types); $n++)
             {
+            $allowed_extensions = trim($types[$n]['allowed_extensions']) != "" ? explode(",",strtolower($types[$n]['allowed_extensions'])): array();
             // skip showing a resource type that we do not to have permission to change to (unless it is currently set to that). Applies to upload only
             if(0 > $ref 
                 && 
                     (checkperm("XU{$types[$n]['ref']}") || in_array($types[$n]['ref'], $hide_resource_types))
                     ||
                     (checkperm("XE") && !checkperm("XE-" . $types[$n]['ref']))
+                    ||
+                    (trim($resource["file_extension"]) != "" && count($allowed_extensions) > 0 && !in_array(strtolower($resource["file_extension"]),$allowed_extensions))
+                &&
+                    $resource['resource_type'] != $types[$n]['ref']
                 )
                 {
                 continue;
