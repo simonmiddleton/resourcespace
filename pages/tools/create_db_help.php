60a58b85a5d4d0aac482325ab2acaf99b88802ba
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-06-22 14:37:37 +0000

    NEW: Script to create the templates for the database help files (to be pulled in to the KB automatically later) [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15255 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ pages/tools/create_db_help.php
@@ -0,0 +1,49 @@
+<?php
+// Creates or updates the database help files, ready for manual completion by adding a note to each column.
+
+// Uncomment next line to execute.
+exit();
+
+$folder=dirname(__FILE__) . "/../../dbstruct/";
+
+$dir=scandir($folder);
+foreach ($dir as $file)
+    {
+    if (substr($file,0,6)=="table_")
+        {
+        // Found a table. Work out table name.
+        $s=strpos($file,".");
+        $table=substr($file,6,$s-6);echo "Processing: " . $table . "\n";
+
+        // Create the help file if it doesn't exist.
+        $helpfile=$folder . "help_" . $table . ".txt";
+        if (!file_exists($helpfile)) {touch($helpfile);file_put_contents($helpfile,"(table desciption goes here)\n",FILE_APPEND);}
+
+        // Load the columns already there
+        $existingcols=file($helpfile);
+
+        // Check all the columns are there; add those that arent
+        $cols=file($folder . $file);
+        foreach ($cols as $col)
+            {
+            $col_split=explode(",",$col);
+            $colname=$col_split[0];
+
+            // Look for the column in the existing columns
+            $found=false;
+            foreach ($existingcols as $existingcol)
+                {
+                $existingcol_split=explode(",",$existingcol);
+                if ($existingcol_split[0]==$colname)
+                    {
+                    $found=true;break;
+                    }
+                }
+            if (!$found)
+                {
+                // Column was not in the list; add it
+                file_put_contents($helpfile,$colname.",".$col_split[1].",\n",FILE_APPEND);
+                }
+            }
+        }
+    }
