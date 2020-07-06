456565ecd21e1d68b8089fa983a0c9226b1fc207
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-07-06 07:56:06 +0000

    FIX: Reverted change from r14844 that removed db query for sysvar [t23601]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15373 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/general_functions.php
+++ include/general_functions.php
@@ -3551,8 +3551,10 @@ function get_sysvar($name, $default=false)
         {
         return $sysvars[$name];
         }
-    // Value not set, return default
-    return $default;
+
+    // Load from db or return default
+    $name=escape_check($name);
+    return sql_value("SELECT `value` FROM `sysvars` WHERE `name`='{$name}'",$default);
     }
 
 /**
