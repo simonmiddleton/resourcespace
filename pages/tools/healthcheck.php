a5767095b140ca3c8f0e781051ee6ea26b408501
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-07-06 11:13:04 +0000

    FIX: Healthcheck error if no resources yet on system [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15378 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/healthcheck.php
+++ pages/tools/healthcheck.php
@@ -98,7 +98,7 @@ if (isset($disksize))
 	{
 	$avail=$disksize*(1000*1000*1000); # Get quota in bytes
 	$used=get_total_disk_usage();      # Total usage in bytes
-    $percent=ceil(($used/$avail)*100);
+    $percent=ceil(((int)$used/$avail)*100);
     echo " " . $percent . "% used";
 	if ($percent>=95) {echo " WARNING nearly full";}
 	}
