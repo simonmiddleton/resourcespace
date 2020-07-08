cfc806f133e16d6cce488c646d5773563712fbbf
tgray <tgray@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-07-08 06:59:34 +0000

    FIX: image_annotation re-introduce call to toggleAnnotationsOption() ft r14724 [t23553]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15393 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/view.php
+++ pages/view.php
@@ -893,6 +893,14 @@ else if(1 == $resource['has_image'])
                     }
                     ?>
 			 rs_tagging_plugin_added = true;
+			  // We have to wait for initialisation process to finish as this does ajax calls
+                        // in order to set itself up
+                        setTimeout(function ()
+                            {
+                            toggleAnnotationsOption(element);
+                            }, 
+                            1000);
+                        return false;
 						
                         }
 
