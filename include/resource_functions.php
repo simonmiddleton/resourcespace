ffb4e0ab91cf807c88b722c1b17163fe1e6cbe00
tgray <tgray@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-07-22 11:25:58 +0000

    FIX: update var type check resource_log() [t23739]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15501 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -3167,7 +3167,7 @@ function resource_log($resource, $type, $field, $notes="", $fromvalue="", $toval
 
     // Param type checks
     $param_str = array($type,$notes,$fromvalue,$tovalue,$purchase_size);
-    $param_int = array($resource,$field,$usage);
+    $param_num = array($resource,$field,$usage,$purchase_price);
  
     foreach($param_str as $par)
         {
@@ -3177,7 +3177,7 @@ function resource_log($resource, $type, $field, $notes="", $fromvalue="", $toval
             } 
         }
  
-    foreach($param_int as $par)
+    foreach($param_num as $par)
         {
         if (!is_numeric($par))
             {
@@ -3185,11 +3185,6 @@ function resource_log($resource, $type, $field, $notes="", $fromvalue="", $toval
             } 
         }
  
-    if (!is_float($purchase_price))
-        {
-        return false;
-        }
-
     // If it is worthy of logging, update the modified date in the resource table
     update_timestamp($resource);
     
