6fab8c991390d0e44d327469d7731f791b5a145f
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-08-11 11:33:03 +0000

    NEW: Sensitive image plugin - blurs image on view page [q11737]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15655 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/sensitive_images/hooks/view.php
@@ -0,0 +1,29 @@
+<?php
+
+function HookSensitive_imagesViewReplacepreviewlink()
+    {
+    global $ref,$sensitive_images_field,$resource;
+    if ($sensitive_images_field==0) {return false;} // not configured yet
+    $sensitive=$resource["field" . $sensitive_images_field];
+    if ($sensitive!="")
+        {
+        ?>
+        <style>
+        #previewimage {filter: blur(15px);}
+
+        #previewimagewrapper::before 
+                {
+                content: '<?php echo htmlspecialchars($sensitive) ?>. Click image to show.';
+                z-index: 5;
+                position: absolute;
+                left: 50%;
+                top: 50%;
+                transform: translate( -50%, -50% );
+                color: white;
+                font-size: 30px !important;
+                text-align: center;
+                }
+        </style>
+        <?php
+        }
+    }
