ebd91a2aa8bc60fe57207428edab79f79f53e4e2
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-08-11 11:06:30 +0000

    NEW: Sensitive image plugin - blurs image in results [q11737]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15653 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/sensitive_images/hooks/all.php
+++ plugins/sensitive_images/hooks/all.php
@@ -10,16 +10,45 @@ function HookSensitive_imagesAllAdditionaljoins()
         }
     }
 
-
-function HookSensitive_imagesSearchResourcethumbtop()
+function SensitiveImageResultsReplace($collection)
     {
+    // Blur image in search results and collections bar
     global $sensitive_images_field,$result,$n;
     if ($sensitive_images_field>0)
         {
         $sensitive=$result[$n]["field" . $sensitive_images_field];
         if (strlen($sensitive)>0)
             {
-            echo "sensitive";
+            ?>
+            <style>
+            <?php echo ($collection?"#CollectionSpace":"#CentralSpaceResources"); ?> #ResourceShell<?php echo $result[$n]["ref"] ?> img {filter: blur(6px);}
+
+            <?php if (!$collection) { ?>#CentralSpaceResources #ResourceShell<?php echo $result[$n]["ref"] ?>::before 
+                {
+                content: '!';
+                z-index: 5;
+                position: absolute;
+                left: 50%;
+                top: 40%;
+                transform: translate( -50%, -50% );
+                color: white;
+                font-size: <?php echo ($collection?"20px":"50px") ?> !important;
+                }
+            <?php } ?>
+            </style>
+            <?php
             }
         }
+    }
+
+function HookSensitive_imagesSearchResourcethumbtop()
+    {
+    // Blur image in search results
+    SensitiveImageResultsReplace(false);
+    }
+
+function HookSensitive_imagesCollectionsRendercollectionthumb()
+    {
+    // Blue image in collections bar
+    SensitiveImageResultsReplace(true);
     }
