5dc6e55e1f4ea36d53b364a371761adca5d401a7
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-08-27 08:45:51 +0000

    FIX: Wordpress plugin escaping username too early causng hash check failure [t24078]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@15743 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/wordpress_sso/include/wordpress_sso_functions.php
+++ plugins/wordpress_sso/include/wordpress_sso_functions.php
@@ -9,7 +9,7 @@ function wordpress_sso_redirect($getdetails=false,$knownuser=false)
 		{
 		// Update the user account with the request id so it can be detected on return
 		$requestid=uniqid("wp_sso");
-		sql_query("update user set wp_authrequest='$requestid' where username='$username'");
+		sql_query("UPDATE user SET wp_authrequest='" . $requestid . "' WHERE username='" . escape_check($username) . "'");
 		//echo "wordpress_sso - Set request ID $requestid for $username";
 		debug("wordpress_sso - sending second request to WP, setting request ID for $username in database");
 		}
