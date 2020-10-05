c2b07335b1ebbf14a969dfd15add6b354ec3ed31
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-10-05 17:53:38 +0000

    FIX: Update searchbar.php to replace '@' prefix with a proper check for existence of cookie for PHP8 [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@16015 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/searchbar.php
+++ include/searchbar.php
@@ -18,9 +18,9 @@ if ($simple_search_reset_after_search)
 else 
     {
     # pull values from cookies if necessary, for non-search pages where this info hasn't been submitted
-    if(!isset($restypes))
+    if(!isset($restypes) && isset($_COOKIE['restypes']))
         {
-        $restypes = @$_COOKIE['restypes'];
+        $restypes = $_COOKIE['restypes'];
         }
 
     if(!isset($search) || false !== strpos($search, '!'))
