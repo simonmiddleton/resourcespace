c4825f8031cc5fb40bac955eb5fe8b5d1b5e30f4
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-10-13 09:54:11 +0000

    CHG: finishing the migration script for permissions [branches/20200806_acota_featured_collection_overhaul_q10271][q10271]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20200806_acota_featured_collection_overhaul_q10271@16077 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- upgrade/scripts/014_featured_collections_new_permissions_syntax.php
+++ upgrade/scripts/014_featured_collections_new_permissions_syntax.php
@@ -1,24 +1,27 @@
 <?php
 include_once __DIR__ . "/../../include/db.php";
 
+set_sysvar(SYSVAR_UPGRADE_PROGRESS_SCRIPT, "Started migrating featured collections permissions from the old format to the new one (e.g j[ID], -j[ID]) ...");
+
 $all_ugs = sql_query("SELECT ref, `name`, permissions FROM usergroup");
 foreach($all_ugs as $ug)
     {
-    logScript("Checking permissions: {$ug["permissions"]}");
+    logScript("Analysing user group #{$ug["ref"]} '{$ug["name"]}'");
+    logScript("Original (old format) permissions: {$ug["permissions"]}");
     $permissions = trim_array(explode(",", $ug["permissions"]));
 
-    $old_style_perms = array();
+    $old_format_perms = array();
     foreach($permissions as $perm)
         {
         $matches = array();
         if(preg_match("/^(j\-?){1}(.+[^*])$/", $perm, $matches) === 1)
             {
-            /* Examples of a match:
+            /* Examples of a match (root category and a sub-category):
             Array
             (
                 [0] => jFC1     --> actual permission (full regex match)
                 [1] => j        --> permission type (j for root levels, "j-" for sub-categories which are also reverse permissions)
-                [2] => FC1      --> the featured collection category branch path (separated by pipes "|")
+                [2] => FC1      --> the featured collection category branch path (separated by pipes "|" and using the category name)
             )
             Array
             (
@@ -27,12 +30,12 @@ foreach($all_ugs as $ug)
                 [2] => FC1|FC1/1.1|FC1/1.1/1.1.1
             )
             */
-            $old_style_perms[] = $matches;
+            $old_format_perms[] = $matches;
             }
         }
 
     $update_permissions = false;
-    foreach($old_style_perms as $j_perm)
+    foreach($old_format_perms as $j_perm)
         {
         $find_fc_by_name = explode("|", $j_perm[2]);
         $find_fc_by_name = trim(end($find_fc_by_name));
@@ -57,13 +60,13 @@ foreach($all_ugs as $ug)
             continue;
             }
 
+        // Ensure there were no issues finding the correct featured collection by double checking that the branch paths match
         $found_branch_path = get_featured_collection_category_branch_by_leaf($fc_categ_ref, array());
         $branch_path_str = array_reduce($found_branch_path, function($carry, $item) { return "{$carry}|{$item["name"]}"; }, "");
         $branch_path_str = mb_substr($branch_path_str, 1, mb_strlen($branch_path_str));
-
         if($j_perm[2] != $branch_path_str)
             {
-            logScript("Found a featured collection category but computed branch paths are different! Old permission has '{$j_perm[2]}' and new one has '{$branch_path_str}'. ResourceSpace might have found a similar named collection incorrectly!");
+            logScript("Found a featured collection category but computed branch path is different! For old permission is '{$j_perm[2]}' and for the new one it is '{$branch_path_str}'. ResourceSpace might have found a similar named collection incorrectly!");
             continue;
             }
 
@@ -80,12 +83,12 @@ foreach($all_ugs as $ug)
     if($update_permissions)
         {
         $permissions_str = join(",", $permissions);
-        logScript("New permissions: " . $permissions_str);
-        // sql_query(
-        //     sprintf("UPDATE usergroup SET permissions = '%s' WHERE ref = '%s'",
-        //     escape_check($permissions_str),
-        //     escape_check($ug["ref"])));
+        logScript("New format permissions: " . $permissions_str);
+        sql_query(
+            sprintf("UPDATE usergroup SET permissions = '%s' WHERE ref = '%s'",
+            escape_check($permissions_str),
+            escape_check($ug["ref"])));
         }
     }
 
-echo PHP_EOL;
+set_sysvar(SYSVAR_UPGRADE_PROGRESS_SCRIPT, "Successfully migrated featured collections permissions to the new format!");
