fb21315c6d81efeba1051f14e111d93af494ec1e
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-10-20 16:17:13 +0000

    FIX: test #1400 [branches/20200806_acota_featured_collection_overhaul_q10271][q10271]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20200806_acota_featured_collection_overhaul_q10271@16152 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- tests/test_list/001400_create_collection.php
+++ tests/test_list/001400_create_collection.php
@@ -1,34 +1,31 @@
 <?php
-
 include_once(__DIR__ . '/../../include/db.php');
 if (php_sapi_name()!=="cli") {exit("This utility is command line only.");}
 
-// create new collections
-$levels = array("Top level");
+// Create the Featured collections tree
+$fc_cat_1 = create_collection($userref, "FC 1");
+save_collection($fc_cat_1, array("featured_collections_changes" => array("update_parent" => 0, "force_featured_collection_type" => true)));
+$fc_cat_1_1 = create_collection($userref, "FC 1 / 1");
+save_collection($fc_cat_1_1, array("featured_collections_changes" => array("update_parent" => 0, "force_featured_collection_type" => true)));
 
-$levelacollection = create_collection(1,"TEST COLLECTION 1",0,0,0,true,$levels);
-$getthemes=get_themes($levels);
+// Check FC tree has been created
+$lvl_1 = get_featured_collection_categories(0, array("access_control" => false));
+$lvl_2 = get_featured_collection_categories($fc_cat_1, array("access_control" => false));
+$fc_tree = array_merge($lvl_1, $lvl_2);
+$fc_tree = array_column($fc_tree, "ref");
+if(!empty(array_diff(array($fc_cat_1, $fc_cat_1_1), $fc_tree)))
+    {
+    echo "Create Featured Collection Categories - ";
+    return false;
+    }
 
-if(count($getthemes) != 1 || !in_array($levelacollection,array_column($getthemes,"ref")))
-    {return false;}
-
-$levels[] = "level 2";    
-$levelbcollection = create_collection(1,"TEST COLLECTION 2",0,0,0,true,$levels);
-$getthemes=get_themes($levels);
-if(count($getthemes) != 1 || !in_array($levelbcollection,array_column($getthemes,"ref")))
-    {return false;}
-
-$levels[] = "level 3";
-$levelccollection = create_collection(1,"TEST COLLECTION 3",0,0,0,true,$levels);
-$getthemes=get_themes(array("Top level","level 2"),true);
-if(count($getthemes) != 2  || !in_array($levelbcollection,array_column($getthemes,"ref")) || !in_array($levelccollection,array_column($getthemes,"ref")) )
-    {return false;}
-                       
-$theme_category_levels=4;
-$levels[] = "level  4";
-$leveldcollection = create_collection(1,"TEST COLLECTION 4",0,0,0,true,$levels);
-$getthemes=get_themes(array("Top level","level 2"),true);
-if(count($getthemes) != 3  || !in_array($levelbcollection,array_column($getthemes,"ref")) || !in_array($levelccollection,array_column($getthemes,"ref")) || !in_array($leveldcollection,array_column($getthemes,"ref")) )
-    {return false;}        
+$public_col = create_collection($userref, "Test 1400 - Public collection", 0, 0, 0, true);
+$find_public_col = search_public_collections("Test 1400", "name", "ASC", true, false);
+$found_public_col = array_column($find_public_col, "ref");
+if(empty($found_public_col))
+    {
+    echo "Create Public Collection - ";
+    return false;
+    }
 
 return true;
