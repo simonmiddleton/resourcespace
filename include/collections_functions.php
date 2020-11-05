defeec578684d4debcb06073e30607ee2d58eab1
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-11-05 12:47:49 +0000

    FIX: Smart collections showing all resources in tile on collections_featured.php [t25955]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@16375 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/collections_functions.php
+++ include/collections_functions.php
@@ -2033,8 +2033,7 @@ function get_featured_collection_resources(array $c, array $ctx)
 
         $node_search = NODE_TOKEN_PREFIX . $c['ref'];
 
-        // do_search() uses -1 to indicate no limit on fetchrows
-        $imgs_limit = (!is_null($imgs_limit) ? $imgs_limit : -1);
+        $imgs_limit = (!is_null($imgs_limit) ? $imgs_limit : 1);
 
         // Access control is still in place (ie permissions are honoured)
         $images = do_search($node_search, '', 'hit_count', 0, $imgs_limit, 'desc', false, 0, false, false, '', true, false, true);
