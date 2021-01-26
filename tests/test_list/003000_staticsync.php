2eb579687d8d4b65d610d7d056b565d79767c1a1
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-01-26 14:29:51 +0000

    NEW: Check in of branch for staticsync unit tests [q11607]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20210122_nbraddy_staticsync_tests_q11607@16834 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- tests/test_list/003000_staticsync.php
+++ tests/test_list/003000_staticsync.php
@@ -6,9 +6,10 @@ if('cli' != PHP_SAPI)
     }
 
 // Staticsync affects this so keep a copy and restore it later
-$saved_user_data = $userdata;
+$saved_user_data    = $userdata;
 $saved_userref      = $userref;
 $saved_perms        = $userpermissions;
+$userpermissions    = array("s","g","j*");
 
 // Set up staticsync to use a folder and make sure it exists.
 $syncdir=$storagedir . "/staticsync/";
@@ -45,9 +46,8 @@ file_put_contents($test_path . "testtextsync.txt","TEST");
 $sync_tree_field= create_resource_type_field("Sync tree", 0, FIELD_TYPE_CATEGORY_TREE, "synctree",TRUE);
 $staticsync_mapped_category_tree = $sync_tree_field;
 
-// Required for Test G
+// Required for Test G and H
 $projectspath = $test_path . "projects/conferenceA/";
-echo "Creating " . $projectspath;
 if (!file_exists($projectspath))
     {
     mkdir($projectspath,0777,true);
@@ -62,12 +62,49 @@ $staticsync_mapfolders[]=array
     "level"=>4
     );
 
+// Required for Test I
+$alternativespath = $projectspath . "projecta.txt_alternatives/";
+if (!file_exists($alternativespath))
+    {
+    mkdir($alternativespath,0777,true);
+    }
+file_put_contents($alternativespath . "projecta_alt1.txt","TEST_ALT1");
+file_put_contents($alternativespath . "projecta_alt2.txt","TEST_ALT2");
+
+// Required for Test J
+$staticsync_alternative_file_text = "_alt_";
+$aftpath = $test_path . "altfiletext/";
+if (!file_exists($aftpath))
+    {
+    mkdir($aftpath,0777,true);
+    }
+file_put_contents($aftpath . "aft_primary.txt","TEST_AFT_PRIMARY");
+file_put_contents($aftpath . "aft_primary_alt_testj.txt","TEST_AFT_ALT");
+
+// Required for Test K
+$staticsync_alt_suffixes = true;
+$staticsync_alt_suffix_array =array (
+   '_odd' => "Odd file",
+   '_side' => "Sidecar file",
+    );
+
+$altsuffixpath = $test_path . "alt_suffixes/";
+if (!file_exists($altsuffixpath))
+    {
+    mkdir($altsuffixpath,0777,true);
+    }
+file_put_contents($altsuffixpath . "alt_suffix_primary.txt","TEST_ALT_SUFFIX_PRIMARY");
+file_put_contents($altsuffixpath . "alt_suffix_primary_odd.txt","TEST_ALT_SUFFIX_ODD");
+file_put_contents($altsuffixpath . "alt_suffix_primary_side.txt","TEST_ALT_SUFFIX_SIDE");
+
 // Run staticsync, but hold back the output (has to be an include not a PHP exec so the above test config is used)
 ob_start();
+$argv=array();
+$argc=0;
 include (dirname(__FILE__) . "/../../pages/tools/staticsync.php");
 ob_clean();
 
-// Check the file has gone
+// Test A: check the file has gone
 if (file_exists($test_path . "teststatic.jpg"))
     {
     echo "Test A failed; File was not ingested.";
@@ -85,7 +122,7 @@ if (!is_array($results) || count($results)==0)
 $resid = $results[0]["ref"];
 
 // Test C: Check that $staticsync_autotheme worked
-$fcs = get_featured_collections(0,array());
+$fcs = get_featured_collections(0,array("access_control"=>false));
 $testfc = array_search("Test_folder",array_column($fcs,"name"));
 if($testfc===false)
     {
@@ -94,7 +131,7 @@ if($testfc===false)
     }
 
 // Test D: Check that $staticsync_autotheme created the correct sub featured collection
-$subfcs = get_featured_collections($fcs[$testfc]["ref"],array());
+$subfcs = get_featured_collections($fcs[$testfc]["ref"],array("access_control"=>false));
 $featuredfc = array_search("Featured",array_column($subfcs,"name"));
 if($featuredfc===false)
     {
@@ -114,37 +151,67 @@ if (!is_array($results) || count($results)==0 || $results[0]["resource_type"] !=
 $treedata = get_data_by_field($resid,$sync_tree_field);
 if($treedata != "test_folder, featured")
     {
-    echo "Test F failed: \$staticsync_mapped_category_tree failed";
+    echo "Test F failed: \$staticsync_mapped_category_tree failed - ";
     return false;
     }
-
 
 // Test G,H - Check extracting data using $staticsync_mapfolders works
 $results=do_search("projecta");
 if (!is_array($results) || count($results)==0)
     {
-    echo "Test G failed: \$staticsync_extension_mapping failed";
+    echo "Test G failed: \$staticsync_extension_mapping failed - ";
     return false;
     }
-$mappeddata = get_data_by_field($results[0]["ref"],$project_field);
-if(trim($mappeddata) != "Conference A")
+$projectresource=$results[0]["ref"];
+$mappeddata = get_data_by_field($projectresource,$project_field);
+if(trim($mappeddata) != "conferenceA")
     {
-    echo "Test H failed: \$staticsync_mapped_category_tree failed";
+    echo "Test H failed: \$staticsync_mapped_category_tree failed - ";
     return false;
     }
 
+// Test I - staticsync_alternatives_suffix
+$alts_i = get_alternative_files($projectresource);
+if(!is_array($alts_i) || count($alts_i) != 2)
+    {
+    echo "Test I failed: \$staticsync_alternatives_suffix failed - ";
+    return false;
+    }
+    
+// Test J - staticsync_alternative_file_text
+$results=do_search("aft_primary");
+if (!is_array($results) || count($results)==0)
+    {
+    echo "Test J failed: ingested file 'aft_primary.txt' could not be found - ";
+    return false;
+    }
+$aft_resource=$results[0]["ref"];
+$alts_j = get_alternative_files($aft_resource);
+if(!is_array($alts_j) || count($alts_j) != 1 || $alts_j[0]["description"] != "testj")
+    {
+    echo "Test J failed: \$staticsync_alternative_file_text failed - ";
+    return false;
+    }
 
-// Test H - staticsync_alternatives_suffix
-// Test I - staticsync_alternative_file_text
-// Test J - staticsync_alt_suffix_array
-
-
-// Check that the collection was created (TO DO)
-/*
-$themes_in_my_collections=true;
-$collections=get_resource_collections($results[0]["ref"]);
-print_r($collections);
-*/
+// Test K - staticsync_alt_suffix_array
+$results=do_search("alt_suffix_primary");
+if (!is_array($results) || count($results) != 1)
+    {
+    echo "Test K failed: ingested file 'alt_suffix_primary.txt' could not be found - ";
+    return false;
+    }
+$altsuffix_resource=$results[0]["ref"];
+$alts_k = get_alternative_files($altsuffix_resource);
+if(!is_array($alts_k) 
+    ||
+    count($alts_k) != 2 
+    ||
+    !match_values(array_column($alts_k,'name'),array_values($staticsync_alt_suffix_array))
+    )
+    {
+    echo "Test K failed: \$staticsync_alt_suffix_array failed - ";
+    return false;
+    }
 
 $userref            = $saved_userref;
 $userpermissions    = $saved_perms;
