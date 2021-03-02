71c6d6069f607d24b907770a30f2240a65c8e19b
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-03-02 10:04:55 +0000

    FIX: access control when running special search for collections [t27551][q11867]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@17064 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_functions.php
+++ include/search_functions.php
@@ -1279,7 +1279,8 @@ function search_special($search,$sql_join,$fetchrows,$sql_prefix,$sql_suffix,$or
         $upload_share_active = upload_share_active();
         $validcollections = $upload_share_active !== false ? get_session_collections(get_rs_session_id(), $userref) : array_column(get_user_collections($userref,"","name","ASC",-1,false), "ref");
 
-        if(validate_collection_parent($collection)=="" || (checkperm("j*")) || (checkperm("j" . validate_collection_parent($collection))))            {
+        if(in_array($collection, $validcollections) || featured_collection_check_access_control($collection))
+            {
             if(!collection_readable($collection))
                 {
                 return array();
