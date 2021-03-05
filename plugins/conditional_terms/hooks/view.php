60f0faee5c03034a5d6d8cf43fd2c801977fbe9f
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-03-05 16:53:58 +0000

    FIX: Conditional terms plugin applied to download buttons on view page. [t27504]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@17133 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/conditional_terms/hooks/view.php
+++ plugins/conditional_terms/hooks/view.php
@@ -2,7 +2,7 @@
 
 function HookConditional_termsViewDownloadlink($baseparams)
     {
-    global $baseurl, $resource, $conditional_terms_field, $conditional_terms_value, $fields, $search, $order_by, $archive, $sort, $offset;
+    global $baseurl, $resource, $conditional_terms_field, $conditional_terms_value, $fields, $search, $order_by, $archive, $sort, $offset, $download_usage;
 
     $showterms=false;
 
@@ -18,9 +18,16 @@ function HookConditional_termsViewDownloadlink($baseparams)
         return false;
         }
     
+    $redirect = "pages/download_progress.php?";
+
+    if ($download_usage)
+        {
+        $redirect = "pages/download_usage.php?";
+        }
+
     ?>href="<?php echo $baseurl ?>/pages/terms.php?<?php echo $baseparams ?>&amp;search=<?php
             echo urlencode($search) ?>&amp;url=<?php
-            echo urlencode("pages/download_progress.php?" . $baseparams . "&search=" . urlencode($search)
+            echo urlencode($redirect . $baseparams . "&search=" . urlencode($search)
                     . "&offset=" . $offset . "&archive=" . $archive . "&sort=".$sort."&order_by="
                     . urlencode($order_by))?>&noredir=true"<?php
     
