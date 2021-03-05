1c6233591e47d1e62d8ae3eba82cd32ddf22ab45
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-03-05 15:59:52 +0000

    FIX: Accessibility plugin display issues fixed, search result icons no longer obscured, consistent font size. [t27645]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@17130 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/accessibility/hooks/all.php
+++ plugins/accessibility/hooks/all.php
@@ -29,3 +29,13 @@ function HookAccessibilityAllAdditionalheaderjs() {
 	// Have the same functionality when ajax is not involved:
 	HookAccessibilityAllAfterheaderajax();
 }
+
+function HookAccessibilityAllThumbstextheight() {
+	// Required for the larger 18px font size
+	global $field_height, $resource_panel_icons_height, $resource_id_height, $resource_type_icon_height;
+	
+	$field_height = 35;
+	$resource_panel_icons_height = 35;
+	$resource_id_height = 25;
+	$resource_type_icon_height = 26;
+}
