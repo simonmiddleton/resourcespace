26a21385cdf448e535e166843522af3140c45c69
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-04-07 15:41:36 +0000

    FIX: Users without j permission can use multi-select to add resources to a collection. [t28129]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@17320 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_functions.php
+++ include/search_functions.php
@@ -1130,7 +1130,7 @@ function search_filter($search,$archive,$restypes,$starsearch,$recent_search_day
 function search_special($search,$sql_join,$fetchrows,$sql_prefix,$sql_suffix,$order_by,$orig_order,$select,$sql_filter,$archive,$return_disk_usage,$return_refs_only=false, $returnsql=false)
     {
     # Process special searches. These return early with results.
-    global $FIXED_LIST_FIELD_TYPES, $lang, $k;
+    global $FIXED_LIST_FIELD_TYPES, $lang, $k, $USER_SELECTION_COLLECTION;
     
     # View Last
     if (substr($search,0,5)=="!last") 
@@ -1284,12 +1284,6 @@ function search_special($search,$sql_join,$fetchrows,$sql_prefix,$sql_suffix,$or
         else
             {
             $user_collections = array_column(get_user_collections($userref,"","name","ASC",-1,false), "ref");
-            $selection_collection = array();
-            $user_selection_collection = get_user_selection_collection($userref);
-            if (isset($user_selection_collection))
-                {
-                $selection_collection[] = $user_selection_collection;
-                }
             $public_collections = array_column(search_public_collections('', 'name', 'ASC', true, false), 'ref');
             # include collections of requested resources
             $request_collections = array();
@@ -1305,7 +1299,7 @@ function search_special($search,$sql_join,$fetchrows,$sql_prefix,$sql_suffix,$or
                 include_once('research_functions.php');
                 $research_collections = array_column(get_research_requests(), 'collection');
                 }
-            $validcollections = array_unique(array_merge($user_collections, $selection_collection, $public_collections, $request_collections, $research_collections));
+            $validcollections = array_unique(array_merge($user_collections, array($USER_SELECTION_COLLECTION), $public_collections, $request_collections, $research_collections));
             }
 
         if(in_array($collection, $validcollections) || featured_collection_check_access_control($collection))
