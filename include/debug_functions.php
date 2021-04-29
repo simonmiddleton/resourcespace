8bcd6362e8fe2f82b1740cea7ebf09badc0ee8cc
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-04-29 10:14:01 +0000

    CHG: track variables of any type and log. Fail safe by attempting to stringify the variable. [branches/20210423_acota_q10595][q10595]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20210423_acota_q10595@17441 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/debug_functions.php
+++ include/debug_functions.php
@@ -68,16 +68,6 @@ function debug_function_call($name, array $args)
         return false;
         }
 
-    $stringify = function($value)
-        {
-        if(is_bool($value))
-            {
-            return ($value ? "true" : "false");
-            }
-
-        return trim(preg_replace('/\s+/m', ' ', print_r($value, true)));
-        };
-
     $args_str = "";
     $fct = new ReflectionFunction($name);
     foreach($fct->getParameters() as $param)
@@ -97,7 +87,7 @@ function debug_function_call($name, array $args)
             $value = $param->getDefaultValue();
             }
 
-        $args_str .= sprintf("\$%s = %s, ", $param->getName(), $stringify($value));
+        $args_str .= sprintf("\$%s = %s, ", $param->getName(), debug_stringify($value));
         }
     $args_str = rtrim($args_str, ", ");
 
@@ -128,6 +118,25 @@ function clear_tracking_vars_info(array $users)
 
 
 /**
+* Stringify variables for use in the debug log. This is used more as fallback to json_encode() failing to maintain quick
+* readability of the logs.
+* 
+* @param mixed $value Any value that needs stringified
+* 
+* @return string
+*/
+function debug_stringify($value)
+    {
+    if(is_bool($value))
+        {
+        return ($value ? 'true' : 'false');
+        }
+
+    return trim(preg_replace('/\s+/m', ' ', print_r($value, true)));
+    }
+
+
+/**
 * Check if ResourceSpace is still tracking variables for debug purposes.
 * 
 * @uses get_sysvar() to return global scope data.
@@ -149,7 +158,11 @@ function is_tracking_vars_active(int $user)
 
 
 /**
+* Get all tracked variables (for debug) for user
 * 
+* @param int $user User ID
+* 
+* @return array List of variable names
 */
 function get_tracked_vars(int $user)
     {
@@ -166,41 +179,57 @@ function get_tracked_vars(int $user)
 
 
 /**
+* Debug log tracked variables (as configured in System > System console).
+* 
+* IMPORTANT: the debug log will contain the JSON encoded version of the tracked variable. For further analysis, just copy
+* the value (ie. everything within double quotes) and prettyfi it in your IDE.
+* 
 * Partially implements structured data from RFC 5424 @see https://tools.ietf.org/html/rfc5424
+* 
+* @param string $place The place/entity this is being logged at. For functions use a position keyword (before, begin, end,
+*                      after), the @ sign and then the function name (e.g end@include_plugin_config). In other cases it
+*                      might be better to record the line "e.g line-59@include/db.php".
+*                      IMPORTANT: it is important to keep the location format consistent to help developers filter faster
+*                      when processing the debug log for tracked vars.
+* @param array $vars   Defined variables within the scope the function was called from
+* 
 */
-function debug_track_vars(string $ns)
+function debug_track_vars(string $place, array $vars)
     {
-    /*
-    SYSLOG-MSG      = HEADER SP STRUCTURED-DATA [SP MSG]
-
-    STRUCTURED-DATA = NILVALUE / 1*SD-ELEMENT
-    SD-ELEMENT      = "[" SD-ID *(SP SD-PARAM) "]"
-    SD-PARAM        = PARAM-NAME "=" %d34 PARAM-VALUE %d34
-    SD-ID           = SD-NAME
-    PARAM-NAME      = SD-NAME
-    PARAM-VALUE     = UTF-8-STRING ; characters '"', '\' and
-                                 ; ']' MUST be escaped.
-    SD-NAME         = 1*32PRINTUSASCII
-                    ; except '=', SP, ']', %d34 (")
-    */
-
-    $pid = getmypid();
-    $format = 'tracking var: [pid=%s ns="%s"][%s="%s"]';
+    $pid = getmypid() ?: 'Undefined';
+    $format = 'tracking var: [pid=%1$s place="%2$s"][%3$s="%4$s"]';
+    $format_json_err = 'tracking var: [pid=%1$s place="%2$s"][error] JSON error "%3$s" when $%4$s = %5$s';
 
     // For readability reasons, we show each tracked var on a new line in the debug log. If performance is badly affected,
     // we can switch to combine all tracked vars in the last SD-ELEMENT (ie [var1="value" var2="value"])
     $tracked_vars = get_tracked_vars($GLOBALS['userref'] ?? 0);
     foreach($tracked_vars as $tracked_var)
         {
-        // TODO: continue work here. We need to get the value of the tracked_var from input or global scope
-        debug(sprintf($format, $pid, $ns, $tracked_var, 'none'));
+        if(!isset($vars[$tracked_var]))
+            {
+            continue;
+            }
+
+        // JSON encode the tracked variables' value. If it fails, attempt to log this in the debug log.
+        $tracked_var_value = json_encode($vars[$tracked_var], JSON_NUMERIC_CHECK);
+        if(json_last_error() !== JSON_ERROR_NONE)
+            {
+            $json_last_error_msg = json_last_error_msg();
+
+            debug(
+                sprintf(
+                    $format_json_err,
+                    $pid,
+                    $place,
+                    json_last_error_msg(),
+                    $tracked_var,
+                    debug_stringify($vars[$tracked_var])
+                )
+            );
+
+            continue;
+            }
+
+        debug(sprintf($format, $pid, $place, $tracked_var, $tracked_var_value));
         }
-
-
-
-
-
-
-
-    // return debug($msg);
     }
