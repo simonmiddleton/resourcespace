6fe50f9a9ad6432f19282dc4a801976a2844b76d
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-05-07 11:00:17 +0000

    FIX: Simple LDAP plugin error if binding failed[notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@17496 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/simpleldap/include/simpleldap_functions.php
+++ plugins/simpleldap/include/simpleldap_functions.php
@@ -61,7 +61,14 @@ function simpleldap_authenticate($username,$password){
 		foreach ($binddomains as $binddomain)
 			{
 			debug("LDAP - Attempting to bind to LDAP server as : " . $username . "@" .  $binddomain);
-			$login = @ldap_bind( $ds, "$ldap_username@" . $binddomain, $password );
+            $GLOBALS["use_error_exception"] = true;
+            try {
+			$login = ldap_bind( $ds, "$ldap_username@" . $binddomain, $password );
+            }
+            catch(Exception $e){
+                debug("ERROR: LDAP bind failed " . $e->errorMessage());
+                $login=false;
+            }
 			if (!$login){continue;}else{$userdomain=$binddomain;break;}
 			}
 		if (!$login){debug("LDAP - failed to bind to LDAP server");	return false; }
