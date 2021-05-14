7bbc50458e2566be245534897ba663e0cec91632
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-05-14 14:19:01 +0000

    NEW: upgrade script for updating password column [branches/20210513_acota_q10529][q10529]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20210513_acota_q10529@17556 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/definitions.php
+++ include/definitions.php
@@ -1,7 +1,7 @@
 <?php
 
 // current upgrade level of ResourceSpace (used for migration scripts, will set sysvar using this if not already defined)
-define('SYSTEM_UPGRADE_LEVEL', 15);
+define('SYSTEM_UPGRADE_LEVEL', 16);
 
 // PHP VERSION AND MINIMUM SUPPORTED
 if (!defined('PHP_VERSION_ID'))
