93e4e0e474548eaac74876a1d504050e3a8fe920
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-05-28 15:56:59 +0000

    NEW: Fixed error on video tracks setup page [q10585]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20210329_jhardy_video_splice_q10585@17681 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/video_tracks/config/config.php
+++ plugins/video_tracks/config/config.php
@@ -11,3 +11,10 @@ $video_tracks_export_folder="";
 $video_tracks_process_size_limit=100;
 
 $video_tracks_allow_original_custom_formats = false;
+
+$video_tracks_output_formats_saved=base64_encode(serialize(array(
+    "mp4"=>array(
+            "command"=>"-f mp4 -ar 22050 -b 650k -ab 32k -ac 1",
+            "extension"=>"mp4")             
+            )
+    ));
