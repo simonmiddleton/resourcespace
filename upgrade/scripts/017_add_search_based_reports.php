9a3ab5d85588e2d902a59913844538f93db75a3a
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-06-04 08:38:54 +0000

    CHG: Upgrade script to manually add the extra search based reports to existing systems. [q8345]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@17725 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ upgrade/scripts/017_add_search_based_reports.php
@@ -0,0 +1,26 @@
+<?php
+
+// Add the new reports created for report-from-search functionality
+$path=dirname(__FILE__)."/../../dbstruct/data_report.txt";
+
+$f=fopen($path,"r");
+while (($row = fgetcsv($f,5000)) !== false)
+    {
+    # Escape values
+
+    // Pull off the ref - not inserted but used to check we're inserting the new reports only.
+    $ref=$row[0];
+    array_shift($row);
+
+    for ($n=0;$n<count($row);$n++)
+        {
+        $row[$n]=escape_check($row[$n]);
+        $row[$n]="'" . $row[$n] . "'";
+        if ($row[$n]=="''") {$row[$n]="null";}
+        }
+    if (in_array($ref,array(22,23,24)))
+        {
+        $sql="insert into report values (null," . join (",",$row) . ")";
+        sql_query($sql,false,-1,false);
+        }
+    }
