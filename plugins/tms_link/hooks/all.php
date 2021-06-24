a381ed1bf80b8f031d703d9c21a5f2c112a01f24
tgray <tgray@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-06-24 09:43:53 +0000

    FIX: tms_link - add error handling for tms_link_get_tms_data() [t27965]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@17913 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/tms_link/hooks/all.php
+++ plugins/tms_link/hooks/all.php
@@ -87,6 +87,12 @@ function HookTms_linkAllUpdate_field($resource, $field, $value, $existing)
     $tms_object_id = intval($value);
     $tmsdata = tms_link_get_tms_data($resource, $tms_object_id);
 
+    // if call to tms_link_get_tms_data() does not return an array, error has occurred
+    if (!is_array($tmsdata))
+        {
+        return $tmsdata;  // return error message
+        }
+
     debug("tms_link: updating resource id #" . $resource);
 
     foreach(tms_link_get_modules_mappings() as $module)
