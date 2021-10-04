11b9e6568c972ac3b022526131a2476e122a0c76
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-10-04 10:15:59 +0000

    CHG: Update version.php as per process. [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.7_RC@18358 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/version.php
+++ include/version.php
@@ -1,8 +1,8 @@
 <?php
 $productname="ResourceSpace"; # Product name. Do not change.
-$productversion="SVN Trunk";
+$productversion="SVN 9.7";
 
 # External items reload key
 # Increment the below value to force a reload of the CSS, JavaScripts and other included items.
 # This is intended for developer use when altering such files. It will force a reload on all client browsers.
-$css_reload_key = 222;
+$css_reload_key = 397;
