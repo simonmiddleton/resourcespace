6a5c48f76f9a24cbc16e4607a67aad5f3d112bb9
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-11-16 17:17:53 +0000

    FIX: Prevent plugin config files from being included multiple times [t30130]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@18680 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/team/team_user_edit.php
+++ pages/team/team_user_edit.php
@@ -110,16 +110,13 @@ if(getval('loginas', '') != '')
     global $CSRF_token_identifier, $usersession;
 
     //userkey and CSRF tokens still need to be placed in post array as preform_login() references these directly
-    $_POST                         = [];
-    $_POST['userkey']              = md5(escape_check($user["username"]) . $scramble_key);
+    $_POST = [];
+    $_POST['username'] = $user['username'];
+    $_POST['password'] = $user['password'];
+    $_POST['userkey'] = md5(escape_check($user["username"]) . $scramble_key);
     $_POST[$CSRF_token_identifier] = generateCSRFToken($usersession, 'autologin');
 
-    $result=perform_login($user['username'], $user['password']);
-	if ($result['valid'])
-		{
-        set_login_cookies($result["ref"],$session_hash,$language, $user_preferences);
-        redirect($baseurl . '/pages/home.php');
-        }
+    include '../../login.php';
     exit();
     }
 ?>
