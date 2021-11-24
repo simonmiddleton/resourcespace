dd31392c7e241f8438e12302074d022510db0700
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-11-24 15:39:51 +0000

    CHG: Updated include/message_functions.php to use prepared statements [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@18729 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/message_functions.php
+++ include/message_functions.php
@@ -99,10 +99,10 @@ function message_add($users,$text,$url="",$owner=null,$notification_type=MESSAGE
 
 	if (is_null($owner))
 		{
-		$owner = 'NULL';
+		$owner = NULL;
 		}
 
-	ps_query("INSERT INTO `message` (`owner`, `created`, `expires`, `message`, `url`, `related_activity`, `related_ref`, `type`) VALUES (? , NOW(), DATE_ADD(NOW(), INTERVAL ? SECOND), ?, ?, ?, ?, ?)", array("s",$owner,"i",$ttl_seconds,"s",$text,"s",$url,"i",$related_activity,"i",$related_ref,"i",$notification_type));
+	ps_query("INSERT INTO `message` (`owner`, `created`, `expires`, `message`, `url`, `related_activity`, `related_ref`, `type`) VALUES (? , NOW(), DATE_ADD(NOW(), INTERVAL ? SECOND), ?, ?, ?, ?, ?)", array("i",$owner,"i",$ttl_seconds,"s",$text,"s",$url,"i",$related_activity,"i",$related_ref,"i",$notification_type));
 	$message_ref = sql_insert_id();
 
 	foreach($users as $user)
