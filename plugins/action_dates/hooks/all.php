31271f1ce01344619e041ba4edab152113536559
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-12-07 16:13:50 +0000

    CHG: Updated action_dates plugin to use prepared statements[q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@18805 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/action_dates/hooks/all.php
+++ plugins/action_dates/hooks/all.php
@@ -26,13 +26,12 @@ function HookAction_datesCronCron()
     global $userref;
     $userref=0;
 
-    $eligible_states_list="";
+    $eligible_states = array();
     if (isset($action_dates_eligible_states) && is_array($action_dates_eligible_states)) {
-        $eligible_states_list = implode(",",$action_dates_eligible_states);
+        $eligible_states = $action_dates_eligible_states;
     }
 
-	$allowable_fields=sql_array("select ref as value from resource_type_field where type in (4,6,10)", "schema");
-    
+	$allowable_fields=ps_array("select ref as value from resource_type_field where type in (4,6,10)",[], "schema");
     $email_state_refs    = array();   # List of refs which are due to undergo state change (including full deletion) in n days 
     $email_restrict_refs = array();   # List of refs which are due to be restricted in n days
     $state_change_notify = array();   # List of refs whose state has changed (excluding full deletion) 
@@ -44,9 +43,13 @@ function HookAction_datesCronCron()
         $fieldinfo = get_resource_type_field($action_dates_restrictfield);
         echo "action_dates: Checking restrict action field $action_dates_restrictfield.".$LINE_END;
 
-        $restrict_resources=sql_query("SELECT rd.resource, rd.value FROM resource_data rd LEFT JOIN resource r ON r.ref=rd.resource "
-            . ($eligible_states_list == "" ? "" : "AND r.archive IN ({$eligible_states_list})")    
-            . " WHERE r.ref > 0 and r.access=0 and rd.resource_type_field = '$action_dates_restrictfield' and rd.value <>'' and rd.value is not null");
+        $sql = "SELECT rd.resource, rd.value FROM resource_data rd LEFT JOIN resource r ON r.ref=rd.resource ";
+        $sql .= (!empty($eligible_states) ? "" : "AND r.archive IN (" . ps_param_insert(count($eligible_states)) . ") ");
+        $sql .= "WHERE r.ref > 0 and r.access=0 and rd.resource_type_field = ? and rd.value <>'' and rd.value is not null";
+        $sql_params = ps_param_fill($eligible_states,"i");
+        $sql_params = array_merge($sql_params, array('i',$action_dates_restrictfield));
+
+        $restrict_resources=ps_query($sql,$sql_params);
 		foreach ($restrict_resources as $resource)
 			{
 			$ref=$resource["resource"];
@@ -54,11 +57,11 @@ function HookAction_datesCronCron()
 			if (time()>=strtotime($resource["value"]))		
 				{		
 				# Restrict access to the resource as date has been reached
-				$existing_access=sql_value("select access as value from resource where ref='$ref'","");
+				$existing_access=ps_value("select access as value from resource where ref=?",["i",$ref],"");
 				if($existing_access==0) # Only apply to resources that are currently open
 					{
 					echo " - Restricting resource {$ref}".$LINE_END;
-					sql_query("update resource set access=1 where ref='$ref'");
+					ps_query("update resource set access=1 where ref=?",["i",$ref]);
 					resource_log($ref,'a','',$lang['action_dates_restrict_logtext'],$existing_access,1);		
 					}
 				}
@@ -89,9 +92,11 @@ function HookAction_datesCronCron()
         if($action_dates_reallydelete)
             {
             # FULL DELETION - Build candidate list of resources which have the deletion date field populated
-            $candidate_resources = sql_query("SELECT rd.resource, rd.value FROM resource r LEFT JOIN resource_data rd ON r.ref = rd.resource " 
-                                    . ($eligible_states_list == "" ? "" : " AND r.archive IN ({$eligible_states_list})")    
-                                    . " WHERE r.ref > 0 AND rd.resource_type_field = '{$action_dates_deletefield}' AND value <> '' AND rd.value IS NOT NULL");
+            $sql  ="SELECT rd.resource, rd.value FROM resource r LEFT JOIN resource_data rd ON r.ref = rd.resource ";
+            $sql .=(!empty($eligible_states) ? "" : " AND r.archive IN (" . ps_param_insert(count($eligible_states)) . ") ");
+            $sql .="WHERE r.ref > 0 AND rd.resource_type_field = ? AND value <> '' AND rd.value IS NOT NULL";
+            $sql_params=array("i",$action_dates_deletefield);
+            $candidate_resources = ps_query($sql,$sql_params);
             }
         else
             {
@@ -102,10 +107,22 @@ function HookAction_datesCronCron()
                 }
             # NOT FULL DELETION - Build candidate list of resources which have the deletion date field populated
             #                     and which are neither in the resource deletion state nor in the action dates new state
-            $candidate_resources = sql_query("SELECT rd.resource, rd.value FROM resource r LEFT JOIN resource_data rd ON r.ref = rd.resource " 
-                                    . ($eligible_states_list == "" ? "" : " AND r.archive IN ({$eligible_states_list})")    
-                                    . " AND r.archive NOT IN ({$resource_deletion_state},{$action_dates_new_state}) " 
-                                    . " WHERE r.ref > 0 AND rd.resource_type_field = '{$action_dates_deletefield}' AND value <> '' AND rd.value IS NOT NULL");
+            $sql = "SELECT rd.resource, rd.value FROM resource r LEFT JOIN resource_data rd ON r.ref = rd.resource ";
+            $sql_params = array();
+
+            if (!empty($eligible_states))
+                {
+                $sql .= " AND r.archive IN (" . ps_param_insert(count($eligible_states)) . ")";
+                $sql_params = array_merge($sql_params,ps_param_fill($eligible_states,"i"));
+                }
+
+            $sql .= " AND r.archive NOT IN (?,?) ";
+            $sql_params = array_merge($sql_params,["i",$resource_deletion_state,"i",$action_dates_new_state]);
+
+            $sql .= " WHERE r.ref > 0 AND rd.resource_type_field = ? AND value <> '' AND rd.value IS NOT NULL";
+            $sql_params = array_merge($sql_params,["i",$action_dates_deletefield]);
+
+            $candidate_resources = ps_query($sql,$sql_params);
 
             # NOT FULL DELETION - Resolve the target archive state to which candidate resources are to be moved
             # If the new state differs from the default resource deletion state, it means we only want to move resources to that state
@@ -143,12 +160,12 @@ function HookAction_datesCronCron()
                 else
                     {
                     # NOT FULL DELETION - Update resources to the target archive state
-                    sql_query("UPDATE resource SET archive = '{$resource_deletion_state}' WHERE ref = '{$ref}'");
+                    ps_query("UPDATE resource SET archive = ? WHERE ref = ?",["i",$resource_deletion_state,"i",$ref]);
                     $state_change_notify[] = $ref;
                     if($action_dates_remove_from_collection)
                         {
                         // Remove the resource from any collections
-                        sql_query("delete from collection_resource where resource='$ref'");
+                        ps_query("delete from collection_resource where resource=?",["i",$ref]);
                         }
                 
                     }
@@ -312,7 +329,24 @@ function HookAction_datesCronCron()
         if(in_array($datefield['type'],$DATE_FIELD_TYPES))
             {
             echo "action_dates: Checking extra action dates for field " . $datefield["ref"] . "." . $LINE_END;
-            $additional_resources=sql_query("SELECT rd.resource, rd.value FROM resource_data rd LEFT JOIN resource r ON r.ref=rd.resource WHERE r.ref > 0 AND rd.resource_type_field = '$field' AND rd.value <>'' AND rd.value IS NOT null AND r.archive<>'$resource_deletion_state' AND r.archive<>'$newstatus'");
+            $sql="SELECT 
+                rd.resource, 
+                rd.value 
+            FROM resource_data rd 
+                LEFT JOIN resource r ON r.ref=rd.resource 
+            WHERE r.ref > 0 
+                AND rd.resource_type_field = '$field' 
+                AND rd.value <>'' 
+                AND rd.value IS NOT null 
+                AND r.archive<>'$resource_deletion_state' 
+                AND r.archive<>'$newstatus'";
+            
+            $sql_params=array(
+                "i",$field,
+                "i",$resource_deletion_state,
+                "i",$newstatus
+            );
+            $additional_resources=ps_query($sql,$sql_params);
             
             foreach ($additional_resources as $resource)
                 {
