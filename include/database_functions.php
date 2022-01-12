b8d15ed568f46704f3423677442ab51d0ee5d2ec
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-01-12 15:39:00 +0000

    CHG: node name type to mediumtext. NEW: dbstruct support for updating from text to mediumtext. [branches/20220112_acota_q11023][q11023]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20220112_acota_q11023@18922 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/database_functions.php
+++ include/database_functions.php
@@ -1319,12 +1319,13 @@ function CheckDBStruct($path,$verbose=false)
                                     # Check the column is of the correct type
                                     preg_match('/\s*(\w+)\s*\((\d+)\)/i',$basecoltype,$matchbase);
                                     preg_match('/\s*(\w+)\s*\((\d+)\)/i',$existingcoltype,$matchexisting);
+
                                     // Checks added so that we don't trim off data if a varchar size has been increased manually or by a plugin. 
                                     // - If column is of same type but smaller number, update
                                     // - If target column is of type text, update
                                     // - If target column is of type varchar and currently int, update (e.g. the 'archive' column in collection_savedsearch moved from a single state to a multiple)
+                                    // - If target column is of type mediumtext and currently is text, update
                                     // - If target column is of type longtext and currently is text
-
                                     if(
                                         (count($matchbase) == 3 && count($matchexisting) == 3 && $matchbase[1] == $matchexisting[1] && $matchbase[2] > $matchexisting[2])
                                         || (stripos($basecoltype, "text") !== false && stripos($existingcoltype, "text") === false)
@@ -1334,6 +1335,7 @@ function CheckDBStruct($path,$verbose=false)
                                         && (strtoupper(substr($existingcoltype,0,7))=="TINYINT" || strtoupper(substr($existingcoltype,0,8))=="SMALLINT")
                                         )
                                         || (strtoupper(substr($basecoltype, 0, 7)) == "VARCHAR" && strtoupper(substr($existingcoltype, 0, 3) == "INT"))
+                                        || (strtoupper(substr($basecoltype, 0, 10)) == "MEDIUMTEXT" && strtoupper(substr($existingcoltype, 0, 4) == "TEXT"))
                                         || (strtoupper(substr($basecoltype, 0, 8)) == "LONGTEXT" && strtoupper(substr($existingcoltype, 0, 4) == "TEXT"))
                                         )
                                         {
