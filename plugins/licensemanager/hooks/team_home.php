118c4717f070cf3a09aa0d21e0e29eb0c9b33e7a
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-01-14 11:07:06 +0000

    FIX: Only render menu options for consent and license manager if the user has permission to access the page [t30483]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@18932 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/licensemanager/hooks/team_home.php
+++ plugins/licensemanager/hooks/team_home.php
@@ -3,7 +3,10 @@
 function HookLicensemanagerTeam_homeCustomteamfunction()
     {
     global $lang,$baseurl_short;
+    if(checkperm('a'))
+        {
     ?>
     <li><a href="<?php echo $baseurl_short?>plugins/licensemanager/pages/list.php" onClick="return CentralSpaceLoad(this,true);"><i aria-hidden="true" class="fa fa-fw fa-scroll"></i><br /><?php echo $lang["managelicenses"]?></a></li>
     <?php
+        }
     }
