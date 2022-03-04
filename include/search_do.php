69b2aab714736fe5ebde915668a5d30ca236351b
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-03-04 08:28:55 +0000

    FIX: [9.8] Increment $last_key_offset when searching to allow for consecutively skipped $noadd words. [t30734]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.8@19186 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_do.php
+++ include/search_do.php
@@ -931,18 +931,17 @@ function do_search(
 					}
                
                 $qk=1; // Set the counter to the first keyword
+                $last_key_offset=1;
 				foreach($quotedkeywords as $quotedkeyword)
 					{
 					global $noadd, $wildcard_always_applied, $wildcard_always_applied_leading;
 					if (in_array($quotedkeyword, $noadd)) # skip common words that are excluded from indexing
 						{
-						$skipped_last = true;       
+						# Support skipped keywords - if the last keyword was skipped (listed in $noadd), increase the allowed position from the previous keyword. Useful for quoted searches that contain $noadd words, e.g. "black and white" where "and" is a skipped keyword.
+						++$last_key_offset;
 						}
 					else
 						{
-						$last_key_offset=1;
-						if (isset($skipped_last) && $skipped_last) {$last_key_offset=2;} # Support skipped keywords - if the last keyword was skipped (listed in $noadd), increase the allowed position from the previous keyword. Useful for quoted searches that contain $noadd words, e.g. "black and white" where "and" is a skipped keyword.
-						
 						$keyref = resolve_keyword($quotedkeyword, false,true,false); # Resolve keyword.
                         if ($keyref === false)
                             {
@@ -996,7 +995,7 @@ function do_search(
                             $fixedunion .=" JOIN `node_keyword` nk_[union_index]_" . $qk . " ON nk_[union_index]_" . $qk . ".node = nk_[union_index]_" . ($qk-1) . ".node AND nk_[union_index]_" . $qk . ".keyword = '" . $keyref . "' AND  nk_[union_index]_" . $qk . ".position=nk_[union_index]_" . ($qk-1) . ".position+" . $last_key_offset ;
                             }
                         
-                        $skipped_last=false;
+                        $last_key_offset=1;
                         $qk++;
                         } // End of if keyword not excluded (not in $noadd array)
                     } // End of each keyword in quoted string
