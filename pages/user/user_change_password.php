9c5aed5c6da377dbd0c549e5483a5b939e0570d2
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-03-11 16:41:14 +0000

    CHG: Updated user_change_password.php to use prepared statements. [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19236 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/user/user_change_password.php
+++ pages/user/user_change_password.php
@@ -8,7 +8,7 @@ if($resetvalues!="")
     {
     if(substr($resetvalues,0,2) == "3D")
         {
-        // Email Smay have encoded the = character
+        // Email may have encoded the = character
         $resetvalues = substr($resetvalues,2);
         }
         
@@ -16,7 +16,7 @@ if($resetvalues!="")
     $resetuserref=substr($resetvalues,0,$rplength-15);
     $resetkey=substr($resetvalues,$rplength-15);
     $valid_reset_link=false;
-	$resetvaliduser=sql_query("select ref, username, email, fullname, usergroup, password, password_reset_hash, last_active from user where ref='" . escape_check($resetuserref)  . "'",""); 
+	$resetvaliduser = ps_query("SELECT ref, username, email, fullname, usergroup, password, password_reset_hash, last_active FROM user WHERE ref = ?", ["i", $resetuserref]);
 	if(count($resetvaliduser)==1)
 		{
 		$resetuser=$resetvaliduser[0];
