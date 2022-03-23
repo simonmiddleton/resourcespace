cf17c3771f34df21af5d56995cd8ec460be66066
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-03-23 15:46:34 +0000

    CHG: Updated batch/cron_jobs/009_flush_unused_keywords.php to use prepared statements. [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19301 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- batch/cron_jobs/009_flush_unused_keywords.php
+++ batch/cron_jobs/009_flush_unused_keywords.php
@@ -5,16 +5,16 @@ if($sysvars["upgrade_system_level"] < 2)
     // After migrating to nodes it is prefereable to delete resource_keyword rows for fixed list data
     // as this is now stored in normalised form in resource_node and node_keyword
     
-    $fixed_fields=sql_array("SELECT ref value FROM resource_type_field WHERE type IN ('" . join("','",$FIXED_LIST_FIELD_TYPES) . "') ");
+    $fixed_fields = ps_array("SELECT ref value FROM resource_type_field WHERE type IN (" . ps_param_insert(count($FIXED_LIST_FIELD_TYPES)) . ")", ps_param_fill($FIXED_LIST_FIELD_TYPES, "i"));
     
     foreach($fixed_fields as $fixed_field)
         {
         echo " - Deleting resource_keyword data for field ref#" . $fixed_field . $LINE_END;
-        $count_query="SELECT COUNT(*) value FROM resource_keyword WHERE resource_type_field='" . $fixed_field . "'";
-        $c=sql_value($count_query,0);
+        $count_query = "SELECT COUNT(*) value FROM resource_keyword WHERE resource_type_field = ?";
+        $c = ps_value($count_query, array("i", $fixed_field), 0);
         while ($c>0)
             {
-            sql_query("DELETE FROM resource_keyword WHERE resource_type_field ='" . $fixed_field . "' LIMIT 1000");
+            ps_query("DELETE FROM resource_keyword WHERE resource_type_field = ? LIMIT 1000", array("i", $fixed_field));
             $c = $c - 1000;
             echo (" -- " . $c . " rows remaining to delete" . $LINE_END);
             }
