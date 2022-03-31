ab2b00a6054164ee067ccd1b9ca4b422e8079223
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-03-31 10:43:19 +0000

    CHG: added more documentation for the new constant [branches/20220323_acota_q10015][q10015]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20220323_acota_q10015@19384 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/definitions.php
+++ include/definitions.php
@@ -465,7 +465,21 @@ const SYSTEM_REQUIRED_PHP_MODULES = [
 ];
 
 /*
-Example of a "utility" entry structure:-
+List of ResourceSpace system utilities (core and optional). If adding a new entry, make sure get_utility_path() is updated
+as well to handle the new entry.
+
+A system utility structure will have the following keys/properties:-
+- required = is this utility core to ResourceSpace?
+- path_var_name = the variable name holding the utility path {@see get_utility_path()}
+- display_name = self explanatory
+- show_on_check_page = when we use multiple components of a bigger "package" (e.g IM has convert, identify, composite and mogrify)
+- version_check - argument = the argument used to get the version out (NB: some utilities do this on STDERR). Default: -version
+- version_check - callback = function used to verify we got the expected version. It should return an array as expected 
+    by the check.php page:
+    * - utility - Updated utility structure (if required to do so)
+    * - found - PHP bool representing whether we've found what we were expecting in the version output.
+
+Example:
 'utilityname' => [
     'required' => false,
     'path_var_name' => 'utilityname_path',
