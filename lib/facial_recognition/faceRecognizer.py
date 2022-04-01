512c2fba4eb31c87a1bb23ef610bd4bb10a42c12
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-04-01 12:05:45 +0000

    CHG: code to work with Python 3 [branches/20220331_acota_q12244][q12244]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20220331_acota_q12244@19399 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- lib/facial_recognition/faceRecognizer.py
+++ lib/facial_recognition/faceRecognizer.py
@@ -1,7 +1,7 @@
 import os, sys, cv2, json
 
 if 3 > len(sys.argv):
-    print >> sys.stderr, 'Try running: faceRecognizer.py <path/to/lbph_model.xml> <path/to/image.ext>'
+    print('Try running: faceRecognizer.py <path/to/lbph_model.xml> <path/to/image.ext>', file = sys.stderr)
     sys.exit(1)
 
 # Init
@@ -9,11 +9,11 @@ LBPH_MODEL_PATH = sys.argv[1]
 IMAGE_PATH      = sys.argv[2]
 
 if not os.path.isfile(LBPH_MODEL_PATH):
-    print >> sys.stderr, "No model state file '{}'".format(LBPH_MODEL_PATH)
+    print("No model state file '{}'".format(LBPH_MODEL_PATH), file = sys.stderr)
     sys.exit(1)
 
 if not os.path.isfile(IMAGE_PATH):
-    print >> sys.stderr, "No test data file at '{}'".format(IMAGE_PATH)
+    print("No test data file at '{}'".format(IMAGE_PATH), file = sys.stderr)
     sys.exit(1)
 
 (major, minor, _) = cv2.__version__.split(".")
@@ -29,11 +29,10 @@ elif major == 4 and minor > 2:
 else:
     model = cv2.face.createLBPHFaceRecognizer()
     model.load(LBPH_MODEL_PATH)
-
 testImage = cv2.imread(IMAGE_PATH, 0)
 
 if testImage is None:
-    print >> sys.stderr, "Error: Could not read '{}'".format(IMAGE_PATH)
+    print("Error: Could not read '{}'".format(IMAGE_PATH), file = sys.stderr)
     sys.exit(1)
 
 predictedData = model.predict(testImage)
