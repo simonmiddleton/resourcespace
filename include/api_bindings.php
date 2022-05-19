babe996f7645d622baacf77d595399ad9b838642
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-05-19 11:20:20 +0000

    FIX: add extra check for API call [notag][9.8]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/9.8@19628 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/api_bindings.php
+++ include/api_bindings.php
@@ -867,7 +867,7 @@ function api_get_data_by_field($ref, $field)
         }
 
     // Get the data for a specific field for a specific resource.
-    $results = get_data_by_field($ref, $field);
+    $results = metadata_field_view_access($field) ? get_data_by_field($ref, $field) : false;
     
     if(is_array($results))
         {
