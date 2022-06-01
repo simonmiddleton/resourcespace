661c45bd15f3e685c9b70a4fe6e89217d5b80561
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-06-01 09:38:48 +0000

    CHG: Port of tms_link plugin to prepared statements [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19695 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/tms_link/pages/tms_module_config.php
+++ plugins/tms_link/pages/tms_module_config.php
@@ -153,7 +153,7 @@ include '../../../include/header.php';
                             <select class="medwidth" name="tms_rs_mappings[<?php echo $tms_rs_mapping_index; ?>][rs_field]">
                                 <option value=""><?php echo $lang['select']; ?></option>
                         <?php
-                        $fields = sql_query('SELECT * FROM resource_type_field ORDER BY title, name', "schema");
+                        $fields = ps_query('SELECT * FROM resource_type_field ORDER BY title, name', array(), "schema");
                         foreach($fields as $field)
                             {
                             $selected = ($tms_rs_mapping['rs_field'] == $field['ref'] ? ' selected' : '');
@@ -197,7 +197,7 @@ include '../../../include/header.php';
                 new_row_html += '<td><select class="medwidth" name="tms_rs_mappings[' + row_index + '][rs_field]">';
                 new_row_html += '<option value=""><?php echo $lang['select']; ?></option>';
                 <?php
-                $fields = sql_query('SELECT * FROM resource_type_field ORDER BY title, name', "schema");
+                $fields = ps_query('SELECT * FROM resource_type_field ORDER BY title, name', array(), "schema");
                 foreach($fields as $field)
                     {
                     $option_text = lang_or_i18n_get_translated($field['title'], 'fieldtitle-');
