e22e90d7aa6210224e435b34afa8ab63c4b8f9d2
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-06-15 14:29:22 +0000

    CHG: Porting team_dash_admin.php to prepared statements [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19779 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/team/team_dash_admin.php
+++ pages/team/team_dash_admin.php
@@ -70,8 +70,8 @@ if(getvalescaped("quicksave",FALSE))
 			else
 				{
 				#Add to the front of the pile if the user already has the tile
-				sql_query("DELETE FROM user_dash_tile WHERE dash_tile=".$tile["ref"]);
-				sql_query("INSERT user_dash_tile (user,dash_tile,order_by) SELECT user.ref,'".$tile["ref"]."',5 FROM user");
+				ps_query("DELETE FROM user_dash_tile WHERE dash_tile= ?", ['i', $tile["ref"]]);
+				ps_query("INSERT user_dash_tile (user,dash_tile,order_by) SELECT user.ref,'".$tile["ref"]."',5 FROM user");
 
 				$dtiles_available = get_alluser_available_tiles();
 				exit("positiveglow");
