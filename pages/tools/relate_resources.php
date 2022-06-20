e5a8375c17214334dc019d235b7298d24764226c
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-06-20 15:02:42 +0000

    CHG: Updated relate_resources to use prepared statements [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19820 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/relate_resources.php
+++ pages/tools/relate_resources.php
@@ -39,7 +39,7 @@ foreach($rlist as $k => $v)
         continue;
         }
 
-    if(sql_value("SELECT count(ref) AS `value` FROM resource WHERE ref = '" . escape_check($v) . "'", 0) != 1)
+    if(ps_value("SELECT count(ref) AS `value` FROM resource WHERE ref = ?",array("i",$v), 0) != 1)
         {
         unset($rlist[$k]);
         }
@@ -52,24 +52,22 @@ for($n = 0; $n < count($rlist); $n++)
         // Don't relate a resource to itself
         if($rlist[$n] != $rlist[$m])
             {
-            $rlist_n_escaped = escape_check($rlist[$n]);
-            $rlist_m_escaped = escape_check($rlist[$m]);
+            $resource_n = intval($rlist[$n]);
+            $resource_m = intval($rlist[$m]);
 
-            $sql = "
-                SELECT count(1) AS `value`
-                  FROM resource_related
-                 WHERE resource = '{$rlist_n_escaped}'
-                   AND related = '{$rlist_m_escaped}'
-                 LIMIT 1";
+            $sql = "SELECT count(1) AS `value`
+                    FROM resource_related
+                    WHERE resource = ?
+                    AND related = ?
+                    LIMIT 1";
 
-            if(sql_value($sql, 0) != 1)
+            if(ps_value($sql,array("i",$resource_n, "i",$resource_m), 0) != 1)
                 {
-                sql_query("
-                    INSERT INTO resource_related (resource, related)
-                         VALUES ('{$rlist_n_escaped}','{$rlist_m_escaped}')");
+                ps_query("INSERT INTO resource_related (resource, related)
+                          VALUES (?,?)",array("i",$resource_n, "i",$resource_m));
                 }
             }
         }
     }
 
-echo "Completed Relating Resources";
+echo "Completed Relating Resources\n";
