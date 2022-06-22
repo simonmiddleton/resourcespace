db95afb642da52974f86f42f0035fb15f1c3a394
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-06-22 09:53:24 +0000

    CHG: Porting some tools to use prepared statements [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19839 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/renumber_resources.php
+++ pages/tools/renumber_resources.php
@@ -88,22 +88,30 @@ for ($n=0;$n<$totalresources;$n++)
         migrate_files($ref, $newref, $alternative["ref"], $alternative["file_extension"], $sizes);
         }
 
-    # Update the ref everywhere
-    sql_query("update resource set ref='$newref' where ref='$ref'");
-    sql_query("update resource set ref='$newref' where ref='$ref'");
-    sql_query("update annotation set resource='$newref' where resource='$ref'");
-    sql_query("update collection_resource set resource='$newref' where resource='$ref'");
-    sql_query("update comment set resource_ref='$newref' where resource_ref='$ref'");
-    sql_query("update resource_license set resource='$newref' where resource='$ref'");
-    sql_query("update resource_alt_files set resource='$newref' where resource='$ref'");
-    sql_query("update resource_consent set resource='$newref' where resource='$ref'");
-    sql_query("update resource_custom_access set resource='$newref' where resource='$ref'");
-    sql_query("update resource_data set resource='$newref' where resource='$ref'");
-    sql_query("update resource_dimensions set resource='$newref' where resource='$ref'");
-    sql_query("update resource_keyword set resource='$newref' where resource='$ref'");
-    sql_query("update resource_log set resource='$newref' where resource='$ref'");
-    sql_query("update resource_node set resource='$newref' where resource='$ref'");
-    sql_query("update resource_related set resource='$newref' where resource='$ref'");
+
+    # The following tables are not subjected to the renumbering process and were never intended to because this is a one-off script:-
+    # collection_log, resource
+    # external_access_keys, resource
+    # grant_edit, resource
+    # propose_changes_data, resource
+    # slideshow, resource_ref
+
+    $parameters=array("i",$newref, "i",$ref);
+    # Update the ref on following tables
+    ps_query("update resource set ref=? where ref=?",$parameters);
+    ps_query("update annotation set resource=? where resource=?",$parameters);
+    ps_query("update collection_resource set resource=? where resource=?",$parameters);
+    ps_query("update comment set resource_ref=? where resource_ref=?",$parameters);
+    ps_query("update resource_license set resource=? where resource=?",$parameters);
+    ps_query("update resource_alt_files set resource=? where resource=?",$parameters);
+    ps_query("update resource_consent set resource=? where resource=?",$parameters);
+    ps_query("update resource_custom_access set resource=? where resource=?",$parameters);
+    ps_query("update resource_data set resource=? where resource=?",$parameters);
+    ps_query("update resource_dimensions set resource=? where resource=?",$parameters);
+    ps_query("update resource_keyword set resource=? where resource=?",$parameters);
+    ps_query("update resource_log set resource=? where resource=?",$parameters);
+    ps_query("update resource_node set resource=? where resource=?",$parameters);
+    ps_query("update resource_related set resource=? where resource=?",$parameters);
 
     }
-    
+
--- pages/tools/reset_hitcount_to_logged_downloads.php
+++ pages/tools/reset_hitcount_to_logged_downloads.php
@@ -16,8 +16,10 @@ for ($n=0;$n<count($rd);$n++)
 	{
 	$ref=$rd[$n]['ref'];
 	echo "Updating " . $ref. "<br />";
-	$count=sql_query("select * from resource_log where resource=$ref and type='d'");
-	sql_query("update resource set hit_count=0,new_hit_count=".count($count)." where ref='$ref'");
+	$parameters=array("i",$ref);
+	$count=ps_value("select count(*) value from resource_log where resource=? and type='d'",$parameters,0);
+	$parameters=array("i",$count, "i",$ref);
+	ps_query("update resource set hit_count=0,new_hit_count=? where ref=?",$parameters);
 	}
 echo "...done.";
 
