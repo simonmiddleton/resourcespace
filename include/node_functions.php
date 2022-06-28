9e5d3677e9749b5d8857ea1548df686e3eafc681
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-06-28 19:41:38 +0000

    FIX: PHP 8.1 - Passing null to non-nullable internal function parameters is deprecated, node_functions and user_functions [t31101]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19949 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/node_functions.php
+++ include/node_functions.php
@@ -23,6 +23,18 @@ function set_node($ref, $resource_type_field, $name, $parent, $order_by)
         {
         return false;
         }
+    
+    if(!is_null($parent))
+        {
+        if(is_int_loose($parent))
+            {
+            $parent = (int) $parent;
+            }
+        else 
+            {
+            return false;
+            }
+        }
 
     // Prevent the creation of duplicate nodes unless type is category tree and the nodes have different parents in the tree.
     $resource_type_field_data = get_resource_type_field($resource_type_field);
@@ -71,7 +83,7 @@ function set_node($ref, $resource_type_field, $name, $parent, $order_by)
         (
         "i",$resource_type_field,
         "s",$name,
-        "i",(trim($parent)=="" ? NULL : $parent),
+        "i",$parent,
         "s",$order_by
         );
 
@@ -265,6 +277,18 @@ function get_nodes($resource_type_field, $parent = NULL, $recursive = FALSE, $of
         return [];    
         }
 
+    if(!is_null($parent))
+        {
+        if(is_int_loose($parent))
+            {
+            $parent = (int) $parent;
+            }
+        else 
+            {
+            return false;
+            }
+        }
+
     $fieldinfo  = get_resource_type_field($resource_type_field);
     if(!in_array($fieldinfo["type"],$FIXED_LIST_FIELD_TYPES) && (is_null($rows) || (int)$rows > 10000 ))
         {
@@ -314,7 +338,7 @@ function get_nodes($resource_type_field, $parent = NULL, $recursive = FALSE, $of
         }
   
 
-    $parent_sql = trim($parent) == "" ? ($recursive ? "TRUE" : "parent IS NULL") : ("parent = ?");
+    $parent_sql = is_null($parent) ? ($recursive ? "TRUE" : "parent IS NULL") : ("parent = ?");
     if (strpos($parent_sql,"?")!==false) {$parameters[]="i";$parameters[]=$parent;}
     
     // Order by translated_name or order_by based on flag
