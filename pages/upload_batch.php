aa9dc832fe37d2b4295ed5fd3dcaa223363f2364
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-06-28 09:01:08 +0000

    FIX: remove references to resource_keyword table [q11023][snag #508]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20220112_acota_q11023@19939 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/upload_batch.php
+++ pages/upload_batch.php
@@ -749,16 +749,19 @@ if ($processupload)
             $filename_field=getvalescaped("filename_field",0,true);
             if($filename_field != 0)
                 {
-                $target_resource=ps_array("
-                    select resource value from resource_data where resource_type_field = ? and value = ? AND resource > ?
-                    union
-                    select resource value from resource_node rn join node n on rn.node = n.ref where n.resource_type_field = ? and name = ? and resource > ?", 
-                    ['i', $filename_field, 
-                     's', $origuploadedfilename,
-                     'i', $fstemplate_alt_threshold,
-                     'i', $filename_field, 
-                     's', $origuploadedfilename, 
-                     'i', $fstemplate_alt_threshold],"");
+                $target_resource = ps_array(
+                    'SELECT resource value
+                       FROM resource_node AS rn
+                       JOIN node AS n ON rn.node = n.ref
+                      WHERE n.resource_type_field = ?
+                        AND name = ?
+                        AND resource > ?', 
+                    [
+                        'i', $filename_field, 
+                        's', $origuploadedfilename, 
+                        'i', $fstemplate_alt_threshold
+                    ]
+                );
                 $target_resourceDebug = $target_resource;
                 $target_resourceDebug_message1= "Target resource details - target_resource: " . (count($target_resource)>0 ? json_encode($target_resource) : "NONE") . " . resource_type_field: $filename_field . value: $origuploadedfilename . template_alt_threshold: $fstemplate_alt_threshold . collection: $batch_replace_col";
                 debug($target_resourceDebug_message1);
