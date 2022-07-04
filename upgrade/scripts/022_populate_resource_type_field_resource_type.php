ab1079fcb839e54b4ed5e5ff041af9e82885c44e
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-07-04 12:01:35 +0000

    CHG: Fix to use correct resource_type_column [q12841]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20220704_dhuby_fields_belong_to_multiple_types_q12841@20043 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- upgrade/scripts/022_populate_resource_type_field_resource_type.php
+++ upgrade/scripts/022_populate_resource_type_field_resource_type.php
@@ -2,9 +2,9 @@
 set_sysvar(SYSVAR_UPGRADE_PROGRESS_SCRIPT, "Start decoupling of resource types and fields to enable multi type support...");
 
 // Populate new join table
-ps_query("insert into resource_type_field_resource_type(resource_type_field,resource_type) select ref,type from resource_type_field where type>0");
+ps_query("insert into resource_type_field_resource_type(resource_type_field,resource_type) select ref,resource_type from resource_type_field where type>0");
 
 // Set global values
-ps_query("update resource_type_field set global=1-least(1,type)"); // Type is 0, global is 1. All higher values of type, global is zero.
+ps_query("update resource_type_field set global=1-least(1,resource_type)"); // Type is 0, global is 1. All higher values of type, global is zero.
 
 set_sysvar(SYSVAR_UPGRADE_PROGRESS_SCRIPT, "Finished decoupling of resource types and fields to enable multi type support!");
