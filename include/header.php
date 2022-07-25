5df5d63f6e8f2d1e6abe203832a36c24e0c18df9
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-07-25 07:57:08 +0000

    FIX: Removed maximum-scale=1.0 from header tags for accessibility reasons [t31775]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@20232 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/header.php
+++ include/header.php
@@ -53,7 +53,7 @@ $noauth_page = ($pagename == "login");
     }
 ?>
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <?php hook('extra_meta'); ?>
 
 <title><?php echo htmlspecialchars($applicationname)?></title>
