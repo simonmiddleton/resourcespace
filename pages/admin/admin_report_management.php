f0858c4e7112c9df0cf4b1e17e5615eeb0d3dbe2
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-07-28 16:21:23 +0000

    FIX: Sanitise order by for report management page [q12537]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@20289 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/admin/admin_report_management.php
+++ pages/admin/admin_report_management.php
@@ -5,14 +5,18 @@ include "../../include/db.php";
 include "../../include/authenticate.php";
 
 if (!checkperm("a"))
-	{
-	exit ("Permission denied.");
-	}
+    {
+    exit ("Permission denied.");
+    }
 
 include "../../include/header.php";
 
 $find=getval("find","");
 $order_by=getval("orderby","name");
+if (!in_array(strtolower($order_by),["ref","name","support_non_correlated_sql"]))
+    {
+    $order_by="name";
+    }
 
 $url_params = array("find" => $find, "orderby" => $order_by);
 $url=generateURL($baseurl . "/pages/admin/admin_report_management.php", $url_params);
@@ -26,99 +30,99 @@ if($find!="")
 $reports=ps_query("SELECT ref, `name`, support_non_correlated_sql FROM report {$find_sql} ORDER BY {$order_by}",$sql_params??[]);
 
 ?><div class="BasicsBox"> 
-	<h1><?php echo $lang["page-title_report_management"]; ?></h1>
-	<?php
-	$links_trail = array(
-	    array(
-	        'title' => $lang["systemsetup"],
-	        'href'  => $baseurl_short . "pages/admin/admin_home.php",
-			'menu' =>  true
-	    ),
-	    array(
-	        'title' => $lang["page-title_report_management"],
-	    )
-	);
+    <h1><?php echo $lang["page-title_report_management"]; ?></h1>
+    <?php
+    $links_trail = array(
+        array(
+            'title' => $lang["systemsetup"],
+            'href'  => $baseurl_short . "pages/admin/admin_home.php",
+            'menu' =>  true
+        ),
+        array(
+            'title' => $lang["page-title_report_management"],
+        )
+    );
 
-	renderBreadcrumbs($links_trail);
-	?>
-	
-	<p><?php echo $lang['page-subtitle_report_management_edit'];render_help_link("resourceadmin/reports-and-statistics"); ?></p>
+    renderBreadcrumbs($links_trail);
+    ?>
+    
+    <p><?php echo $lang['page-subtitle_report_management_edit'];render_help_link("resourceadmin/reports-and-statistics"); ?></p>
 
-	<!--code for copy report link -->
-	<!-- form #copy_report -->
+    <!--code for copy report link -->
+    <!-- form #copy_report -->
 <form method="post"  
-	id="copy_report" 
-	action="admin_report_management_edit.php"
-	onsubmit="return CentralSpacePost(this, true);">
-	<input type="hidden" name="copyreport" value="true">
-	<input type="hidden" name="ref" value="">
-	<?php generateFormToken("copy_report"); ?>
+    id="copy_report" 
+    action="admin_report_management_edit.php"
+    onsubmit="return CentralSpacePost(this, true);">
+    <input type="hidden" name="copyreport" value="true">
+    <input type="hidden" name="ref" value="">
+    <?php generateFormToken("copy_report"); ?>
 </form>
 <!-- javascript - submits form #copy_report -->
 <script>
 function copyReport(ref)
-	{
-	frm = document.forms["copy_report"];
-	frm.ref.value=ref;
-	frm.submit();	
-	}
-	</script>
+    {
+    frm = document.forms["copy_report"];
+    frm.ref.value=ref;
+    frm.submit();	
+    }
+    </script>
 <!-- end code for copy report link -->	
 
 <?php
 function addColumnHeader($orderName, $labelKey)
-	{
-	global $baseurl, $order_by, $find, $lang;
+    {
+    global $baseurl, $order_by, $find, $lang;
 
-	if ($order_by == $orderName)
-		$image = '<span class="ASC"></span>';
-	else if ($order_by == $orderName . ' desc')
-		$image = '<span class="DESC"></span>';
-	else
-		$image = '';
+    if ($order_by == $orderName)
+        $image = '<span class="ASC"></span>';
+    else if ($order_by == $orderName . ' desc')
+        $image = '<span class="DESC"></span>';
+    else
+        $image = '';
 
-	?><td>
-	<a href="<?php echo $baseurl ?>/pages/admin/admin_report_management.php?<?php
-	if ($find!="") { ?>&find=<?php echo $find; }
-	?>&orderby=<?php echo $orderName . ($order_by==$orderName ? '+desc' : ''); ?>"
-	   onClick="return CentralSpaceLoad(this);"><?php echo $lang[$labelKey] . $image ?></a>
-	</td>
-	<?php
+    ?><td>
+    <a href="<?php echo $baseurl ?>/pages/admin/admin_report_management.php?<?php
+    if ($find!="") { ?>&find=<?php echo $find; }
+    ?>&orderby=<?php echo $orderName . ($order_by==$orderName ? '+desc' : ''); ?>"
+       onClick="return CentralSpaceLoad(this);"><?php echo $lang[$labelKey] . $image ?></a>
+    </td>
+    <?php
 }
 
-	?>
-	<div class="Listview">
-		<table border="0" cellspacing="0" cellpadding="0" class="ListviewStyle">
-			<tr class="ListviewTitleStyle">
-				<?php
+    ?>
+    <div class="Listview">
+        <table border="0" cellspacing="0" cellpadding="0" class="ListviewStyle">
+            <tr class="ListviewTitleStyle">
+                <?php
                 addColumnHeader("ref", "property-reference");
                 addColumnHeader("name", "property-name");
                 addColumnHeader('support_non_correlated_sql', 'property-support_non_correlated_sql');
                 ?>
-				<td><div class="ListTools"><?php echo $lang["tools"]?></div></td>
-			</tr>
+                <td><div class="ListTools"><?php echo $lang["tools"]?></div></td>
+            </tr>
 <?php
-		foreach ($reports as $report)
-			{
+        foreach ($reports as $report)
+            {
             $support_non_correlated_sql = ($report['support_non_correlated_sql'] === '1');
             $edit_url_extra = array();
             $edit_url_extra = ($find == "" ? $edit_url_extra : array_merge($edit_url_extra, array("find" => $find)));
             $edit_url_extra = ($order_by == "name" ? $edit_url_extra : array_merge($edit_url_extra, array("orderby" => $order_by)));
             $edit_url = generateURL("{$baseurl_short}pages/admin/admin_report_management_edit.php", array("ref" => $report["ref"]), $edit_url_extra);
-			$view_url="{$baseurl_short}pages/team/team_report.php?report={$report['ref']}&backurl=" . urlencode($url);
+            $view_url="{$baseurl_short}pages/team/team_report.php?report={$report['ref']}&backurl=" . urlencode($url);
             $a_href = (!(!db_use_multiple_connection_modes() && $execution_lockout) ? $edit_url : $view_url);
             ?>
             <tr>
-				<td>
-					<a href="<?php echo $a_href; ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo str_highlight ($report["ref"],$find,STR_HIGHLIGHT_SIMPLE); ?></a>
-				</td>
-				<td>
-					<a href="<?php echo $a_href; ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo str_highlight ($report["name"],$find,STR_HIGHLIGHT_SIMPLE); ?></a>
-				</td>
+                <td>
+                    <a href="<?php echo $a_href; ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo str_highlight ($report["ref"],$find,STR_HIGHLIGHT_SIMPLE); ?></a>
+                </td>
+                <td>
+                    <a href="<?php echo $a_href; ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo str_highlight ($report["name"],$find,STR_HIGHLIGHT_SIMPLE); ?></a>
+                </td>
                 <td><?php echo ($support_non_correlated_sql ? $lang['yes'] : $lang['no']); ?></td>
-				<td>
-					<div class="ListView" align="right">
-						<?php
+                <td>
+                    <div class="ListView" align="right">
+                        <?php
                         if(!$support_non_correlated_sql)
                             {
                             echo LINK_CARET; ?><a href="<?php echo $view_url; ?>" onclick="return CentralSpaceLoad(this, true);"><?php echo $lang["action-view"]; ?></a>
@@ -132,65 +136,65 @@ function addColumnHeader($orderName, $labelKey)
                             <?php
                             }
                             ?>
-					</div>
-				</td>
-			</tr>
+                    </div>
+                </td>
+            </tr>
 <?php
-			}
+            }
 ?>		</table>
-	</div>
+    </div>
 </div>		<!-- end of BasicsBox -->
 
 <div class="BasicsBox">
-	<form method="post" action="<?php echo $baseurl_short?>pages/admin/admin_report_management.php" onSubmit="return CentralSpacePost(this,false);">
+    <form method="post" action="<?php echo $baseurl_short?>pages/admin/admin_report_management.php" onSubmit="return CentralSpacePost(this,false);">
         <?php generateFormToken("admin_report_management_find"); ?>
-		<div class="Question">
-			<label for="find"><?php echo $lang["property-search_filter"] ?></label>
-			<input name="find" type="text" class="medwidth" value="<?php echo $find; ?>">
-			<input name="save" type="submit" value="&nbsp;&nbsp;<?php echo $lang["searchbutton"]; ?>&nbsp;&nbsp;">
-			<div class="clearerleft"></div>
-		</div>
+        <div class="Question">
+            <label for="find"><?php echo $lang["property-search_filter"] ?></label>
+            <input name="find" type="text" class="medwidth" value="<?php echo $find; ?>">
+            <input name="save" type="submit" value="&nbsp;&nbsp;<?php echo $lang["searchbutton"]; ?>&nbsp;&nbsp;">
+            <div class="clearerleft"></div>
+        </div>
 <?php
-	if ($find!="")
-		{
+    if ($find!="")
+        {
 ?>		<div class="QuestionSubmit">
-			<label for="buttonsave"></label>
-			<input name="buttonsave" type="button" onclick="CentralSpaceLoad('admin_report_management.php',false);"
-				   value="&nbsp;&nbsp;<?php echo $lang["clearbutton"]; ?>&nbsp;&nbsp;">
-		</div>
+            <label for="buttonsave"></label>
+            <input name="buttonsave" type="button" onclick="CentralSpaceLoad('admin_report_management.php',false);"
+                   value="&nbsp;&nbsp;<?php echo $lang["clearbutton"]; ?>&nbsp;&nbsp;">
+        </div>
 <?php
-		}
+        }
 ?>	</form>
 </div>
 
 <div class="BasicsBox">
-	<form method="post" action="<?php echo $baseurl_short; ?>pages/admin/admin_report_management_edit.php" onSubmit="return CentralSpacePost(this,false);">
+    <form method="post" action="<?php echo $baseurl_short; ?>pages/admin/admin_report_management_edit.php" onSubmit="return CentralSpacePost(this,false);">
         <?php generateFormToken("admin_report_management"); ?>
-    	<div class="Question">
-			<label for="name"><?php echo $lang['action-title_create_report_called']; ?></label>
-			<div class="tickset">
-				<div class="Inline">
-					<input name="newreportname" type="text" value="" class="shrtwidth">
-				</div>
-				<div class="Inline">
-					<input name="Submit" type="submit" value="&nbsp;&nbsp;<?php echo $lang["create"]; ?>&nbsp;&nbsp;" onclick="return (this.form.elements[0].value!='');">
-				</div>
-			</div>
-			<div class="clearerleft"></div>
-		</div>
-		<?php
-		if ($order_by)
-			{
-			?><input type="hidden" name="orderby" value="<?php echo $order_by; ?>">
-			<?php
-			}
-		if ($find)
-			{
-			?><input type="hidden" name="find" value="<?php echo $find; ?>">
-			<?php
-			}
-		?>
-	</form>
+        <div class="Question">
+            <label for="name"><?php echo $lang['action-title_create_report_called']; ?></label>
+            <div class="tickset">
+                <div class="Inline">
+                    <input name="newreportname" type="text" value="" class="shrtwidth">
+                </div>
+                <div class="Inline">
+                    <input name="Submit" type="submit" value="&nbsp;&nbsp;<?php echo $lang["create"]; ?>&nbsp;&nbsp;" onclick="return (this.form.elements[0].value!='');">
+                </div>
+            </div>
+            <div class="clearerleft"></div>
+        </div>
+        <?php
+        if ($order_by)
+            {
+            ?><input type="hidden" name="orderby" value="<?php echo htmlspecialchars($order_by); ?>">
+            <?php
+            }
+        if ($find)
+            {
+            ?><input type="hidden" name="find" value="<?php echo $find; ?>">
+            <?php
+            }
+        ?>
+    </form>
 </div>
 
 
