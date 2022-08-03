simonmiddleton/resourcespace

commit fd2fab261a1dd5f65b414f0416e6091c8277c270

CHG: Porting of some search functions to prepared statements [q12537]
git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@19956 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/search_functions.php
@@ -23,10 +23,12 @@ function suggest_refinement($refs,$search)
     # Given an array of resource references ($refs) and the original
     # search query ($search), produce a list of suggested search refinements to 
     # reduce the result set intelligently.
-    $in=join(",",$refs);
+
+    if (count($refs)==0) {return array();} // Nothing to do, nothing to return
+    $in=ps_param_insert(count($refs));
     $suggest=array();
     # find common keywords
-    $refine=sql_query("SELECT k.keyword,count(k.ref) c FROM resource_node rn LEFT JOIN node n ON n.ref=rn.node LEFT JOIN node_keyword nk ON nk.node=n.ref LEFT JOIN keyword k on nk.keyword=k.ref WHERE rn.resource IN ($in) AND length(k.keyword)>=3 AND length(k.keyword)<=15 AND k.keyword NOT LIKE '%0%' AND k.keyword NOT LIKE '%1%' AND k.keyword NOT LIKE '%2%' AND k.keyword NOT LIKE '%3%' AND k.keyword NOT LIKE '%4%' AND k.keyword NOT LIKE '%5%' AND k.keyword NOT LIKE '%6%' AND k.keyword NOT LIKE '%7%' AND k.keyword NOT LIKE '%8%' AND k.keyword NOT LIKE '%9%' GROUP BY k.keyword ORDER BY c DESC LIMIT 5");
+    $refine=ps_query("SELECT k.keyword,count(k.ref) c FROM resource_node rn LEFT JOIN node n ON n.ref=rn.node LEFT JOIN node_keyword nk ON nk.node=n.ref LEFT JOIN keyword k on nk.keyword=k.ref WHERE rn.resource IN ($in) AND length(k.keyword)>=3 AND length(k.keyword)<=15 AND k.keyword NOT LIKE '%0%' AND k.keyword NOT LIKE '%1%' AND k.keyword NOT LIKE '%2%' AND k.keyword NOT LIKE '%3%' AND k.keyword NOT LIKE '%4%' AND k.keyword NOT LIKE '%5%' AND k.keyword NOT LIKE '%6%' AND k.keyword NOT LIKE '%7%' AND k.keyword NOT LIKE '%8%' AND k.keyword NOT LIKE '%9%' GROUP BY k.keyword ORDER BY c DESC LIMIT 5",ps_param_fill($refs,"i"));
     for ($n=0;$n<count($refine);$n++)
         {
         if (strpos($search,$refine[$n]["keyword"])===false)
@@ -2172,7 +2174,7 @@ function resolve_keyword($keyword,$create=false,$normalize=true,$stem=true)
         {
         # Create a new keyword.
         debug("resolve_keyword: Creating new keyword for " . $keyword);
-        sql_query("insert into keyword (keyword,soundex,hit_count) values ('" . escape_check($keyword) . "',left('".soundex(escape_check($keyword))."',10),0)");
+        ps_query("insert into keyword (keyword,soundex,hit_count) values (?,?,10)",array("s",$keyword,"s",soundex($keyword)));
         $return=sql_insert_id();
         }
     
@@ -2463,7 +2465,7 @@ function save_related_keywords($keyword,$related)
         {
         for ($n=0;$n<count($s);$n++)
             {
-            sql_query("insert into keyword_related (keyword,related) values ('$keyref','" . resolve_keyword($s[$n],true,false,false) . "')");
+            ps_query("insert into keyword_related (keyword,related) values (?,?)",array("i",$keyref,"i",resolve_keyword($s[$n],true,false,false)));
             }
         }
     return true;

commit 989d8448477ff79a63a51157a85e8b970cc9893f

FIX: Incorrect PS porting around soundex insertation when indexing, broken in r19956 - patch to RC [q12537]
git-svn-id: http://svn.resourcespace.com/svn/rs/releases/10.0_RC@20328 c08608d7-6e46-0410-86ca-f2a6f1370df5

include/search_functions.php
@@ -2175,7 +2175,7 @@ function resolve_keyword($keyword,$create=false,$normalize=true,$stem=true)
         {
         # Create a new keyword.
         debug("resolve_keyword: Creating new keyword for " . $keyword);
-        ps_query("insert into keyword (keyword,soundex,hit_count) values (?,?,10)",array("s",$keyword,"s",soundex($keyword)));
+        ps_query("insert into keyword (keyword,soundex,hit_count) values (?,left(?,10),0)",array("s",$keyword,"s",soundex($keyword)));
         $return=sql_insert_id();
         }
     