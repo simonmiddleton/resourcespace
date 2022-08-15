1e57f9e6ada0b3996825c64132d14ac53ba5aa6a
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-08-15 10:10:09 +0000

    FIX: video bookend plugin error when resource file missing [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@20399 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/video_bookend/hooks/view.php
+++ plugins/video_bookend/hooks/view.php
@@ -1,20 +1,21 @@
 <?php
 
-function HookVideo_bookendViewAfterresourceactions (){
-	global $ref,$access,$lang,$resource,$cropper_allowed_extensions,$baseurl_short,$resourcetoolsGT;
+function HookVideo_bookendViewAfterresourceactions()
+    {
+    global $ref,$access,$lang,$resource,$baseurl_short;
 
-	$original_file_path=get_resource_path($ref,true,"",false,$resource['file_extension']);
-	$original_file_mime_type = mime_content_type($original_file_path);
-	if ($access==0 && $resource['has_image']==1 && substr($original_file_mime_type,0,6)=="video/")
+    $original_file_path=get_resource_path($ref,true,"",false,$resource['file_extension']);
+    if(file_exists($original_file_path))
         {
-		?>
-		<li><a onClick='return CentralSpaceLoad(this,true);' href='<?php echo $baseurl_short;?>plugins/video_bookend/pages/bookend.php?ref=<?php echo $ref?>'>
-		<?php echo "<i class='fa fa-arrows-h'></i>&nbsp;" .$lang['bookend'];?>
-		</a></li>
-		<?php
-		return true;
+        $original_file_mime_type = mime_content_type($original_file_path);
+        if ($access==0 && $resource['has_image']==1 && substr($original_file_mime_type,0,6)=="video/")
+            {
+            ?>
+            <li><a onClick='return CentralSpaceLoad(this,true);' href='<?php echo $baseurl_short;?>plugins/video_bookend/pages/bookend.php?ref=<?php echo $ref?>'>
+            <?php echo "<i class='fa fa-arrows-h'></i>&nbsp;" . $lang['bookend'];?>
+            </a></li>
+            <?php
+            return true;
+            }
         }
-
-}
-
-?>
+    }
