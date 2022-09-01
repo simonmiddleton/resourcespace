b605fc0c0eab792c8c4aa0d17739737cebfc5f93
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-09-01 10:57:26 +0000

    FIX: [10.0 RC] Removing debug line from collections_functions.php[t31101]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/10.0_RC@20559 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/collections_functions.php
+++ include/collections_functions.php
@@ -2876,7 +2876,7 @@ function get_featured_collection_resources(array $c, array $ctx)
         $union,
         sql_limit(null, $limit)
     );
-    //die("SQL: " . $sql . "\nParameters:\n" .print_r(array_merge($subquery_params,$unionparams),true));
+
     $fc_resources = ps_array($sql,array_merge($subquery_params,$unionparams),"themeimage");
     $CACHE_FC_RESOURCES[$cache_id] = $fc_resources;
     return $fc_resources;
