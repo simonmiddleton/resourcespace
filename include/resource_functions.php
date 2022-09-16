9bbd0f9b451b1bd2dd94d4b64e0f5153eed625d2
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-09-16 07:58:54 +0000

    FIX: [10.0] Edit page not showing custom permissions and get_resource_access() not returning correct group access [t32117]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/10.0@20674 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -4869,16 +4869,16 @@ function get_resource_access($resource)
             $groupspecific=$resourcedata['group_access'];
             }
         }
-	if (isset($userspecific) && $userspecific !== false)
-		{
-		$customuseraccess=true;
-		return (int) $userspecific;
-		}
-	if (isset($groupspecific) && $groupspecific!="")
-		{
-		$customgroupaccess=true;
-		return (int) $groupspecific;
-		}
+    if (isset($userspecific) && $userspecific !== false)
+        {
+        $customuseraccess=true;
+        return (int) $userspecific;
+        }        
+    if (isset($groupspecific) && $groupspecific !== "")
+        {
+        $customgroupaccess=true;
+        return (int) $groupspecific;
+        }
 
 	if (checkperm('T'.$resource_type))
 		{
