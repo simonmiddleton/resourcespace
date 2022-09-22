7658704fb93654cf949c9b350e8ab420b3d5c1f5
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-09-22 13:25:00 +0000

    FIX: simpleldap caused by a bad prepared statement change [t32074]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@20691 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/simpleldap/include/simpleldap_functions.php
+++ plugins/simpleldap/include/simpleldap_functions.php
@@ -256,7 +256,7 @@ function simpleldap_authenticate($username,$password)
                         // that some ldap servers seem to return a result count as the first value
                         $newdept = simpleldap_to_utf8($usermemberofgroup);
                         $usermemberof[]=$newdept;
-                        ps_query("replace into simpleldap_groupmap (ldapgroup, rsgroup) values (?, NULL)", ['i', $newdept]);
+                        ps_query("replace into simpleldap_groupmap (ldapgroup, rsgroup) values (?, NULL)", ['s', $newdept]);
                         } 
                     }
                 }
