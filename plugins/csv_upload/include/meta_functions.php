b1adae84f2311c3fd7d72aa85b2a6d76d34561a6
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-09-28 14:26:33 +0000

    FIX: [10.0] CSV upload plugin not working correctly after nodes phase 4 [t32140]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/10.0@20716 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/csv_upload/include/meta_functions.php
+++ plugins/csv_upload/include/meta_functions.php
@@ -7,30 +7,18 @@ function meta_get_map()		// returns array of [resource_type][table][attributes],
 	$meta=array();
 	
 	foreach (ps_query("SELECT ref, upper(title) AS `name`, `type`, title AS `nicename`, resource_type, required FROM resource_type_field WHERE name IS NOT NULL AND `name` <> '' AND (resource_type IN (SELECT ref FROM resource_type) OR resource_type = 0)", [], "schema") as $field)
-	{
-		# Get translated - support i18n - upload columns must be in user's local language.
-		$field['name']=trim(i18n_get_translated($field['name']));
-		
-		if (!isset($meta[$field['resource_type']])) $meta[$field['resource_type']]=array();		// make meta[<resource_type>] if does not exist			
-		$meta[$field['resource_type']][$field['name']]['remote_table']="resource_data";			// add meta[<resource_type>][<field>]=>attributes (remote_table,remote_ref,required,options):
-		$meta[$field['resource_type']][$field['name']]['remote_ref']=$field['ref'];
-		$meta[$field['resource_type']][$field['name']]['nicename']=$field['nicename'];	
-		$meta[$field['resource_type']][$field['name']]['required']=$field['required'];		
-		$meta[$field['resource_type']][$field['name']]['type']=$field['type'];
-		$meta[$field['resource_type']][$field['name']]['missing']=false;
-        $meta[$field['resource_type']][$field['name']]['options'] = extract_node_options(get_nodes($field['ref'], NULL, $field['type'] == FIELD_TYPE_CATEGORY_TREE));
-
-		if($meta[$field['resource_type']][$field['name']]['type']!=7) // Don't do this for category trees, not supported yet
-			{
-			for ($i=0; $i<count($meta[$field['resource_type']][$field['name']]['options']); $i++) 
-				{
-				if(isset($meta[$field['resource_type']][$field['name']]['options'][$i]))
-					{
-					$meta[$field['resource_type']][$field['name']]['options'][$i]=trim ($meta[$field['resource_type']][$field['name']]['options'][$i]);
-					}
-				}
-			}
-	}
+        {
+        # Get translated - support i18n - upload columns must be in user's local language.
+        $field['name']=trim(i18n_get_translated($field['name']));
+        
+        if (!isset($meta[$field['resource_type']])) $meta[$field['resource_type']]=array();		// make meta[<resource_type>] if does not exist			
+        $meta[$field['resource_type']][$field['name']]['remote_table']="resource_data";			// add meta[<resource_type>][<field>]=>attributes (remote_table,remote_ref,required,options):
+        $meta[$field['resource_type']][$field['name']]['remote_ref']=$field['ref'];
+        $meta[$field['resource_type']][$field['name']]['nicename']=$field['nicename'];	
+        $meta[$field['resource_type']][$field['name']]['required']=$field['required'];		
+        $meta[$field['resource_type']][$field['name']]['type']=$field['type'];
+        $meta[$field['resource_type']][$field['name']]['missing']=false;
+        }
 	$columns=ps_query("SELECT upper(column_name) AS name, column_name AS nicename FROM information_schema.columns WHERE table_name = 'resource' AND table_schema = ?", ['s', $mysql_db]);
 
 	foreach (array_keys($meta) as $resource_type)
@@ -43,9 +31,8 @@ function meta_get_map()		// returns array of [resource_type][table][attributes],
 			$meta[$resource_type][$column['name']]['remote_table']="resource";			
 			$meta[$resource_type][$column['name']]['remote_ref']=null;		// not required as mapping to resource table
 			$meta[$resource_type][$column['name']]['required']=($column=="resource_type");
-			$meta[$resource_type][$column['name']]['missing']=false;
-			$meta[$resource_type][$column['name']]['options']=array();		// for now leave this empty	
-		}
+			$meta[$resource_type][$column['name']]['missing']=false;		
+        }
 	}
 	
 	return $meta;	
