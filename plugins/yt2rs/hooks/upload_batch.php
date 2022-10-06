275ca76c4e85bb31e70fbc9677761e5bfd8eda56
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-10-06 15:32:08 +0000

    FIX: yt2rs due to not handling all cases [t32207]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@20801 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/yt2rs/hooks/upload_batch.php
+++ plugins/yt2rs/hooks/upload_batch.php
@@ -4,6 +4,8 @@ function Hookyt2rsUpload_batchUpload_page_bottom()
 	global $userref, $yt2rs_field_id, $lang;
 	$ref_user = 0 - $userref;
 	$youtube_copy_path = get_data_by_field($ref_user, $yt2rs_field_id);
+    $matches = [];
+
 	if ($youtube_copy_path == "")
 		{
 		return false;
@@ -16,14 +18,16 @@ function Hookyt2rsUpload_batchUpload_page_bottom()
 		{
 		preg_match("/v=([^&]+)/", $youtube_copy_path, $matches);
 		}
+
+    if(!isset($matches[1]))
+        {
+        return false;
+        }
+
 	$ytthumb_id = $matches[1];
 	$thumb_path = 'http://img.youtube.com/vi/' . $ytthumb_id . '/mqdefault.jpg';
 ?>
-	<h1><?php
-	echo $lang['yt2rs_thumb']; ?></h1>
-	
-
+	<h1><?php echo htmlspecialchars($lang['yt2rs_thumb']); ?></h1>
 <?php
-	echo $thumb_path;
+	echo htmlspecialchars($thumb_path);
 	}
-?>
