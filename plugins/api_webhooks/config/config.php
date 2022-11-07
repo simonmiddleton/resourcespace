283056e39da18f275a162384db278161dc324e6c
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-11-07 14:49:07 +0000

    NEW: Continued work on api_webhooks [q12918]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20221107_dhuby_q12918_api_webhooks@20943 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/api_webhooks/config/config.php
+++ plugins/api_webhooks/config/config.php
@@ -5,13 +5,13 @@ $api_webhooks_urls=[
     [
     "location"=>"edit",
     "buttontext"=>"Fetch object data from CMS",
-    "url"=>"https://remote.system/some_script.abc?resource_id="
+    "url"=>"https://www.resourcespace.com/some_script.abc?resource_id="
      ]
 ,
     [
     "location"=>"edit",
     "buttontext"=>"Push image to CMS",
-    "url"=>"https://remote.system/another_script.abc?resource_id="
+    "url"=>"https://www.resourcespace.com/another_script.abc?resource_id="
     ]
 ];
 
--- plugins/api_webhooks/hooks/edit.php
+++ plugins/api_webhooks/hooks/edit.php
@@ -5,14 +5,21 @@ function HookApi_webhooksEditExtra_edit_buttons()
     // Add configured buttons as appropriate.
     global $api_webhooks_urls;
 
+    ?>
+    <input type="hidden" name="api_webhooks_submitted_button" id="api_webhooks_submitted_button" value="" />
+    <?php
+    $counter=0;
     foreach ($api_webhooks_urls as $url)
         {
         ?>
         <input  name="savex"
         class="editsave APIWebhooksEditButton"
         type="submit"
-        value="&nbsp;&nbsp;&#8634;&nbsp;<?php echo $url["buttontext"]; ?>&nbsp;&nbsp;" />
+        value="&nbsp;&nbsp;&#8634;&nbsp;<?php echo $url["buttontext"]; ?>&nbsp;&nbsp;"
+        onclick="document.getElementById('api_webhooks_submitted_button').value=<?php echo $counter ?>"
+        />
         <?php
+        $counter++;
         }
     }
 
@@ -20,6 +27,32 @@ function HookApi_webhooksEditExtra_edit_buttons()
 function HookApi_webhooksEditRedirectaftersave()
     {
     // Process the saved form and access the remote script.
-    echo "XXX" . getval("savex","");
+    global $api_webhooks_urls,$ref;
+
+    // Fetch appropriate button
+    $button_index=getval("api_webhooks_submitted_button","");if ($button_index=="") {return false;} // No button pressed? Redirect to view page as normal.
+    $button=$api_webhooks_urls[$button_index];
+    
+    // Perform API call.
+    $url=$button["url"] . $ref;
+    $options = array
+            (
+            'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'ignore_errors' => true
+            )
+        );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    // Handle any errors.
+    if (strpos($http_response_header[0],"200 OK")===false)
+        {
+        ?>
+        <script>alert('Error - POST to <?php echo $url ?> returned <?php echo $http_response_header[0] ?>');</script>
+        <?php
+        }
+
     return true;
     }
