adbc871bb51235d2a93cd349a7144a93203bfa0f
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2022-11-07 15:53:32 +0000

    FIX: Prevent SQL error from prepared statement where IN contains no values. [t32568]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@20948 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -6108,6 +6108,11 @@ function update_related_resource($ref,$related,$add=true)
         $related = array((int)$related);
         }
 
+    if (count($related) == 0)
+        {
+        return false;
+        }
+
     // Check edit access
     $access = get_edit_access($ref);
     if(!$access)
@@ -6122,7 +6127,6 @@ function update_related_resource($ref,$related,$add=true)
             return false;
             }
         }
-
 
     // This params array can be used for both SELECT and DELETE
 	$relatedparams = array_merge(["i",$ref],ps_param_fill($related,"i"),ps_param_fill($related,"i"),["i",$ref]);
