12c94cfa90cbd35a06b7dc4b70a937ef626779b0
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-01-04 10:39:09 +0000

    FIX: missed to delete old code line [branches/20221115_acota_q12691][q12691]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20221115_acota_q12691@21210 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/view.php
+++ pages/view.php
@@ -1583,7 +1583,6 @@ if (strlen((string) $resource["file_extension"]) > 0
         }
     }
 
-if(($nodownloads || $counter == 0) && !checkperm('T' . $resource['resource_type'] . '_'))
 if(($nodownloads || $counter == 0) && !resource_has_access_denied_by_RT_size($resource['resource_type'], ''))
 	{
 	hook('beforenodownloadresult');
