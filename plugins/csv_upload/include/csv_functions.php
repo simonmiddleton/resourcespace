eb9e2a6469ba2a9f7714d970dfe42b5676d1c651
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-02-07 10:25:22 +0000

    FIX: Prevent php error if csv contains empty line and reduce calls to autocomplete_blank_fields(). [t32990]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@21414 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/csv_upload/include/csv_functions.php
+++ plugins/csv_upload/include/csv_functions.php
@@ -847,8 +847,11 @@ function csv_upload_process($filename,&$meta,$resource_types,&$messages,$csv_set
             ob_flush();
             }	// end of loop through column fields
 
-        // Autocomplete follows principal resource update
-        autocomplete_blank_fields($resource_id, false);  // false means only autocomplete blank fields
+        if ($processcsv && $csv_set_options["update_existing"] && isset($resource_id))
+            {
+            // Only when updating resources - create_resource() does this already.
+            autocomplete_blank_fields($resource_id, false);
+            }
 
         }  // end of loop through lines
 
