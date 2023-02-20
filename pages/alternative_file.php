bd980522fae74829984f5be4321eb33487a86bd2
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-02-20 23:25:11 +0000

    FIX: Editing alternative files in modal lost modal context [t33188]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@21473 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/alternative_file.php
+++ pages/alternative_file.php
@@ -13,6 +13,7 @@ $archive=getval("archive","",true);
 $restypes=getval("restypes","");
 if (strpos($search,"!")!==false) {$restypes="";}
 $modal = (getval("modal", "") == "true");
+$context = getval("context", "");
 
 $default_sort_direction="DESC";
 if (substr($order_by,0,5)=="field"){$default_sort_direction="ASC";}
@@ -62,44 +63,78 @@ if (getval("tweak","")!="" && enforcePostRequest(false))
       }
    }
 
+    $url_params = [
+        "ref" => $resource,
+        "search" => $search,
+        "offset" => $offset,
+        "order_by" => $order_by,
+        "sort" => $sort,
+        "archive" => $archive,
+        
+    ];
+    if($modal)
+        {
+        $url_params["modal"]="true";
+        if($context="Modal")$url_params["context"] = $context;
+        }
+if (getval("name","")!="" && getval("tweak","")=="" && enforcePostRequest(false)) // do not do this during a tweak operation!
+    {
+    hook("markmanualupload");
+    # Save file data
+    save_alternative_file($resource,$ref);
+    // Check to see if we need to notify users of this change
+    if($notify_on_resource_change_days!=0)
+        {
+        notify_resource_change($resource);
+        }
+    hook ("savealternatefiledata");
+
+    if (getval("tweak","")!='')
+        {
+        redirect(generateURL(
+            "{$baseurl_short}pages/alternative_file.php",
+            array_merge(["resource"=>$resource],$url_params)
+        ));
+        }
+    else
+        {
+        redirect(generateURL("{$baseurl_short}pages/alternative_files.php",$url_params));
+        }
+    }
 
 
-if (getval("name","")!="" && getval("tweak","")=="" && enforcePostRequest(false)) // do not do this during a tweak operation! 
-	{
-	hook("markmanualupload");
-	# Save file data
-	save_alternative_file($resource,$ref);
-	// Check to see if we need to notify users of this change							
-	if($notify_on_resource_change_days!=0)
-		{								
-		notify_resource_change($resource);
-		}
-	hook ("savealternatefiledata");
-    $url_modal_part = "&modal=" . ($modal ? "true" : "");
-	if (getval("tweak","")!=''){
-		redirect ($baseurl_short."pages/alternative_file.php?resource=$resource&ref=$ref&search=".urlencode($search)."&offset=$offset&order_by$order_by&sort=$sort&archive=$archive" . $url_modal_part);
-	} else {
-		redirect ($baseurl_short."pages/alternative_files.php?ref=$resource&search=".urlencode($search)."&offset=$offset&order_by=$order_by&sort=$sort&archive=$archive" . $url_modal_part);
-		}
-	}
-
-	
 include "../include/header.php";
+$backtoalternativefilesurl = generateURL("{$baseurl_short}pages/alternative_files.php",$url_params);
+$backtoalternativefileurl = generateURL("{$baseurl_short}pages/alternative_file.php",$url_params);
 ?>
 <div class="BasicsBox">
 <p>
-<a onClick="return CentralSpaceLoad(this,true);" href="<?php echo $baseurl_short?>pages/alternative_files.php?ref=<?php echo $resource?>&search=<?php echo urlencode($search)?>&offset=<?php echo $offset?>&order_by=<?php echo urlencode($order_by)?>&sort=<?php echo urlencode($sort)?>&archive=<?php echo urlencode($archive)?>"><?php echo LINK_CARET_BACK ?><?php echo $lang["backtomanagealternativefiles"]?></a>
+<a onClick="return <?php echo ($context!="Modal"?"CentralSpace":"Modal")?>Load(this,true);" 
+    href="<?php echo $backtoalternativefilesurl?>">
+    <?php echo LINK_CARET_BACK;
+    echo $lang["backtomanagealternativefiles"];?>
+</a>
 </p>
 
 <h1><?php echo $lang["editalternativefile"]; render_help_link('user/alternative-files');?></h1>
 
-<form method="post" class="form" id="fileform" onsubmit="return <?php echo ($modal ? "Modal" : "CentralSpace"); ?>Post(this, true);" action="<?php echo $baseurl_short?>pages/alternative_file.php?search=<?php echo urlencode($search)?>&offset=<?php echo urlencode($offset)?>&order_by=<?php echo urlencode($order_by)?>&sort=<?php echo urlencode($sort)?>&archive=<?php echo urlencode($archive)?>">
+<form 
+    method="post"
+    class="form"
+    id="fileform"
+    onsubmit="return <?php echo ($context="Modal" ? "Modal" : "CentralSpace"); ?>Post(this, true);" action="<?php echo $backtoalternativefileurl?>">
 <?php
 if($modal)
     {
     ?>
     <input type="hidden" name="modal" value="true">
     <?php
+    if($context="Modal")
+        {
+        ?>
+        <input type="hidden" name="context" value="Modal">
+        <?php
+        }
     }
 generateFormToken('fileform'); ?>
 <input type=hidden name=ref value="<?php echo htmlspecialchars($ref) ?>">
@@ -114,8 +149,8 @@ if(file_exists(get_resource_path($resource , true, 'thm', true, 'jpg', true, 1, 
     $fileurl = get_resource_path($resource, false, 'thm', true, 'jpg', true, 1, false, date('Y-m-d H:i:s'), $ref);
     ?>
     <div class="Question" style="border:0px;"><img id="preview" align="top" src="<?php echo $fileurl; ?>" class="ImageBorder" style="margin-right:10px;"/><br /><br /><div class="clearerleft"> </div></div>
-    <?php 
-    } 
+    <?php
+    }
     ?>
 <div class="Question">
 <label><?php echo $lang["resourceid"]?></label><div class="Fixed"><?php echo htmlspecialchars($resource) ?></div>
@@ -134,16 +169,16 @@ if(file_exists(get_resource_path($resource , true, 'thm', true, 'jpg', true, 1, 
 <?php hook('alternative_file_question', ''); ?>
 
 <?php
-	// if the system is configured to support a type selector for alt files, show it
-	if (isset($alt_types) && count($alt_types) > 1){
-		echo "<div class='Question'>\n<label for='alt_type'>".$lang["alternatetype"]."</label><select name='alt_type' id='alt_type'>";
-		foreach($alt_types as $thealttype){
-			if ($thealttype == $file['alt_type']){$alt_type_selected = " selected='selected'"; } else { $alt_type_selected = ''; }
-			$thealttype = escape_quoted_data($thealttype);
-			echo "\n   <option value='$thealttype' $alt_type_selected >$thealttype</option>";
-		}
-		echo "\n</select>\n<div class='clearerleft'> </div>\n</div>";
-	}
+    // if the system is configured to support a type selector for alt files, show it
+    if (isset($alt_types) && count($alt_types) > 1){
+        echo "<div class='Question'>\n<label for='alt_type'>".$lang["alternatetype"]."</label><select name='alt_type' id='alt_type'>";
+        foreach($alt_types as $thealttype){
+            if ($thealttype == $file['alt_type']){$alt_type_selected = " selected='selected'"; } else { $alt_type_selected = ''; }
+            $thealttype = escape_quoted_data($thealttype);
+            echo "\n   <option value='$thealttype' $alt_type_selected >$thealttype</option>";
+        }
+        echo "\n</select>\n<div class='clearerleft'> </div>\n</div>";
+    }
 ?>
 <?php if ($previews_exist){?>
 <div class="Question" id="question_imagecorrection">
@@ -151,8 +186,8 @@ if(file_exists(get_resource_path($resource , true, 'thm', true, 'jpg', true, 1, 
    <select name="tweak" id="tweak" onChange="form.submit()">
    <option value=""><?php echo $lang["select"]?></option>
    <?php
-	# On some PHP installations, the imagerotate() function is wrong and images are turned incorrectly.
-	# A local configuration setting allows this to be rectified
+    # On some PHP installations, the imagerotate() function is wrong and images are turned incorrectly.
+    # A local configuration setting allows this to be rectified
    if (!$image_rotate_reverse_options)
    {
      ?>
@@ -169,7 +204,7 @@ if(file_exists(get_resource_path($resource , true, 'thm', true, 'jpg', true, 1, 
   }
   ?>
   <option value="restore"><?php echo $lang["recreatepreviews"]?></option>
-  
+
   <?php /*hook("moretweakingopt");*/?>
 </select>
 <div class="clearerleft"> </div>
@@ -177,12 +212,12 @@ if(file_exists(get_resource_path($resource , true, 'thm', true, 'jpg', true, 1, 
 <?php } ?>
 
 <div class="QuestionSubmit">
-<label for="buttons"> </label>			
+<label for="buttons"> </label>
 <input name="save" type="submit" value="&nbsp;&nbsp;<?php echo $lang["save"]?>&nbsp;&nbsp;" />
 </div>
 </form>
 </div>
 
-<?php		
+<?php
 include "../include/footer.php";
 ?>
