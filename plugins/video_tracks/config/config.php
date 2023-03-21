293ae132375a01285f93462f8c7e660a35fec34b
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-03-21 17:31:32 +0000

    FIX: video_tracks_output_formats being overriden by the plugins' own default config value [branches/20230222_acota_q12845][q12845]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20230222_acota_q12845@21622 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/video_tracks/config/config.php
+++ plugins/video_tracks/config/config.php
@@ -11,7 +11,7 @@ $video_tracks_process_size_limit=100;
 
 $video_tracks_allow_original_custom_formats = false;
 
-$video_tracks_output_formats = array(
+$video_tracks_output_formats_default = array(
     "mp4" => array(
         "command" => "-f mp4 -ar 22050 -b 650k -ab 32k -ac 1",
         "extension" => "mp4",
--- plugins/video_tracks/pages/create_video.php
+++ plugins/video_tracks/pages/create_video.php
@@ -23,6 +23,7 @@ if($access != 0)
     }
 
 $message                     = '';
+$video_tracks_output_formats ??= $video_tracks_output_formats_default;
 $video_tracks_export_folder  = get_temp_dir(false, 'video_tracks_exports');
 $resource                    = get_resource_data($ref);
 $edit_access                 = get_edit_access($ref, $resource['archive']);
