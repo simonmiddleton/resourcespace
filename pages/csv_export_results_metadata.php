eefbf68e9115032ddcb0acc52b42ba83b8459a2d
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-03-23 10:17:59 +0000

    FIX: Update to processing search results where data array is returned to prevent no resources found error. [t33480]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@21629 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/csv_export_results_metadata.php
+++ pages/csv_export_results_metadata.php
@@ -23,7 +23,7 @@ $chunk_offset = 0; // Return data in batches. Required for particularly large cs
 while ($chunk_offset < $resources_found)
     {
     $search_results = do_search($search, $restypes, $order_by, $archive, array($chunk_offset, $search_chunk_size), $sort, false, DEPRECATED_STARSEARCH, false, false, '', false, false, true, false, false, $access, null);
-    $resources_to_process = array_merge($resources_to_process, array_column($search_results, "ref"));
+    $resources_to_process = array_merge($resources_to_process, array_column($search_results['data'], "ref"));
     $chunk_offset = $chunk_offset + $search_chunk_size;
     }
 
