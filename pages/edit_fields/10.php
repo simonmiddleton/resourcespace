eaccd93579fae848a0c93cb228b9f77d18bdb379
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-04-06 10:10:41 +0000

    NEW: input:date field tpl [branches/20230406_acota_q12900][q12900]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20230406_acota_q12900@21708 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/10.php
+++ pages/edit_fields/10.php
@@ -1,3 +1,8 @@
 <?php
+?>
+<input type="date" name="<?php echo escape_quoted_data($name); ?>" id="<?php echo escape_quoted_data($name); ?>">
+<hr>
+<p>todo: delete everything below line (and including) once the work is done</p>
+<?php
 # Date uses same code as date + time
 include "4.php";
