36fb29b2f285a6f0c66452e87bc3bddc25daf5d9
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-04-06 13:40:49 +0000

    CHG: added autosave [branches/20230406_acota_q12900][q12900]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20230406_acota_q12900@21713 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/10.php
+++ pages/edit_fields/10.php
@@ -1,10 +1,12 @@
 <?php
+$onchange_attr = $edit_autosave ? sprintf('onchange="AutoSave(%s);"', (int) $field['ref']) : '';
 ?>
 <input
     type="date"
     name="<?php echo escape_quoted_data($name); ?>"
     id="<?php echo escape_quoted_data($name); ?>"
     value="<?php echo escape_quoted_data($value); ?>"
+    <?php echo $onchange_attr; ?>
 >
 <hr>
 <p>todo: delete everything below line (and including) once the work is done</p>
