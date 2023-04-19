d70e957d52e523334683c1ab1d8eba8fc57cf93e
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-04-19 10:02:10 +0000

    CHG: dropping old code [branches/20230406_acota_q12900][q12900]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20230406_acota_q12900@21766 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/10.php
+++ pages/edit_fields/10.php
@@ -8,8 +8,4 @@ $onchange_attr = $edit_autosave ? sprintf('onchange="AutoSave(%s);"', (int) $fie
     value="<?php echo escape_quoted_data($value); ?>"
     <?php echo $onchange_attr; ?>
 >
-<hr>
-<p>todo: delete everything below line (and including) once the work is done</p>
 <?php
-# Date uses same code as date + time
-include "4.php";
