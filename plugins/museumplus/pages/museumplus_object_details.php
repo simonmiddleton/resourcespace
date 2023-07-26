b205a175169c164c6035102ecc54373de98a9f84
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-07-26 11:27:24 +0000

    FIX: MuseumPlus object details page [t34017]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@22389 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/museumplus/pages/museumplus_object_details.php
+++ plugins/museumplus/pages/museumplus_object_details.php
@@ -1,29 +1,100 @@
 <?php
-include '../../../include/db.php';
-include '../../../include/authenticate.php';
+$museumplus_rs_root = dirname(__FILE__, 4);
+include "$museumplus_rs_root/include/db.php";
+include "$museumplus_rs_root/include/authenticate.php";
 if(!checkperm('t'))
     {
     http_response_code(401);
     exit('Access denied!');
     }
-include_once '../../../include/resource_functions.php';
-include_once '../include/museumplus_functions.php';
+include_once "$museumplus_rs_root/include/resource_functions.php";
+include_once "../include/museumplus_functions.php";
 
 
+$errors = [];
 $ref = getval('ref', 0, true);
-$mpid = getval('mpid', get_data_by_field($ref, $museumplus_mpid_field)); # CAN BE ALPHANUMERIC
-$museumplus_rs_mappings = plugin_decode_complex_configs($museumplus_rs_saved_mappings);
 
-$connection_data = mplus_generate_connection_data($museumplus_host, $museumplus_application, $museumplus_api_user, $museumplus_api_pass);
-if(empty($connection_data))
+$ramc = mplus_get_associated_module_conf([$ref], true);
+$valid_association = mplus_validate_association($ramc, false, true);
+if (isset($valid_association['errors']) && !empty($valid_association['errors']))
     {
-    $error = $lang['museumplus_error_bad_conn_data'];
+    $errors[] = $valid_association['errors'][0];
     }
 
-$mplus_data = mplus_search($connection_data, $museumplus_rs_mappings, 'Object', $mpid, $museumplus_search_mpid_field);
+$modules = mplus_flip_struct_by_module($valid_association);
+foreach($modules as $module_name => $mdata)
+    {
+    if (empty($mdata['field_mappings']))
+        {
+        $errors[] = str_replace('%name', $module_name, $lang['museumplus_error_module_no_field_maps']);
+        continue;
+        }
+
+    foreach($mdata['resources'] as $resources_chunk)
+        {
+        $mplus_search = mplus_search(
+            $module_name,
+            mplus_xml_search_by_fieldpath(MPLUS_FIELD_ID, [$resources_chunk], $mdata['field_mappings'])
+        );
+        if(empty($mplus_search))
+            {
+            $errors[] = 'WARN: Search failed for some reason. Check debug log, filter by "[do_http_request][curl]"';
+            continue;
+            }
+
+        $mplus_search_xml = mplus_get_response_xml($mplus_search);
+
+        // No module items found (if since last successful validation, the module item has been deleted in MuseumPlus)
+        $module_node = $mplus_search_xml->getElementsByTagName('module')->item(0);
+        if($module_node->hasAttributes() && $module_node->attributes->getNamedItem('totalSize')->value == 0)
+            {
+            $errors[] = 'Unable to find searched module items';
+            continue;
+            }
+
+        foreach($mplus_search_xml->getElementsByTagName('moduleItem') as $module_item)
+            {
+            $mpid = $module_item->getAttribute('id');
+            if($mpid === '' || !$module_item->hasChildNodes())
+                {
+                continue;
+                }
+
+            foreach($module_item->childNodes as $child_node)
+                {
+                if(!in_array($child_node->tagName, ['systemField', 'dataField', 'virtualField']))
+                    {
+                    continue;
+                    }
+
+                $attr_name = $child_node->getAttribute('name');
+                if(!in_array($attr_name, array_merge($mdata['field_mappings'], ['__lastModified'])))
+                    {
+                    continue;
+                    }
+
+                $value = $child_node->getElementsByTagName('value')->item(0);
+                $module_items_data[$module_name][$mpid][$attr_name] = (!is_null($value) ? $value->nodeValue : '');
+                }
+            }
+        }
+    }
+$mplus_data = $module_items_data[$ramc[$ref]['module_name']][$valid_association[$ref][MPLUS_FIELD_ID] ?? 0] ?? [];
+
 include '../../../include/header.php';
+if (!empty($errors))
+    {
+    render_top_page_error_style(implode('<br> - ', $errors));
+    }
 ?>
 <h2><?php echo htmlspecialchars($lang['museumplus_object_details_title']); ?></h2>
+<p><?php printf(
+    'Resource #%d associated with module "%s" using UID "%s" found M+ record with technical ID (__id) #%d',
+    $ref,
+    htmlspecialchars($ramc[$ref]['module_name']),
+    htmlspecialchars($ramc[$ref]['field_values'][$ramc[$ref]['rs_uid_field']]),
+    $valid_association[$ref][MPLUS_FIELD_ID] ?? 0
+); ?></p>
 <div id="MuseumPlusDetailContainer" class='Listview'>
     <table>
     <?php
