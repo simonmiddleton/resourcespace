30ac5100a7f67f005a294419056ad0b6b292cb2f
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-08-07 11:27:26 +0000

    FIX: Cast to string to avoid passing a non string variable to explode in update exif tool field [t34182]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@22457 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/update_exiftool_field.php
+++ pages/tools/update_exiftool_field.php
@@ -84,7 +84,7 @@ foreach ($fieldrefs as $fieldref)
     $type=(int)$fieldref_info["type"];
     $exiftool_filter=(string)$fieldref_info["exiftool_filter"];
     $exiftool_tag=(string)$fieldref_info["exiftool_field"];
-    $restypes = !is_null($fieldref_info["resource_types"])?explode(",",$fieldref_info["resource_types"]):[];
+    $restypes = !is_null($fieldref_info["resource_types"])?explode(",",(string)$fieldref_info["resource_types"]):[];
     if ($exiftool_tag=="")
         {
         die ("Please add an exiftool mapping to your $title Field");
@@ -115,7 +115,6 @@ foreach ($fieldrefs as $fieldref)
         }
     else
         {
-            echo "bang". ps_param_insert(count($restypes));
         $rd= ps_query("SELECT ref,file_extension FROM resource $join WHERE resource_type IN(" . ps_param_insert(count($restypes)) . ") $conditionand ORDER BY ref", array_merge(ps_param_fill($restypes,"i"), $params));
         }	
     $exiftool_tags=explode(",",$exiftool_tag);
