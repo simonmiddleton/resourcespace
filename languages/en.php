9033394461bd54c4e1bd63ef7fff3ccc09d2bd96
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-10-12 17:38:27 +0000

    FIX: Send error response when attempting to create filter with no name [t34526]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@22878 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- languages/en.php
+++ languages/en.php
@@ -1958,6 +1958,7 @@ $lang["error-type-mismatch"] = "Argument '%arg' must be of type %expected-type, 
 $lang["error-request-missing-key"] = "Request missing the '%key' parameter."; # %key will be replaced with the missing POST param name required
 $lang["error-collection-unreadable"] = "Your user doesn't have read access to collection #%ref."; # %ref will be replaced with the actual collection ref
 $lang['error-unable_to_register_filter'] = "Unable to register stream filter '%FILTER_NAME'! Filtering may not work properly.";
+$lang['error-invalid_name']='Invalid name';
 
 $lang["no-options-available"]="No options available";
 $lang["header-upload-subtitle"] = "Step %number: %subtitle"; # %number, %subtitle will be replaced, e.g. Step 1: Specify Default Content For New Resources
