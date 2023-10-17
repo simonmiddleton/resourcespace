f332e1a68be71aa9e379e01c3a0d5bf9199529e6
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-10-17 15:41:49 +0000

    FIX: Check $month_part is numeric before trying to convert to wordy value [t34530]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@22899 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/general_functions.php
+++ include/general_functions.php
@@ -132,6 +132,10 @@ function nicedate($date, $time = false, $wordy = true, $offset_tz = false)
         };
 
     $month_part = substr($date, $bce_offset + 5, 2);
+    if(!is_numeric($month_part))
+        {
+        return '';
+        }
     $m = $wordy ? ($lang["months"][$month_part - 1]??"") : $month_part;
     if($m == "")
         {
@@ -3057,9 +3061,12 @@ function user_set_usergroup($user,$usergroup)
  * @param  int    $length Length of desired string of bytes
  * @return string         Random character string
  */
-function generateSecureKey(int $length = 64): string
+function generateSecureKey($length = 64)
     {
-    return bin2hex(openssl_random_pseudo_bytes($length / 2));
+    $bytes = openssl_random_pseudo_bytes($length / 2);
+    $hex   = substr(bin2hex($bytes), 0, 64); 
+
+    return $hex;
     }
 
 /**
