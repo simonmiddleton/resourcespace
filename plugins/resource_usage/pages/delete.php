c069ea0b450e139626725127045f3c66123b07cb
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-01-23 13:58:14 +0000

    FIX: Escaping variables on resource_usage delete page. [t34975]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@23377 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/resource_usage/pages/delete.php
+++ plugins/resource_usage/pages/delete.php
@@ -1,58 +1,55 @@
 <?php
 include "../../../include/db.php";
-
-include "../../../include/authenticate.php"; if (!checkperm("r")) {exit ("Permission denied.");}
-
+include "../../../include/authenticate.php";
 
 $ref      = getval('ref', '');
 $resource = getval('resource', '');
 
-# Check access
-$edit_access = get_edit_access($resource);
-if(!$edit_access)
-    {
-    # Should never arrive at this page without edit access
+# Should never arrive at this page without edit access
+if (!get_edit_access($resource)) {
     exit("Access denied");
-    }
+}
 
-if(getval('submitted', '') != '' && enforcePostRequest(false))
-    {
-    $parameters=array("i",$ref, "i",$resource);
+if (getval('submitted', '') != '' && enforcePostRequest(false)) {
+    $parameters = array("i", $ref, "i", $resource);
     ps_query("DELETE FROM resource_usage WHERE ref = ? AND resource = ?", $parameters);
-    
+
     resource_log($resource, '', '', $lang['delete_usage'] . ' ' . $ref);
-    
+
     redirect('pages/view.php?ref=' . $resource);
-    }
+}
         
 include "../../../include/header.php";
 ?>
 <div class="BasicsBox">
     <p>
-        <a href="<?php echo $baseurl_short?>pages/view.php?ref=<?php echo $resource ?>"  onClick="return CentralSpaceLoad(this, true);">&lt;&nbsp;<?php echo $lang['backtoresourceview']; ?></a>
+        <a href="<?php echo $baseurl_short?>pages/view.php?ref=<?php echo escape($resource) ?>" onClick="return CentralSpaceLoad(this, true);">
+            <?php echo LINK_CARET_BACK . escape($lang['backtoresourceview']); ?>
+        </a>
     </p>
-    <h1><?php echo $lang['delete_usage']; ?></h1>
+    
+    <h1><?php echo escape($lang['delete_usage']); ?></h1>
 
-    <form method="post" action="<?php echo $baseurl_short?>plugins/resource_usage/pages/delete.php" onSubmit="return CentralSpacePost(this,true);">
+    <form method="post" action="<?php echo $baseurl_short; ?>plugins/resource_usage/pages/delete.php" onSubmit="return CentralSpacePost(this, true);">
         <?php generateFormToken("resource_usage_deleteForm"); ?>
         <input type=hidden name="submitted" value="true">
-        <input type=hidden name="ref" value="<?php echo $ref; ?>">
-        <input type=hidden name="resource" value="<?php echo $resource; ?>">
+        <input type=hidden name="ref" value="<?php echo escape($ref); ?>">
+        <input type=hidden name="resource" value="<?php echo escape($resource); ?>">
 
         <div class="Question">
-            <label><?php echo $lang['resourceid']; ?></label>
-            <div class="Fixed"><?php echo htmlspecialchars($resource); ?></div>
+            <label><?php echo escape($lang['resourceid']); ?></label>
+            <div class="Fixed"><?php echo escape($resource); ?></div>
             <div class="clearerleft"></div>
         </div>
 
         <div class="Question">
-            <label><?php echo $lang['usage_ref']; ?></label>
-            <div class="Fixed"><?php echo htmlspecialchars($ref); ?></div>
+            <label><?php echo escape($lang['usage_ref']); ?></label>
+            <div class="Fixed"><?php echo escape($ref); ?></div>
             <div class="clearerleft"></div>
         </div>
 
         <div class="QuestionSubmit">     
-            <input name="delete" type="submit" value="&nbsp;&nbsp;<?php echo $lang["action-delete"]?>&nbsp;&nbsp;" />
+            <input name="delete" type="submit" value="<?php echo escape($lang["action-delete"]); ?>" />
         </div>
     </form>
 </div>
