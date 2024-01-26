80b25c4d3f6a23b545354400fdf4b742512f0b75
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-01-26 13:55:02 +0000

    FIX: Only return necessary columns in new consentmanager_get_consents() API function [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@23426 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/consentmanager/include/consent_functions.php
+++ plugins/consentmanager/include/consent_functions.php
@@ -2,5 +2,5 @@
 
 function consentmanager_get_consents($ref)
     {
-    return ps_query("select consent.* from consent join resource_consent on consent.ref=resource_consent.consent where resource_consent.resource= ? order by ref", ['i', $ref]);
+    return ps_query("select consent.ref,consent.name,consent.expires,consent.consent_usage from consent join resource_consent on consent.ref=resource_consent.consent where resource_consent.resource= ? order by ref", ['i', $ref]);
     }
