06017014a733a435dae09a6f4031c0a5d9638f8a
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-02-13 09:14:22 +0000

    CHG: Bump Rector major version, now at v1.0.0 and updated PHPStan (dependency). [notag]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@23574 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- rector.php
+++ rector.php
@@ -5,14 +5,27 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Removing\Rector\FuncCall\RemoveFuncCallArgRector;
 use Rector\Removing\ValueObject\RemoveFuncCallArg;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictScalarReturnExprRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    // Note: you can override processed paths when calling Rector: vendor/bin/rector process --dry-run -- include/ pages/search.php
-    $rectorConfig->paths([
+/*
+Common refactoring examples:
+
+1. Removing a functions' argument:
+    ->withConfiguredRule(
+        RemoveFuncCallArgRector::class,
+        [new RemoveFuncCallArg('get_edit_access', 2)]
+    )
+
+Result:
+-function get_edit_access($resource,$status=-999,$metadata=false,&$resourcedata="")
++function get_edit_access($resource,$status=-999,&$resourcedata="")
+
+2. TBD
+*/
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/api',
         __DIR__ . '/batch',
         __DIR__ . '/include',
@@ -24,43 +37,17 @@ return static function (RectorConfig $rectorConfig): void {
 
         // __DIR__ . '/css', # shouldn't contain PHP
         // __DIR__ . '/lib', # shouldn't really contain our code!
-    ]);
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         __DIR__ . '/plugins/*/css',
         __DIR__ . '/plugins/*/dbstruct',
         __DIR__ . '/plugins/*/lib',
-    ]);
-
-    // TODO: run these rules over a small set of files to avoid overwhelming PR
-    // Define sets of rules - https://getrector.com/documentation/set-lists
-    $rectorConfig->sets([
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_82,
-    ]);
-
-    // Individual rules - https://github.com/rectorphp/rector/blob/main/docs/rector_rules_overview.md
-    $rectorConfig->rules([
+    ])
+    ->withPreparedSets(deadCode: true)
+    // Reach current PHP version (based on composer.json) - https://getrector.com/documentation/php-version-features
+    ->withPhpSets()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
         ReturnTypeFromStrictNativeCallRector::class,
         ReturnTypeFromStrictScalarReturnExprRector::class,
     ]);
-
-
-    /*
-    IMPORTANT: Rector will change tabs to spaces. Unfortunately this will mean that many times it will change
-    indentation in places where you might not want to. This should be a "problem" until we update the entire code base
-    to only use spaces. Until then just be aware of it :).
-
-    Common refactoring examples:
-
-    1. Removing a functions' argument:
-    -function get_edit_access($resource,$status=-999,$metadata=false,&$resourcedata="")
-    +function get_edit_access($resource,$status=-999,&$resourcedata="")
-
-    $rectorConfig->ruleWithConfiguration(
-        RemoveFuncCallArgRector::class,
-        [new RemoveFuncCallArg('get_edit_access', 2)]
-    );
-
-    2. TBD
-    */
-};
