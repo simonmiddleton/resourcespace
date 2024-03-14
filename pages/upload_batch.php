879ca89093d874f11cd0b9cbcf3c64a6ba85ff4f
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-03-14 16:37:43 +0000

    FIX: When edit then upload is used, ensure that the original filename is populated after the template is copied to the target resource [t35285]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@23872 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/upload_batch.php
+++ pages/upload_batch.php
@@ -360,7 +360,7 @@ $uploadparams= array(
 
 $searchparams = get_search_params();
 
-global $merge_filename_with_title, $merge_filename_with_title_default;
+global $merge_filename_with_title, $merge_filename_with_title_default, $filename_field;
 if($merge_filename_with_title)
     {
     $merge_filename_with_title_option = urlencode(getval('merge_filename_with_title_option', $merge_filename_with_title_default));
@@ -670,6 +670,11 @@ if ($processupload)
             elseif(!$upload_then_edit)
                 {
                 $ref=copy_resource(0-$userref,-1,$lang["createdfromwebuploader"]); # Copy from user template
+                # Store original filename early in the process so that macros can benefit
+                if (isset($filename_field))
+                    {
+                    update_field($ref,$filename_field,$origuploadedfilename);
+                    }
                 }
 
             // copy_resource() returns false if user doesn't have a resource template
