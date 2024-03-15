d9c1f8a4b70e40a519e6617c3d1674622c1a2b13
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-03-15 16:55:15 +0000

    FIX: Use the pre size for the view page preview if user has restricted access to prevent download.php blocking download. [t35257]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@23877 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/render_functions.php
+++ include/render_functions.php
@@ -6750,7 +6750,7 @@ function render_resource_view_image(array $resource, array $context)
 
     // Set the preview sizes to look for. Access will be checked by get_resource_preview()
     // Retina mode uses 'scr' size
-    $viewsizes = ($GLOBALS["retina_mode"] || !$GLOBALS["resource_view_use_pre"]) ? ["scr"]: [];
+    $viewsizes = (($GLOBALS["retina_mode"] || !$GLOBALS["resource_view_use_pre"]) && $access === 0) ? ["scr"]: [];
     $viewsizes[] = "pre";
     $viewsizes[] = "thm";
 
