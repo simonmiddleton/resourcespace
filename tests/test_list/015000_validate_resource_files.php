af7e0f5807b315ec3b077504c4155a8cef3393bb
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-04-11 07:58:34 +0000

    NEW: Check in of file presence check branch [q12792]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20240311_nbraddy_q12792@24127 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- tests/test_list/015000_validate_resource_files.php
+++ tests/test_list/015000_validate_resource_files.php
@@ -6,12 +6,12 @@ $enable_thumbnail_creation_on_upload = false; // Don't need previews here
 $GLOBALS["valid_upload_paths"][] = realpath(__DIR__ . "/../../");
 
 // Create resources with files
-$image_type = create_resource_type("15000 image resources");
+$test_image_type = create_resource_type("15000 image resources");
 
 $test15000_resources = [];
-$test15000_resources[0] = create_resource($image_type,0);
-$test15000_resources[1] = create_resource($image_type,0);
-$test15000_resources[2] = create_resource($image_type,0);
+$test15000_resources[0] = create_resource($test_image_type,0);
+$test15000_resources[1] = create_resource($test_image_type,0);
+$test15000_resources[2] = create_resource($test_image_type,0);
 
 
 
@@ -31,12 +31,23 @@ if (array_search(false,$result) !== false) {
     return false;
 }
 
-// Test B - Same as A with a file missing
+// Test B - File missing
 $path = get_resource_path($test15000_resources[1],true,'',false,'png');
 unlink($path);
 $result = validate_resource_files($test15000_resources,["file_exists"=>true]);
 if (array_search(false,$result) !== $test15000_resources[1]) {
     echo " Test B - Failed to detect absence of resource file";
+    return false;
+}
+
+
+// Test C - Check resource type in $file_integrity_ignore_resource_types
+$ignore_image_type = create_resource_type("15000 ignore resource type");
+$file_integrity_ignore_resource_types[] = $ignore_image_type;
+update_resource_type($test15000_resources[1],$ignore_image_type);
+$tovalidate = get_resources_to_validate();
+if(in_array($test15000_resources[1],$tovalidate)) {
+    echo " Test B - get_resources_to_validate() returned resource of type in \$file_integrity_ignore_resource_types array";
     return false;
 }
 
