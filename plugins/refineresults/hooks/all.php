6aaf70c3b2d99c314c621f7a15384b907018238b
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-05-01 08:47:03 +0000

    FIX: Eliminate null parameter error in refineresults [t35545]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24246 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/refineresults/hooks/all.php
+++ plugins/refineresults/hooks/all.php
@@ -7,7 +7,7 @@ function HookRefineresultsSearchBeforesearchresults()
     // Only time when this would be needed is when legacy_actions is enabled otherwise we do it through dropdown actions
     $query = 'SELECT inst_version AS `value` FROM plugins WHERE name = \'legacy_actions\';';
 
-    if (trim(ps_value($query, array(), '')) === '') {
+    if (trim((string)ps_value($query, array(), '')) === '') {
         return false;
     }
 
