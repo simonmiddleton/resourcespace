7e654671e55b74ba69bd5a0f3f75bb5cf3de3dd0
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-05-08 09:51:57 +0000

    FIX: Adding CLI mode to script update_resource_file_sizes.php. [t35570]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24269 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/tools/update_resource_file_sizes.php
+++ pages/tools/update_resource_file_sizes.php
@@ -9,7 +9,10 @@
 
 include "../../include/boot.php";
 
-include "../../include/authenticate.php"; if (!checkperm("a")) {exit("Permission denied");}
+if (!(PHP_SAPI == 'cli'))
+    {
+    include "../../include/authenticate.php"; if (!checkperm("a")) {exit("Permission denied");}
+    }
 
 set_time_limit(0);
 
@@ -21,13 +24,20 @@ foreach($resources as $resource){
    $resource_path=get_resource_path($resource['ref'],true,"",false,$resource['file_extension']);
    if (file_exists($resource_path)) {
         $filesize=filesize_unlimited($resource_path);
-        
+
         ps_query("update resource_dimensions set file_size= ? where resource= ?", ['i', $filesize, 'i', $resource['ref']]);
-        echo "Ref: ".$resource['ref']." - ".$resource['field'.$view_title_field]." - updating resource_dimensions file_size column - ".formatfilesize($filesize); 
-        echo "<br />";
-   
+
+        echo "Ref: ".$resource['ref']." - ".$resource['field'.$view_title_field]." - updating resource_dimensions file_size column - ";
+        if (PHP_SAPI == 'cli')
+            {
+            echo str_replace('&nbsp;', ' ', formatfilesize($filesize)) . PHP_EOL;
+            }
+        else
+            {
+            echo formatfilesize($filesize) . "<br />";
+            }
    }
-    
+
    $alt_files= ps_query("select file_extension,file_name,ref from resource_alt_files where resource= ?", ['i', $resource['ref']]);
    if (count($alt_files)>0){
        foreach ($alt_files as $alt){
@@ -36,15 +46,30 @@ foreach($resources as $resource){
                // allow to re-run script without re-copying files
                $filesize=filesize_unlimited($alt_path);
                ps_query("update resource_alt_files set file_size= ? where resource= ? and ref= ?", ['i', $filesize, 'i', $resource['ref'], 'i', $alt['ref']]);
-               echo "&nbsp;&nbsp;&nbsp;&nbsp;ALT - ".$alt['file_name']." - updating alt file size - ".formatfilesize($filesize); 
-               echo "<br />";
-           }   
+               echo "ALT - ".$alt['file_name']." - updating alt file size - ";
+               if (PHP_SAPI == 'cli')
+                   {
+                   echo str_replace('&nbsp;', ' ', formatfilesize($filesize)) . PHP_EOL;
+                   }
+               else
+                   {
+                   echo formatfilesize($filesize) . "<br />";
+                   }
+           }
       }
     } 
 
   update_disk_usage($resource['ref']); 
-  echo "updating disk usage"; 
-  echo "<br />"; echo "<br />";
+  echo "updating disk usage";
+  if (PHP_SAPI == 'cli')
+      {
+      echo PHP_EOL . PHP_EOL;
+      }
+  else
+      {
+      echo "<br />"; echo "<br />";
+      }
+
   flush();ob_flush();
  }
 
