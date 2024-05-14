e054abb824b7b6a44115fc2729fd0f91f30dedf6
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-05-14 15:48:35 +0000

    FIX: Handle blank file extension in transform plugin when rendering resource actions [t35610]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24309 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/transform/hooks/view.php
+++ plugins/transform/hooks/view.php
@@ -8,8 +8,12 @@ function HookTransformViewAfterresourceactions (){
         {
         return false;
         }
- 
-    if ($access==0 && (int) $resource['has_image'] !== RESOURCE_PREVIEWS_NONE && in_array(strtoupper($resource['file_extension']),$cropper_allowed_extensions)){
+
+    if (
+        $access==0
+        && (int) $resource['has_image'] !== RESOURCE_PREVIEWS_NONE
+        && in_array(strtoupper($resource['file_extension']??""),$cropper_allowed_extensions)
+    ) {
         $urlparams = array(
             "ref"       =>  $ref,
             "search"    =>  $search,
