ba677164599a82c7f214cdd135a7a667bb190624
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-05-28 16:04:05 +0000

    FIX: Adding check to ensure false isn't considered a valid path if user data incorrectly supplied in $temp_folder in a future call. [t35612]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24430 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/file_functions.php
+++ include/file_functions.php
@@ -376,11 +376,7 @@ function validate_temp_path(string $test_path, string $temp_folder = '') : bool
     {
     $temp_dir = realpath(get_temp_dir(false, $temp_folder));
     $test_path = realpath(pathinfo($test_path, PATHINFO_DIRNAME));
-    if ($temp_dir === $test_path)
-        {
-        return true;
-        }
-    return false;
+    return $test_path !== false && $temp_dir !== false && $temp_dir === $test_path;
     }
 
 
