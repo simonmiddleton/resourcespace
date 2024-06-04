c654aa3d0f13bee79bff7d45eb3a15280ddf08c9
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-06-04 15:38:58 +0000

    FIX: Removed unused lang strings from video_tracks plugin [t35627]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24487 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/video_tracks/languages/en.php
+++ plugins/video_tracks/languages/en.php
@@ -20,13 +20,9 @@ $lang["video_tracks_select_generate_opt"] ="Please choose a generate option";
 $lang["video_tracks_save_alt_not_perm"] ="Unable to save alternative without permission";
 $lang["video_tracks_save_to"] = "Save to";
 $lang["video_tracks_save_alternative"] = "Alternative file";
-$lang["video_tracks_save_export"] = "Export folder";
 $lang["video_tracks_generate"] = "Generate";
 $lang["video_tracks_process_size_limit"] = "Maximum size of resource file that will be processed immediately (MB). Larger files will be processed offline and the user notified upon completion";
 $lang["video_tracks_offline_notice"]="Your request has been queued. You will be notified when the new file has been generated";
-$lang["video_tracks_export_file_created"] = "Your custom video file has been created.";
-$lang["video_tracks_export_file_failed"] = "Creation of the output file failed.";
-$lang["video_tracks_export_file_description"] = "Custom video file";
 $lang['video_tracks_upgrade_msg_deprecated_output_format'] = "IMPORTANT! The Video Tracks plugin has deprecated the output formats settings. They can only be set in config.php. The plugin will not work as intended until the configuration option has been copied over. Please copy the following:- %nl%####%nl%%output_formats_config%####%nl%";
 
 // Labels:
