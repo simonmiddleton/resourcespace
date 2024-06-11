34281f3a6319bc33446facd7f2ee0b20baac3504
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-06-11 21:05:15 +0000

    FIX: create_image_alternatives()) creating duplicate alternative files [t35625]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24532 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/image_processing.php
+++ include/image_processing.php
@@ -4179,11 +4179,17 @@ function create_image_alternatives(int $ref, array $params, $force = false)
         {
         $alternate_config = $GLOBALS["image_alternatives"][$n];
         debug("Considering image alternative. Name: ''" . $alternate_config['name'] . "', description: '" . ($alternate_config['description'] ?? "") . "'");
-        $exts = explode(',', $alternate_config['source_extensions']);
-        foreach($arrexisting as $existing)
-             {
-            if(in_array($resource_extension, $exts)
-                && $alternate_config['name'] == $existing["name"]
+        $exts = array_filter(explode(',',trim($alternate_config['source_extensions'])));
+        foreach($arrexisting as $existing) {
+            if (
+                (!empty($exts) && !in_array($resource_extension, $exts))
+            ) {
+                // Not required for this resource extension
+               continue;
+            }
+
+            if (
+                $alternate_config['name'] == $existing["name"]
                 && (!isset($alternate_config['description']) || $alternate_config['description'] == $existing["description"])
                 && $alternate_config['target_extension'] == $existing["file_extension"]
                 )
