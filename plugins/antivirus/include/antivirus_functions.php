715929a04f264036e8e3ea0ce3aeacf5d6810adf
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-06-11 13:25:06 +0000

    FIX: Adding support for clamdscan. [t35721]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24525 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/antivirus/include/antivirus_functions.php
+++ plugins/antivirus/include/antivirus_functions.php
@@ -34,7 +34,22 @@ function antivirus_scan($file_path)
 
     $cmd_output = run_command($cmd);
 
-    if('' == $cmd_output)
+    debug("Antivirus plugin scan result for path: $file_path : $cmd_output");
+
+    $clamdscan = substr($antivirus_path, -9) == 'clamdscan';
+    $clamdscan_result = false; # Default is unsafe
+    if ($clamdscan)
+        {
+        $scan_result_error =  stripos($cmd_output, 'error') !== false; # Clamdscan may return errors. Always report file is unsafe if scanning results in error.
+        $scan_result_is_infected = substr($cmd_output, strpos($cmd_output, 'Infected files:') + 16, 1);
+        if (!$scan_result_error && $scan_result_is_infected === '0')
+            {
+            # File is safe
+            $clamdscan_result = true;
+            }
+        }
+
+    if($clamdscan_result || '' == $cmd_output)
         {
         return 'SAFE';
         }
