f7ee682a68a63e0cea10d3d54fa96c70d2fdbf80
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-06-20 15:22:35 +0000

    CHG: format_chooser logic to support new layout [branches/20240604_acota_q13172][q13172]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20240604_acota_q13172@24593 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/format_chooser/hooks/view.php
+++ plugins/format_chooser/hooks/view.php
@@ -43,13 +43,49 @@ function HookFormat_chooserViewAppend_to_download_filename_td(array $resource, $
 
 function HookFormat_chooserViewAppend_to_updateDownloadLink_js()
 {
-    // Directly within Javascript world on the view page!
-    ?>
-    let format = jQuery('select#' + ns + 'format').find(":selected").val();
-    console.debug('HookFormat_chooserViewAppend_to_updateDownloadLink_js: format = %o', format);
+    global $baseurl;
 
-    let profile = jQuery('select#' + ns + 'profile').find(":selected").val();
+    /*
+    IMPORTANT: Directly within Javascript world on the view page!
+
+    The logic will modify the "downloadlink" URL and inject the users' selection (format & profile) required by the
+    plugin.
+
+    Use cases and URL placement (href/onclick attributes):
+    - Download progress (normal download) - onclick=directDownload()
+    - Request resource - href -> leave alone, no action required
+    - Terms and Download usage - href -> the URL of interest is inside the "url" query string param.
+    */
+    ?>
+    console.debug('HookFormat_chooserViewAppend_to_updateDownloadLink_js specific logic ...');
+    const format = jQuery('select#' + ns + 'format').find(":selected").val().toLowerCase();
+    const profile = jQuery('select#' + ns + 'profile').find(":selected").val();
+    console.debug('HookFormat_chooserViewAppend_to_updateDownloadLink_js: format = %o', format);
     console.debug('HookFormat_chooserViewAppend_to_updateDownloadLink_js: profile = %o', profile);
+
+    // Example (final) regex (simplified): /^directDownload\('(https\:\/\/localhost\S*)', this\)$/m
+    const direct_dld_regex = new RegExp("^directDownload\\('(<?php echo preg_quote(parse_url($baseurl, PHP_URL_SCHEME)); ?>\\:\\\/\\\/<?php echo preg_quote(parse_url($baseurl, PHP_URL_HOST)); ?>\\S*)', this\\)$", 'm');
+    const dld_btn_onclick = download_btn.attr('onclick');
+    const dld_btn_href = download_btn.attr('href');
+
+    if (dld_btn_href === '#' && direct_dld_regex.test(dld_btn_onclick)) {
+        const orig_url = direct_dld_regex.exec(dld_btn_onclick)[1];
+        let format_chooser_modified = new URL(orig_url);
+        format_chooser_modified.searchParams.set('ext', format);
+        format_chooser_modified.searchParams.set('profile', profile);
+        download_btn.attr('onclick', dld_btn_onclick.replace(orig_url, format_chooser_modified.toString()));
+    } else if (
+        dld_btn_href.startsWith('<?php echo "{$baseurl}/pages/download_usage.php"; ?>')
+        || dld_btn_href.startsWith('<?php echo "{$baseurl}/pages/terms.php"; ?>')
+    ) {
+        const orig_url = new URL(dld_btn_href);
+        let format_chooser_modified = new URL(dld_btn_href);
+        let inner_url = new URL(orig_url.searchParams.get('url'));
+        inner_url.searchParams.set('ext', format);
+        inner_url.searchParams.set('profile', profile);
+        format_chooser_modified.searchParams.set('url', inner_url.toString());
+        download_btn.prop('href', dld_btn_href.replace(orig_url, format_chooser_modified.toString()));
+    }
     <?php
 }
 
