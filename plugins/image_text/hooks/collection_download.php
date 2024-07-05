8ab4eae7c339056cccdd0f6779cc5721f59c41af
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-07-05 16:19:29 +0000

    FIX: More updates to run_command() calls in image_text plugin [t35625]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@24684 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/image_text/hooks/collection_download.php
+++ plugins/image_text/hooks/collection_download.php
@@ -12,47 +12,53 @@ function HookImage_textCollection_downloadReplaceuseoriginal()
     <div class="clearerleft"> </div></div>
     <?php
     return false;        
-        
     }
 
-
-
 function HookImage_textCollection_downloadModifydownloadfile()
-    {
+{
     global $result, $n, $p, $newpath, $userref, $usergroup, $pextension,
     $image_text_restypes, $image_text_override_groups, $image_text_filetypes,
     $usesize, $pextension, $use_watermark, $alternative, $tmpfile,
-    $filename, $image_text_height_proportion, $image_text_max_height, 
+    $filename, $image_text_height_proportion, $image_text_max_height,
     $image_text_min_height, $image_text_font, $image_text_position,$image_text_banner_position,
     $imagemagick_path;
-    
+
     # Return if not configured for this resource type or if user has requested no overlay and is permitted this
-    if(!in_array($result[$n]['resource_type'], $image_text_restypes) || !in_array(strtoupper($result[$n]['file_extension']), $image_text_filetypes) || (getval("nooverlay","")!="" && in_array($usergroup, $image_text_override_groups)) || $use_watermark){return false;}
-    
+    if (
+        !in_array($result[$n]['resource_type'], $image_text_restypes)
+        || !in_array(strtoupper($result[$n]['file_extension']), $image_text_filetypes)
+        || (getval("nooverlay","") != "" && in_array($usergroup, $image_text_override_groups))
+        || $use_watermark
+    ) {
+        return false;
+    }
+
      # Get text from field
     global $image_text_field_select, $image_text_default_text;
     $overlaytext=get_data_by_field($result[$n]["ref"], $image_text_field_select);
-    if($overlaytext=="")
-        {
-        if($image_text_default_text!="")
-            {$overlaytext=$image_text_default_text;}
-        else
-            {return false;}
+    if ($overlaytext=="") {
+        if ($image_text_default_text != "") {
+            $overlaytext=$image_text_default_text;
+        } else {
+            return false;
         }
-    
+    }
+
     # If this is not a temporary file having metadata written see if we already have a suitable size with the correct text
     $image_text_saved_file=get_resource_path($result[$n]["ref"],true,$usesize . "_image_text_" . md5($overlaytext . $image_text_height_proportion . $image_text_max_height . $image_text_min_height . $image_text_font . $image_text_position . $image_text_banner_position) . "_" ,false,$pextension,-1,1);
-    
-    if ($p!=$tmpfile && $p!=$newpath && file_exists($image_text_saved_file))
-        {
+
+    if ($p!=$tmpfile && $p!=$newpath && file_exists($image_text_saved_file)) {
         $p=$image_text_saved_file;
         return true;
-        }
+    }
 
     # Locate imagemagick.
     $identify_fullpath = get_utility_path("im-identify");
-    if ($identify_fullpath==false) {exit("Could not find ImageMagick 'identify' utility at location '$imagemagick_path'.");}
-        
+    if ($identify_fullpath == false) {
+        debug("Could not find ImageMagick 'identify' utility at location '{$imagemagick_path}'.");
+        return false;
+    }
+
     # Get image's dimensions.
     $identcommand = $identify_fullpath . ' -format %wx%h [FILE]';
     $cmdparams =  ["[FILE]" => new CommandPlaceholderArg($p, 'is_valid_rs_path')];
@@ -71,7 +77,6 @@ function HookImage_textCollection_downloadModifydownloadfile()
 
     $tmpolfile= get_temp_dir() . "/" . $result[$n]["ref"] . "_image_text_" . $userref . "." . $pextension;
 
-
     $createolcommand = $convert_fullpath . ' -background [BACKCOLOUR] -fill white -gravity [POSITION] -font [FONT] -size [WIDTHxHEIGHT] caption:[CAPTION] [TMPOLFILE]';
     $cmdparams =  [
         "[BACKCOLOUR]" => "#000",
@@ -80,39 +85,41 @@ function HookImage_textCollection_downloadModifydownloadfile()
         "[FONT]" => $image_text_font,
         "[WIDTHxHEIGHT]" => (int) $width . "x" . (int) $olheight,
         "[CAPTION]" => new CommandPlaceholderArg($overlaytext, 'is_string'),
-        "[TMPOLFILE]" => $tmpolfile,
+        "[TMPOLFILE]" => new CommandPlaceholderArg($tmpolfile, 'is_valid_rs_path'),
     ];
     run_command($createolcommand, false, $cmdparams);
 
     $newdlfile = get_temp_dir() . "/" . $result[$n]["ref"] . "_image_text_result_" . $userref . "." . $pextension;
 
-    if($image_text_banner_position=="bottom")
-        {$convertcommand = $convert_fullpath . " " . escapeshellarg($p) .  ' ' . escapeshellarg($tmpolfile) . ' -append ' . escapeshellarg($newdlfile);}
-    else
-        {
-        $convertcommand = $convert_fullpath . " " . escapeshellarg($tmpolfile) .  ' ' . escapeshellarg($p) . ' -append ' . escapeshellarg($newdlfile);
-        }
-    run_command($convertcommand);
+    if ($image_text_banner_position == "bottom") {
+        $convertcommand = $convert_fullpath . " [FILE] [TMPOLFILE] -append [NEWDLFILE]";
+    } else {
+        $convertcommand = $convert_fullpath . " [TMPOLFILE] [FILE] -append [NEWDLFILE]";
+    }
+     $cmdparams = [
+        "[FILE]" => new CommandPlaceholderArg($p, 'is_valid_rs_path'),
+        "[TMPOLFILE]" => new CommandPlaceholderArg($tmpolfile, 'is_valid_rs_path'),
+        "[NEWDLFILE]" => new CommandPlaceholderArg($newdlfile, 'is_valid_rs_path'),
+    ];
+    run_command($convertcommand, false, $cmdparams);
 
-    if ($p!=$tmpfile) # If this is not a temporary file having metadata written then copy it to the filestore for future use
-        {
+    if ($p!=$tmpfile) {
+        # If this is not a temporary file having metadata written then copy it to the filestore for future use
         copy($newdlfile, $image_text_saved_file);
-        }
-    
-    if ($p==$tmpfile || $p==$newpath) # Replace file in temporary directory with modified file
-        {
+    }
+
+    if ($p == $tmpfile || $p == $newpath) {
+        # Replace file in temporary directory with modified file
         unlink($p);
         rename($newdlfile,$p);
-        }
-    else # File is in original location, just retarget $p
-        {
+    } else {
+        # File is in original location, just retarget $p
         $p=$newdlfile;
-        }
-        
-    if(file_exists($tmpolfile))
-        {
-        unlink($tmpolfile);
-        }    
-    
-    return true;
     }
+
+    if (file_exists($tmpolfile)) {
+        unlink($tmpolfile);
+    }
+
+    return true;
+}
