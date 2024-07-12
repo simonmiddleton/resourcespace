8d603c02b95b2ed4744090311e19311aacc97f02
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-07-12 09:04:17 +0000

    FIX: [10.4 RC] Correct location of imagecolorallocate in r24710 [t35873]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/10.4_RC@24737 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/render_functions.php
+++ include/render_functions.php
@@ -5813,9 +5813,10 @@ function render_antispam_question()
     $font=dirname(__FILE__). "/../gfx/fonts/vera.ttf";
 
     $capimage = imagecreate($width, $height);
+    imagecolorallocate($capimage, 245, 245, 245);
     $textcolor = imagecolorallocate($capimage, 34, 34, 34);
     $green = ImageColorAllocate($capimage, 121, 188, 65);
-    imagecolorallocate($capimage, 245, 245, 245);
+
     ImageRectangle($capimage,0,0,$width-1,$height-1,$green); 
     imageline($capimage, 0, $height/2, $width, $height/2, $green); 
     imageline($capimage, $width*4/5, 2, $width*4/5, $height, $green);
