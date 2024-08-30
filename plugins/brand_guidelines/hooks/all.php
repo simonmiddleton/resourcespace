c4bb0480e9ca78d8596f727be7847a3393d1e1b4
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-08-30 16:14:33 +0000

    NEW: dbstruct and top nav link [branches/20240830_acota_q13131][q13131]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20240830_acota_q13131@24956 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/brand_guidelines/hooks/all.php
+++ plugins/brand_guidelines/hooks/all.php
@@ -3,3 +3,12 @@ function HookBrand_guidelinesAllInitialise()
     {
     include_once dirname(__DIR__) . '/include/brand_guidelines_functions.php';
     }
+
+function HookBrand_guidelinesAllHandleuserref() {
+    if (acl_can_view_brand_guidelines()) {
+        $GLOBALS['custom_top_nav'][] = [
+            'title' => '<i aria-hidden="true" class="fa-fw fa-solid fa-list-check"></i>&nbsp;' . $GLOBALS['lang']['brand_guidelines_top_nav_title'],
+            'link' => "{$GLOBALS['baseurl']}/plugins/brand_guidelines/pages/guidelines.php",
+        ];
+    }
+}
--- plugins/brand_guidelines/languages/en.php
+++ plugins/brand_guidelines/languages/en.php
@@ -2,3 +2,4 @@
 $lang['brand_guidelines_'] = '';
 $lang['brand_guidelines_permission_bgv'] = 'Can view the Brand Guidelines';
 $lang['brand_guidelines_permission_bge'] = 'Can edit the Brand Guidelines';
+$lang['brand_guidelines_top_nav_title'] = 'Guidelines';
