1c3a36860ffd51e063516cbfe4c92a2a0474be7b
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-09-17 14:17:53 +0000

    FIX: Further minor change following commit of log in with email address branch r25014 [q13114]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@25062 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/team/team_user_edit.php
+++ pages/team/team_user_edit.php
@@ -182,7 +182,7 @@ if (($user["login_tries"]>=$max_login_attempts_per_username) && (strtotime($user
 <div class="Question" ><label><?php echo escape($lang["username"])?></label><input id="user_edit_username" name="username" type="text" class="stdwidth" value="<?php echo form_value_display($user,"username") ?>"><div class="clearerleft"> </div></div>
 
 <?php if (!hook("password", "", array($user))) { ?>
-<div class="Question"><label><?php echo escape($lang["password"])?></label><input name="password" id="password" type="text" class="medwidth" value="<?php echo escape($lang["hidden"]); ?>" autocomplete="new-password">&nbsp;<input class="medcomplementwidth" type=submit name="suggest" value="<?php echo escape($lang["suggest"])?>" onclick="jQuery.get(this.form.action + '&suggest=true', function(result) {jQuery('#password').val(result);});return false;" /><div class="clearerleft"> </div></div>
+<div class="Question"><label><?php echo escape($lang["password"])?></label><input name="password" id="password" type="text" class="medwidth" value="<?php echo escape($lang["hidden"]); ?>" autocomplete="new-password">&nbsp;<input class="medcomplementwidth" type=submit name="suggest" value="<?php echo escape($lang["suggest"])?>" onclick="jQuery.get(this.form.action + '&suggest=true', function(result) {jQuery('#password').val(DOMPurify.sanitize(result));});return false;" /><div class="clearerleft"> </div></div>
 <?php } else { ?>
 <div><input name="password" id="password" type="hidden" value="<?php echo escape($lang["hidden"]);?>" /></div>
 <?php } ?>
