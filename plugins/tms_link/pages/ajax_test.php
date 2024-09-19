be05213ed2c77108d36e296a876efe755aa25900
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-09-19 17:19:01 +0000

    FIX: [10.4] The TMS test link function error handling corrected so that it actually reports an error instead of causing an error [CR2998][t36182]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/10.4@25072 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/tms_link/pages/ajax_test.php
+++ plugins/tms_link/pages/ajax_test.php
@@ -5,20 +5,26 @@ include '../../../include/authenticate.php'; if (!checkperm('a')) {exit ($lang['
 $tms_test_dsn = getval("dsn","");
 $tms_test_user = getval("tmsuser","");
 $tms_test_password = getval("tmspass","");
-$conn=@odbc_connect($tms_test_dsn, $tms_test_user, $tms_test_password);
 
-if(!$conn)
-  {
-  $error=odbc_errormsg();
-  $return["result"]     = $lang["error"];
-  $return["message"]    = $lang["tms_link_tms_link_failure"] . ": " . $error;
-  }
-else
-  {
-  $return["result"]     = $lang["ok"];
-  $return["message"]    = $lang["tms_link_tms_link_success"];
-  }
-
-odbc_close($conn);
+// Attempt to connect
+$GLOBALS["use_error_exception"] = true;
+try {
+    $conn=odbc_connect($tms_test_dsn, $tms_test_user, $tms_test_password);
+    if(!$conn) {
+        $error=odbc_errormsg();
+        $return["result"]     = $lang["error"];
+        $return["message"]    = $lang["tms_link_tms_link_failure"] . ": " . $error;
+    } else {
+        $return["result"]     = $lang["ok"];
+        $return["message"]    = $lang["tms_link_tms_link_success"];
+        odbc_close($conn);
+    }
+}
+catch (Exception $e) {
+    $returned_error = $e->getMessage();
+    $return["result"]     = $lang["error"];
+    $return["message"]    = $lang["tms_link_tms_link_failure"] . ": " . $returned_error;
+}
+unset($GLOBALS["use_error_exception"]);
 echo json_encode($return);
 exit();
