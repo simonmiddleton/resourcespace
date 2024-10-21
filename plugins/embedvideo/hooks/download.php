e5e99703bc332bbb5b02d44acb6939374b6d49bb
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-10-21 11:45:39 +0000

    FIX: [10.4] Implement hook to bypass cors when requesting a subtitle alternative file for video embeds [t36294] [CR 3062]
    git-svn-id: http://svn.resourcespace.com/svn/rs/releases/10.4@25275 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/embedvideo/hooks/download.php
+++ plugins/embedvideo/hooks/download.php
@@ -3,3 +3,13 @@
 function HookEmbedvideoAllDirectdownloadaccess(){
     return getval('ext', '') === 'vtt';
 } 
+
+function HookEmbedvideoAllModified_cors_process(){
+    return getval('ext', '') === 'vtt' 
+        && getval('alternative', 0, true) !== 0
+        && getval('ref', 0, true) !== 0
+        && (
+            file_exists(get_resource_path(getval('ref', 0, true), true, '', false, 'mp4'))
+            || file_exists(get_resource_path(getval('ref', 0, true), true, 'pre', false, 'mp4'))
+        );
+} 
