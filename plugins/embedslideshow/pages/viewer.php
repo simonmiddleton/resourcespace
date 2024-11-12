32033e57b6260a88794230b6aa5ac63c5faff74a
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-11-12 17:12:35 +0000

    FIX: Embedded slideshows are now played without clipping the contant; aspect ratios are maintained when container is resized [t36233]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@25442 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/embedslideshow/pages/viewer.php
+++ plugins/embedslideshow/pages/viewer.php
@@ -3,7 +3,6 @@ $suppress_headers=true; # Suppress headers including the XFRAME limitation so th
 
 include "../../../include/boot.php";
 
-
 include_once "../languages/en.php"; # Because this may not be included automatically, i.e. if the plugin is not available to all groups.
 
 # Get variables and check key is valid.
@@ -32,211 +31,235 @@ if (!check_access_key_collection($ref,$k))
 $use_watermark=check_use_watermark();
 ob_start();
 ?>
+
 <html>
 <head>
-<link href="../css/embedslideshow.css?css_reload_key=<?php echo $css_reload_key; ?>" rel="stylesheet" type="text/css" media="screen,projection,print" /> 
 <link rel="stylesheet" href="<?php echo $baseurl?>/lib/fontawesome/css/all.min.css?css_reload_key=<?php echo $css_reload_key?>">
 <link rel="stylesheet" href="<?php echo $baseurl?>/lib/fontawesome/css/v4-shims.min.css?css_reload_key=<?php echo $css_reload_key?>">
-<link id="global_font_link" href="<?php echo $baseurl?>/css/fonts/<?php echo $global_font ?>.css?css_reload_key=<?php echo $css_reload_key?>" rel="stylesheet" type="text/css" />
-<script src="../../..<?php echo $jquery_path ?>?css_reload_key=<?php echo $css_reload_key; ?>" type="text/javascript"></script>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slideshow</title>
+    <style>
+        /* Reset some default browser padding */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        /* Layout styling */
+        body {
+            font-family: Arial, sans-serif;
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+        }
+
+        header {
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 1em;
+            position: fixed;
+            width: 100%;
+            z-index: 10;
+        }
+
+        header {
+            top: 0;
+        }
+
+        /* Main content styling */
+        .content {
+            margin-top: 60px; /* Offset for header */
+            margin-bottom: 60px; /* Offset for footer */
+            margin-right: 200px; /* Offset for sidebar */
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: calc(100vh - 120px); /* 100vh minus header and footer */
+            overflow: hidden;
+        }
+
+        /* Slideshow styling */
+        .slideshow {
+            position: relative;
+            width: 100%;
+            height: 100%;
+        }
+
+        .slide {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-size: contain;
+            background-position: center;
+            background-repeat: no-repeat;
+            opacity: 0;
+            transition: opacity 1s ease-in-out;
+        }
+
+        .slide.active {
+            opacity: 1;
+        }
+
+        .embedslideshow_text {
+            margin-top: 10px;
+            margin-left: 10px;
+        }
+    </style>
+
 </head>
 <body>
+<!-- Header -->
+<header>
+    <h1>Slideshow</h1>
+</header>
 
-<div class="embedslideshow_player">
-<div class="embedslideshow_preview" id="embedslideshow_preview" style="position: relative;width:<?php echo $player_width?>px;height:<?php echo $player_height?>px;">
+<!-- Main Content -->
+<main class="content">
+    <div class="slideshow">
 
-<script type="text/javascript">
-var embedslideshow_page=1;
-var embedslideshow_x_offsets =  new Array();
-var embedslideshow_y_offsets =  new Array();
-<?php if ($transition>0) { ?>
-var embedslideshow_auto=true;
-<?php } else { ?>
-var embedslideshow_auto=false;
-<?php } ?>
-var timer;
-</script>
-
-<?php
-$page=1;
-$resources=do_search("!collection" . $ref);
-if(count($resources) == 0)
-    {
-    $all_fcs = get_all_featured_collections();
-    $refs = array_column($all_fcs, 'ref');
-    $ref_key = array_search($ref, $refs);
-    if($ref_key !== null && is_featured_collection_category($all_fcs[$ref_key]))
-        {
-        $resources = get_featured_collection_resources($all_fcs[$ref_key], ['all_fcs' => $all_fcs]);
-        $resources = get_resource_data_batch($resources);
-        }
-    }
-if(count($resources) == 0)
-    {
-    exit($lang["embedslideshow_notavailable"]);
-    }
-foreach ($resources as $resource)
-    {
-        
-    $file_path=get_resource_path($resource["ref"],true,$size,false,$resource["preview_extension"],-1,1,$use_watermark);
-    if (file_exists($file_path))
-        {
-        $preview_path=get_resource_path($resource["ref"],false,$size,false,$resource["preview_extension"],-1,1,$use_watermark);     
-        }
-    else
-    
-        {
-        # Fall back to 'pre' size
-        $preview_path=get_resource_path($resource["ref"],false,"pre",false,$resource["preview_extension"],-1,1,$use_watermark);
-        }        
-    $preview_path .= "&k=" . $k;
-    
-    # sets height and width to display 
-    if(!isset($resource["thumb_width"]) || $resource["thumb_width"]<1 || !isset($resource["thumb_height"]) || $resource["thumb_height"] <1) {continue;/*No Preview Available*/}
-    $ratio=$resource["thumb_width"]/$resource["thumb_height"];
-    
-    if ($ratio > $player_ratio) // Base on the width unless we have been asked to scale to specific width
-        {
-        # Landscape image, width is the largest - scale the height
-        $width = $player_width - 8;
-        $height=floor($width / $ratio);
-        }
-    else
-        {
-        $height = $player_height;
-        $width = floor($height* $ratio);
-        }       
-    ?>
-    <a class="embedslideshow_preview_inner" id="embedslideshow_preview<?php echo $page ?>" style="display:none;" href="#" onClick="embedslideshow_auto=false;embedslideshow_ShowPage(<?php echo $page + 1 ?>,false,false);return false;">
-        <img alt="<?php echo escape(i18n_get_translated($resource['field'.$view_title_field] ?? ""));?>"
-        border="0" width=<?php echo (int) $width; ?> height=<?php echo (int) $height; ?> src="<?php echo escape($preview_path); ?>">
-    </a>
-    <?php 
-    global $embedslideshow_textfield,$embedslideshow_resourcedatatextfield;
-    if($embedslideshow_textfield && $showtext) 
-        { 
-        $resource_data = i18n_get_translated(get_data_by_field($resource["ref"],$embedslideshow_resourcedatatextfield));
-        if($resource_data !="") 
-            {
-            ?>
-            <span class="embedslideshow_text" id="embedslideshow_previewtext<?php echo $page ?>"><?php echo $resource_data;?></span>
-            <?php
-            }       
-        } ?>
-    <script type="text/javascript">
-    embedslideshow_x_offsets[<?php echo $page ?>]=<?php echo ($ratio < $player_ratio)?(ceil(($player_width-$width)/2)+4):0; ?>;
-    embedslideshow_y_offsets[<?php echo $page ?>]=<?php echo ($ratio > $player_ratio)?(ceil(($player_height-$height)/2)+4):0; ?>;
-    </script>
     <?php
-    $page++;
-    }
-$maxpages=$page-1;
-
-// ratio won't be set if none of the resources in the collection have previews available. 
-if(!isset($ratio))
-    {
-    ob_end_clean();
-    exit($lang["embedslideshow_notavailable"]);
-    }
-ob_flush();
-?>
-
-
-</div>
-
-<ul class="embedslideshow_controls_standard">   
-
-<?php if ($width>100) { ?>
-<li class="embedslideshow_begn" Style="cursor: pointer;" onClick="embedslideshow_auto=false;embedslideshow_ShowPage(1,false,false);return false;"><i class="fas fa-step-backward"></i></li>
-<?php } ?>
-
-<li class="embedslideshow_prev" Style="cursor: pointer;" onClick="embedslideshow_auto=false;embedslideshow_ShowPage(embedslideshow_page-1,false,false);return false;"><i class="fas fa-backward"></i></li>
-
-<?php if ($width>100) { ?>
-<li class="embedslideshow_auto" id="embedslideshow_auto" Style="cursor: pointer;" onClick="embedslideshow_auto=!embedslideshow_auto;if (embedslideshow_auto) {embedslideshow_ShowPage(embedslideshow_page+1,false,false);$('#embedslideshow_auto').fadeTo(100,1);} else {clearTimeout(timer);$('#embedslideshow_auto').fadeTo(100,0.4);}return false;"><i class="fas fa-pause"></i></li>
-<?php if ($transition==0) { ?>
-<script type="text/javascript">
-$('#embedslideshow_auto').fadeTo(100,0.4);
-</script>
-<?php } ?>
-
-<?php } ?>
-
-<li class="embedslideshow_next" Style="cursor: pointer;" onClick="embedslideshow_auto=false;embedslideshow_ShowPage(embedslideshow_page+1,false,false);return false;"><i class="fas fa-forward"></i></li>
-
-<?php if ($width>100) { ?>
-<li class="embedslideshow_end" Style="cursor: pointer;" onClick="embedslideshow_auto=false;embedslideshow_ShowPage(embedslideshow_pages.length-1,false,false);return false;"><i class="fas fa-step-forward"></i></li>
-<?php } ?>
-
-<?php if ($width>200) {
-# Jump controls - only if enough room to display them
- ?>
-<li class="embedslideshow_jump" Style="cursor: pointer;" onClick="embedslideshow_auto=false;embedslideshow_ShowPage(document.getElementById('embedslideshow_page_box').value,false,true);return false;"><span><?php echo escape($lang["jump"]); ?></span></li>
-<li class="embedslideshow_jump-box"> <input type="text" id="embedslideshow_page_box" size="1" /> / <span id="page-count">#</span> </li>
-<?php } ?>
-</ul>
-
-<script type="text/javascript">
-
-function embedslideshow_ShowPage(page_set,from_auto,jump)
-    {
-    if (!embedslideshow_auto && from_auto) {return false;} // Auto switched off but timer still running. Terminate.
-    
-    if (embedslideshow_page==page_set && jump) {alert("<?php echo escape($lang["embedslideshow_alreadyonpage"]); ?>");return false;}
-    
-    // Fade out pause button if manually clicked
-    if (!embedslideshow_auto)
+    $page=1;
+    # Load images from the slideshow collection
+    $resources=do_search("!collection" . $ref);
+    if(count($resources) == 0)
         {
-        jQuery('#embedslideshow_auto').fadeTo(100,0.4);
+        $all_fcs = get_all_featured_collections();
+        $refs = array_column($all_fcs, 'ref');
+        $ref_key = array_search($ref, $refs);
+        if($ref_key !== null && is_featured_collection_category($all_fcs[$ref_key]))
+            {
+            $resources = get_featured_collection_resources($all_fcs[$ref_key], ['all_fcs' => $all_fcs]);
+            $resources = get_resource_data_batch($resources);
+            }
         }
-        
-    // Faster fade time when manually clicked
-    if (embedslideshow_auto) {var embedslideshow_fadetime=1000;} else {var embedslideshow_fadetime=200;}
-    
-    // Fade out current page
-    jQuery('#embedslideshow_preview' + embedslideshow_page).fadeOut(embedslideshow_fadetime);
-    jQuery('#embedslideshow_previewtext' + embedslideshow_page).fadeOut(embedslideshow_fadetime);
-        
-    embedslideshow_page=page_set;
-    if (embedslideshow_page>(<?php echo $maxpages ?>)) {embedslideshow_page=1;} // back to first page
-    if (embedslideshow_page<1) {embedslideshow_page=<?php echo $maxpages ?>;} // to last page
-    
-    //document.getElementById("embedslideshow_preview").innerHTML=embedslideshow_pages[embedslideshow_page];
-
-    // Center in space
-    jQuery('#embedslideshow_preview' + embedslideshow_page).css('top',embedslideshow_y_offsets[embedslideshow_page] + 'px');
-    jQuery('#embedslideshow_preview' + embedslideshow_page).css('left',embedslideshow_x_offsets[embedslideshow_page] + 'px');
-    jQuery('.embedslideshow_text').css('left',embedslideshow_x_offsets[embedslideshow_page] + 'px');
-    
-        
-    // Fade in new page
-    jQuery('#embedslideshow_preview' + embedslideshow_page).fadeIn(embedslideshow_fadetime);
-    jQuery('#embedslideshow_previewtext' + embedslideshow_page).fadeIn(embedslideshow_fadetime);
-
-      
-    if (embedslideshow_auto) {timer = setTimeout("embedslideshow_ShowPage(embedslideshow_page+1,true,false);",<?php echo $transition == 0 ? 4000 : $transition * 1000; ?>);} else {clearTimeout(timer);}
-    
-    if(jQuery('#embedslideshow_page_box'))
+    if(count($resources) == 0)
         {
-        jQuery('#embedslideshow_page_box').val(embedslideshow_page);
+        exit($lang["embedslideshow_notavailable"]);
         }
-    }
 
-
-embedslideshow_ShowPage(1,false,false);
-
-<?php if ($width>200)
-    {
+    foreach ($resources as $resource)
+        {
+        $file_path=get_resource_path($resource["ref"],true,$size,false,$resource["preview_extension"],-1,1,$use_watermark);
+        if (file_exists($file_path))
+            {
+            $preview_path=get_resource_path($resource["ref"],false,$size,false,$resource["preview_extension"],-1,1,$use_watermark);     
+            }
+        else
+            {
+            # Fall back to 'pre' size
+            $preview_path=get_resource_path($resource["ref"],false,"pre",false,$resource["preview_extension"],-1,1,$use_watermark);
+            }        
+        $preview_path .= "&k=" . $k;
     ?>
-    // publishes total page count after forward slash next to actual page
-    function totalPages(){
-        document.getElementById('page-count').innerHTML = <?php echo $maxpages ?>;
-        }
-    totalPages();
-   <?php
-   }?>
+    <!-- Markup for current resource START -->
+    <div class="slide" style="background-image: url(<?php echo escape($preview_path); ?>);">
 
+    <?php 
+        global $embedslideshow_textfield,$embedslideshow_resourcedatatextfield;
+        if($embedslideshow_textfield && $showtext) 
+            { 
+            $resource_textdata = i18n_get_translated(get_data_by_field($resource["ref"],$embedslideshow_resourcedatatextfield));
+            if($resource_textdata !="") 
+                {
+                ?>
+                <div class="embedslideshow_text" id="embedslideshow_previewtext<?php echo $page ?>"><?php echo $resource_textdata;?></div>
+                <?php
+                }       
+            }
+    ?>
+    </div>
+    <!-- Markup for current resource END -->
+    <?php 
+        $page++;
+        }
+    $maxpages=$page-1;
+    ob_flush();
+    ?>
+    <!-- Containing section END -->
+    </div>
+</main>
+<!-- Slideshow navigation control markup START -->
+<ul>   
+    <li id="slide-go-start" style="cursor: pointer;display: inline-block;" onClick="showSlidePage('start');return false;"><i class="fas fa-step-backward"></i>&nbsp;</li>
+    <li id="slide-go-prev" style="cursor: pointer;display: inline-block;" onClick="showPrevSlide();return false;"><i class="fas fa-backward"></i>&nbsp;</li>
+    <li id="slide-pause" style="cursor: pointer;display: inline-block;" onClick="pauseSlideShow();return false;"><i class="fas fa-pause"></i>&nbsp;</li>
+    <li id="slide-play" style="cursor: pointer;display: none;" onClick="playSlideShow();return false;"><i class="fas fa-play"></i>&nbsp;</li>
+    <li id="slide-go-next" style="cursor: pointer;display: inline-block;" onClick="showNextSlide();return false;"><i class="fas fa-forward"></i>&nbsp;</li>
+    <li id="slide-go-end" style="cursor: pointer;display: inline-block;" onClick="showSlidePage('end');return false;"><i class="fas fa-step-forward"></i>&nbsp;</li>
+    <li id="slide-go-atpage" style="cursor: pointer;display: inline-block;" onChange="showSlidePage('atpage');return false;">
+        <span><?php echo escape($lang["jump"]);?>&nbsp;<input type="text" id="slide-page-number" size="1" /></span>&nbsp;/&nbsp;#&nbsp;<span id="slide-page-count"></span></li>
+</ul>
+<!-- Slideshow navigation control markup END -->
+
+<!-- JavaScript for slideshow -->
+<script>
+const slides = document.querySelectorAll('.slide');
+const slideTransition = 1000 * <?php echo (int)$transition;?>;
+document.getElementById('slide-page-count').value = slides.length;
+document.getElementById('slide-page-count').innerHTML = slides.length;
+
+let slideTimer = 0;
+let currentSlide = 0;
+
+function showNextSlide() {
+    // Hide current slide
+    slides[currentSlide].classList.remove('active');
+    // Move to the next slide, or back to the first one if at the end
+    currentSlide = (currentSlide + 1) % slides.length;
+    // Show the slide
+    slides[currentSlide].classList.add('active');
+}
+
+function showPrevSlide() {
+    // Hide current slide
+    slides[currentSlide].classList.remove('active');
+    // Move to the previous slide, or back to the last one if at the start
+    currentSlide = currentSlide - 1;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+    // Show the slide
+    slides[currentSlide].classList.add('active');
+}
+
+function showSlidePage(pagerequest) {
+    // Hide current slide
+    slides[currentSlide].classList.remove('active');
+    if(pagerequest=='start') {
+        currentSlide = 0;
+    }
+    if(pagerequest=='end') {
+        currentSlide = slides.length - 1;
+    }
+    if(pagerequest=='atpage') {
+        currentSlide = slides.length - 1;
+    }
+    // Show the slide
+    slides[currentSlide].classList.add('active');
+}
+
+function pauseSlideShow() {
+    clearInterval(slideTimer);
+    document.getElementById('slide-pause').style.display = 'none';
+    document.getElementById('slide-play').style.display = 'inline-block';
+}
+
+function playSlideShow() {
+    showNextSlide();
+    document.getElementById('slide-play').style.display = 'none';
+    document.getElementById('slide-pause').style.display = 'inline-block';
+    slideTimer = setInterval(showNextSlide, slideTransition);
+}
+
+// Initialize slideshow by displaying the first image
+slides[currentSlide].classList.add('active');
+
+// Set interval for changing slides
+slideTimer = setInterval(showNextSlide, slideTransition); 
 </script>
 
-</div>
-</body></html>
+</body>
+</html>
