10c1442b78b3bf7b809b3dcdc06443ab8ce77897
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-11-14 11:48:51 +0000

    CHG: drop unnecessar lang [branches/20240830_acota_q13131][q13131]
    git-svn-id: http://svn.resourcespace.com/svn/rs/branches/20240830_acota_q13131@25461 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/brand_guidelines/languages/en.php
+++ plugins/brand_guidelines/languages/en.php
@@ -1,5 +1,4 @@
 <?php
-$lang['brand_guidelines_'] = ''; #todo: delete once done
 $lang['brand_guidelines_permission_bgv'] = 'Can view the Brand Guidelines';
 $lang['brand_guidelines_permission_bge'] = 'Can edit the Brand Guidelines';
 $lang['brand_guidelines_top_nav_title'] = 'Guidelines';
