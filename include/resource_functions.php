52df270675b216d5ba4e643f59d1fcbbab99bf77
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-11-22 15:07:05 +0000

    FIX: Remove commented line from r25477 [t36515]
    git-svn-id: http://svn.resourcespace.com/svn/rs/trunk@25553 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/resource_functions.php
+++ include/resource_functions.php
@@ -1814,9 +1814,11 @@ function save_resource_data_multi($collection,$editsearch = array(), $postvals =
                     }
                 // Add any onchange code
                 if($fields[$n]["onchange_macro"]!="") {
-                    $val = implode(',',array_column(get_nodes_by_refs($resource_add_nodes ?? $resulting_nodes_for_log),"name"));
+                    $val = implode(
+                        ',',
+                        array_column(get_nodes_by_refs($resource_add_nodes ?? $resulting_nodes_for_log),"name")
+                    );
                     $macro_resource_id=$ref;
-                    //$val = implode(",",$updated_resources[$ref][$fields[$n]['ref']]);
                     eval(eval_check_signed($fields[$n]["onchange_macro"]));
                 }
                 }
